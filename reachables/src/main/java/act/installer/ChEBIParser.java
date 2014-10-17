86a9d55adcb8c144594ffd67337d20db12abe202
Saurabh Srivastava <saurabh@20n.com>
2014-10-16 17:40:56 -0700

    chebi parser

--- reachables/src/main/java/act/installer/ChEBIParser.java
+++ reachables/src/main/java/act/installer/ChEBIParser.java
@@ -4,6 +4,13 @@ import java.io.PrintStream;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
@@ -18,16 +25,19 @@ import org.semanticweb.owlapi.model.OWLException;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
+import act.shared.helpers.P;
+
 public class ChEBIParser {
 
     private static int INDENT = 4;
     private final OWLOntology ontology;
-    private final PrintStream out;
+    private final PrintStream out, err;
     private HashMap<String, EntryTypes> entryTypes;
 
-    private ChEBIParser( OWLOntology _ontology) {
+    private ChEBIParser(OWLOntology _ontology) {
       ontology = _ontology;
       out = System.out;
+      err = System.err;
       entryTypes = new HashMap<String, EntryTypes>();
       entryTypes.put("http://purl.obolibrary.org/obo#Synonym",    EntryTypes.Synonym );
       entryTypes.put("http://purl.obolibrary.org/obo#Definition", EntryTypes.Definition );
@@ -54,8 +64,10 @@ public class ChEBIParser {
                             }
 
 
-    private HashMap<OWLClass, OWLClass> getAllElementsWithParentsIn(Set<OWLClass> parentCategories) throws OWLException {
-      HashMap<OWLClass, OWLClass> selected = new HashMap<OWLClass, OWLClass>();
+    private HashMap<OWLClass, P<OWLClass, HashMap<String, EntryTypes>>> 
+      getAllElementsWithParentsIn(Set<OWLClass> parentCategories) throws OWLException {
+      HashMap<OWLClass, P<OWLClass, HashMap<String, EntryTypes>>> selected;
+      selected = new HashMap<OWLClass, P<OWLClass, HashMap<String, EntryTypes>>>();
       for (OWLClass cl : ontology.getClassesInSignature()) {
         Set<OWLClass> parents = get_has_role_parents(cl);
         HashMap<String, EntryTypes> data = getData(cl);
@@ -64,7 +76,9 @@ public class ChEBIParser {
           if (parentCategories.contains(p)) {
             // this class has a parent within the set we are looking for.
             // so it qualifies as a class to be returned.. add it to map
-            selected.put(cl, p);
+            P<OWLClass, HashMap<String, EntryTypes>> p_data;
+            p_data = new P<OWLClass, HashMap<String, EntryTypes>>(p, data);
+            selected.put(cl, p_data);
           }
         }
       }
@@ -89,9 +103,9 @@ public class ChEBIParser {
     private void readSubtree(OWLClass clazz, HashMap<OWLClass, OWLClass> parents, int level, boolean doPrint) throws OWLException {
       if (doPrint) {
         for (int i = 0; i < level * INDENT; i++) {
-          out.print(" ");
+          err.print(" ");
         }
-        out.println(labelFor(clazz, ontology));
+        err.println(labelFor(clazz, ontology));
       }
       /* Find the children and recurse */
       for (OWLClassExpression c : clazz.getSubClasses(ontology)) {
@@ -149,16 +163,16 @@ public class ChEBIParser {
             if (singletonPropertyHasRole(properties) && classes.size() == 1) {
               OWLClass has_role_parent = classes.toArray(new OWLClass[0])[0];
               roles.add(has_role_parent);
-              // out.println("\t\t Added parent: " + has_role_parent);
+              // err.println("\t\t Added parent: " + has_role_parent);
             } 
             // else {
-            //   out.println("Was expecting singleton sets for properties and classes.");
-            //   out.println("Got more/less: " + properties + " " + classes);
+            //   err.println("Was expecting singleton sets for properties and classes.");
+            //   err.println("Got more/less: " + properties + " " + classes);
             //   System.exit(-1);
             // }
             break;
           default:
-            // out.println("\t Default (SubClassOf): " + sup);
+            // err.println("\t Default (SubClassOf): " + sup);
             break;
         }
       }
@@ -170,8 +184,8 @@ public class ChEBIParser {
         return false;
       for (OWLObjectProperty p : properties) {
         EntryTypes prop = entryTypes.get(p.toStringID());
-        // out.println("\t\t ObjProp: " + prop);
-        // out.println("\t\t ObjProp: " + p);
+        // err.println("\t\t ObjProp: " + prop);
+        // err.println("\t\t ObjProp: " + p);
         if (prop == null) System.exit(-1);
         return EntryTypes.has_role == prop;
       }
@@ -181,65 +195,140 @@ public class ChEBIParser {
     private HashMap<String, EntryTypes> getData(OWLClass clazz) {
       HashMap<String, EntryTypes> data = new HashMap<String, EntryTypes>();
       for (OWLAnnotation a : clazz.getAnnotations(ontology)) {
-        // out.println("\t\t Annotation Property: " + a.getProperty());
+        // err.println("\t\t Annotation Property: " + a.getProperty());
         String id = a.getProperty().toStringID();
         if (id.equals("rdfs:label")) continue;
         EntryTypes prop = entryTypes.get(id);
         String val = ((OWLLiteral)a.getValue()).getLiteral();
-        // out.println("\t\t Annotation Property: " + prop);
-        // out.println("\t\t Annotation Literal : " + value);
+        // err.println("\t\t Annotation Property: " + prop);
+        // err.println("\t\t Annotation Literal : " + value);
         data.put(val, prop);
       }
       return data;
     }
  
-    private static void find(String subtreeRoot, IRI documentIRI) throws OWLException {
+    private static void find(String subtreeRoot, IRI documentIRI, IRI inchiIRI) throws OWLException, IOException {
       OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
       // Load the ontology...
-      OWLOntology ontology = manager
-              .loadOntologyFromOntologyDocument(documentIRI);
+      OWLOntology ontology = manager.loadOntologyFromOntologyDocument(documentIRI);
       // Print metadata about ontology...
-      System.out.println("Ontology Loaded...");
-      System.out.println("Document IRI: " + documentIRI);
-      System.out.println("Ontology    : " + ontology.getOntologyID());
-      System.out.println("Format      : " + manager.getOntologyFormat(ontology));
-      ChEBIParser chebi = new ChEBIParser( ontology);
+      System.err.println("Ontology Loaded...");
+      System.err.println("Document IRI: " + documentIRI);
+      System.err.println("Ontology    : " + ontology.getOntologyID());
+      System.err.println("Format      : " + manager.getOntologyFormat(ontology));
+      
+      HashMap<String, String> inchis = readInchis(inchiIRI);
+      
+      ChEBIParser chebi = new ChEBIParser(ontology); 
 
       OWLClass clazz = manager.getOWLDataFactory().getOWLThing();
-      System.out.println("Toplevel    : " + clazz);
+      System.err.println("Toplevel    : " + clazz);
       clazz = chebi.recurseToSubtreeRoot(clazz, subtreeRoot);
-      System.out.println("Requested   : " + clazz);
+      System.err.println("Requested   : " + clazz);
       // Print the hierarchy
       HashMap<OWLClass, OWLClass> treeParents = new HashMap<OWLClass, OWLClass>();
       boolean doPrintSubtree = true;
       chebi.readSubtree(clazz, treeParents, doPrintSubtree);
 
       Set<OWLClass> metabolite_categories = treeParents.keySet();
-      // System.out.println("Metabolite categories: " + metabolite_categories);
+      // System.err.println("Metabolite categories: " + metabolite_categories);
 
       // The function getAllElementsWithParentsIn picks out elements from the ontology that
       // have a "has_role" relationship with any member of metabolite_categories. 
       // E.g., chemA has_role eukaryotic_metabolite; chemB has_role metabolite
       //      then { chemA -> eukaryotic_metabolite, chemB -> metabolite } will be in the
       //      returned map.
-      // TODO: Additionally the function does also gather data from each of the elems
-      // e.g., { Sy -> Synonyms, X -> xref, S -> SMILES, I -> InChI, Ik -> InChIKey }
-      // but right now does not return it back here.
-      HashMap<OWLClass, OWLClass> elems = chebi.getAllElementsWithParentsIn(metabolite_categories);
+      // The fn also gathers data for each of the elems
+      // e.g., { Sy->Synonyms, X->xref, S->SMILES, I -> InChI, Ik -> InChIKey }
+      HashMap<OWLClass, P<OWLClass, HashMap<String, EntryTypes>>> elems;
+      elems = chebi.getAllElementsWithParentsIn(metabolite_categories);
       for (OWLClass e : elems.keySet())
-        System.out.format("%s\t\t%s\n", labelFor(e, ontology), labelFor(elems.get(e), ontology));
+        output(e, elems.get(e), inchis, ontology);
+    }
+
+    private static HashMap<String, String> readInchis(IRI documentIRI) throws IOException {
+      String loc = documentIRI.getNamespace() + documentIRI.getFragment();
+      if (loc.startsWith("file:")) {
+        loc = loc.substring("file:".length());
+      } else {
+        System.err.println("Expecting inchi file as file:///absolute/path");
+        System.exit(-1);
+      }
+      BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(loc)));
+      String line;
+      HashMap<String, String> inchis = new HashMap<String, String>();
+      while ((line = in.readLine()) != null) {
+        String[] id_inc = line.split("\t");
+        String id = "CHEBI:" + id_inc[0];
+        String inchi = id_inc[1];
+        inchis.put(id, inchi);
+      }
+      return inchis;
+    }
+
+    private static void output(OWLClass elem, P<OWLClass, HashMap<String, EntryTypes>> meta, HashMap<String, String> inchis, OWLOntology ontology) {
+      OWLClass parent = meta.fst();
+      HashMap<String, EntryTypes> data = meta.snd();
+
+      String id = getID(elem);
+      String inchi = inchis.get(id);
+      if (inchi == null) {
+        // If there is no inchi we cannot merge this chemical into Act
+        // so ignore...
+        System.err.println("no inchi for " + id);
+        return;
+      }
+      JSONObject metadata = new JSONObject();
+      metadata.put("of_type", getID(parent));
+
+      // Synonym, Definition, SMILES, xref, InChI, InChIKey, 
+      // has_role, has_part, has_functional_parent, has_parent_hydride,
+      // is_conjugate_acid_of, is_conjugate_base_of, is_enantiomer_of,
+      // is_substituent_group_from, is_tautomer_of
+      JSONArray d;
+      if ((d = getMeta(data, EntryTypes.Definition)) != null && d.length() != 0)
+        metadata.put(EntryTypes.Definition.toString(), d);
+      if ((d = getMeta(data, EntryTypes.Synonym)) != null && d.length() != 0)
+        metadata.put(EntryTypes.Synonym.toString(), d);
+      if ((d = getMeta(data, EntryTypes.xref)) != null && d.length() != 0)
+        metadata.put(EntryTypes.xref.toString(), d);
+
+      // Need to output in format 
+      // CHEBI<tab><ChebiID><tab><inchi><tab><json metadata>
+      String row = "CHEBI\t";
+      row += id + "\t";
+      row += inchi + "\t";
+      row += metadata.toString();
+      System.out.println(row);
+
+      // System.out.println(labelFor(elem, ontology) + "\t" + labelFor(type, ontology));
+    }
+
+    private static String getID(OWLClass c) {
+      String frag = c.getIRI().getFragment(); // return CHEBI_25212 etc.
+      return frag.replace('_', ':');
+    }
+
+    private static JSONArray getMeta(HashMap<String, EntryTypes> map, EntryTypes typToAdd) {
+      JSONArray vals = new JSONArray();
+      for (String val : map.keySet()) {
+        if (typToAdd.equals(map.get(val)))
+          vals.put(val);
+      }
+      return vals;
     }
 
     public static void main(String[] args) throws OWLException,
           InstantiationException, IllegalAccessException,
-          ClassNotFoundException {
+          ClassNotFoundException, IOException {
       // We load an ontology from the URI specified
       IRI documentIRI = IRI.create(args[0]);
+      IRI inchiIRI = IRI.create(args[1]);
     
       String subtree = "CHEBI_25212(metabolite)";
-      if (args.length > 1)
-        subtree = args[1]; // override the default
+      if (args.length > 2)
+        subtree = args[2]; // override the default
 
-      find(subtree, documentIRI);
+      find(subtree, documentIRI, inchiIRI);
     }
 }
