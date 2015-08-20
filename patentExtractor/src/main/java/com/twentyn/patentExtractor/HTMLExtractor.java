5d100b97786a4dd846ec6536899c6643880cc546
Mark T. Daly <mark@20n.com>
2015-08-20 15:19:25 -0700

    WIP: next steps on extracting text from Google's HTML patents.

--- patentExtractor/src/main/java/com/twentyn/patentExtractor/HTMLExtractor.java
+++ patentExtractor/src/main/java/com/twentyn/patentExtractor/HTMLExtractor.java
@@ -3,6 +3,7 @@ package com.twentyn.patentExtractor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,7 +14,9 @@ public class HTMLExtractor {
     Document jsoupDoc = Jsoup.parse(inputFile, "utf-8");
     jsoupDoc.outputSettings().indentAmount(4);
     jsoupDoc.outputSettings().prettyPrint(true);
-    System.out.println(jsoupDoc.html());
+    //System.out.println(jsoupDoc.html());
+    Elements claims = jsoupDoc.body().select("div.patent-claims-section");
+    System.out.println(claims.toString());
   }
 
   public static void main(String[] args) throws Exception {
