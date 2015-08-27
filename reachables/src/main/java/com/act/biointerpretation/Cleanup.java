fa21e87c6908b7cf493ef71ff944d6b566e56277
J. Christopher Anderson <chris@20n.com>
2015-08-27 15:08:35 -0700

    work in progress checkin

--- reachables/src/main/java/com/act/biointerpretation/Cleanup.java
+++ reachables/src/main/java/com/act/biointerpretation/Cleanup.java
@@ -1,25 +1,68 @@
 package com.act.biointerpretation;
 
-import act.server.SQLInterface.DBIterator;
-import act.server.SQLInterface.MongoDB;
+import act.api.NoSQLAPI;
 import act.shared.Reaction;
+import com.ggasoftware.indigo.Indigo;
+import com.ggasoftware.indigo.IndigoInchi;
+import com.ggasoftware.indigo.IndigoObject;
+
+import java.util.Iterator;
 
 public class Cleanup {
-    private MongoDB db;
+    private static NoSQLAPI api = new NoSQLAPI();
 
-    Cleanup() {
-        this.db = new MongoDB("localhost", 27017, "actv01");
+
+    public static void main(String[] args) {
+        checkBalance();
     }
 
-    private void testIterateOverRxns() {
-        DBIterator iterator = this.db.getIteratorOverReactions(true);
-	    Reaction r;
-        while ((r = this.db.getNextReaction(iterator)) != null) {
-            System.out.format("rxn: %d: %s\n", r.getUUID(), r.getReactionName());
+    public static void checkBalance() {
+        Iterator iterator = api.getRxns();
+        while(iterator.hasNext()) {
+            try {
+                Reaction raw = (Reaction) iterator.next();
+                SimpleReaction rxn = SimpleReaction.factory(raw);
+                System.out.println("rxn:" + raw.getReactionName());
+                // balanceOne(rxn);
+            } catch(Exception err) {
+                err.printStackTrace();
+            }
+
+
         }
     }
 
-    public static void main(String[] args) {
-        new Cleanup().testIterateOverRxns();
+    private static void balanceOne(SimpleReaction rxn) throws Exception {
+        System.out.println(rxn.toString());
+        double subsrateBal = 0.0;
+        for(String inchi : rxn.substrates) {
+            Indigo indigo = new Indigo();
+            IndigoInchi iinchi = new IndigoInchi(indigo);
+
+            IndigoObject mol = iinchi.loadMolecule(inchi);
+
+
+
+            String formula = inchi.split("/")[1];
+            if(formula.equals("p+1")) {
+                formula = "H1";
+            }
+
+            String[] atomSplit = formula.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+
+            for(int i=0; i < atomSplit.length-1; i=i+2) {
+                String atom = atomSplit[i];
+                String scount = atomSplit[i+1];
+                int count = 0;
+                try {
+                    count = Integer.parseInt(scount);
+                } catch(Exception err) {
+                    err.printStackTrace();
+                }
+
+            }
+            System.out.println();
+        }
     }
+
 }
