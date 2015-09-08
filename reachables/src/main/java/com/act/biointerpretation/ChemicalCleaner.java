11d6173dfe5b3fb67ed363769679e92cb519bf4d
J. Christopher Anderson <chris@20n.com>
2015-09-08 14:07:41 -0700

    Rough out

--- reachables/src/main/java/com/act/biointerpretation/ChemicalCleaner.java
+++ reachables/src/main/java/com/act/biointerpretation/ChemicalCleaner.java
@@ -7,7 +7,9 @@ import com.ggasoftware.indigo.IndigoInchi;
 import com.ggasoftware.indigo.IndigoObject;
 
 import java.io.FileWriter;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class ChemicalCleaner {
   public static enum ChemError {
@@ -18,34 +20,66 @@ public class ChemicalCleaner {
   NoSQLAPI api;
   Indigo indigo;
   IndigoInchi iinchi;
+  Map<String, Long> existingChems;
+  long totalChemsEncountered = 0;
+  long totalChemsToDrKnow = 0;
+  long totalChemsErrorLogged = 0;
 
   public ChemicalCleaner() {
     this.api = new NoSQLAPI();
     this.indigo = new Indigo();
     this.iinchi = new IndigoInchi(this.indigo);
+    existingChems = new HashMap<>();
   }
 
-  public Chemical clean(Chemical achem) {
+  /**
+   * Checks an inchi for validity and uniqueness in the db. Returns the
+   * id of the chemical in the Dr. Know if it is already there, or successfully
+   * added.  If the inchi is a dud, returns -1
+   * @param id
+   * @returnß
+   */
+  public long clean(Long id) {
+    //Check if this id has already been cleaned up
+
+
+    //If the inchi has been seen before, return that db entry
+    Chemical achem = api.readChemicalFromInKnowledgeGraph(id);
     String inchi = achem.getInChI();
+    if(existingChems.containsKey(inchi)) {
+      return existingChems.get(inchi);
+    }
+
+    totalChemsEncountered++;
     
     // if it contains a FAKE, not a true small molecule
     // so SKIP. If it contains a &gt means bad data from
     // wikipedia, also SKIP
     if(inchi.contains("FAKE") || inchi.contains("&gt")) {
       log(achem, ChemError.BAD_INCHIS, inchi);
-      return null;
+      totalChemsErrorLogged++;
+      return -1;
     }
 
+    //Try to interpret the inchi
+    IndigoObject mol = null;
     try {
-      IndigoObject mol = iinchi.loadMolecule(inchi);
+      mol = iinchi.loadMolecule(inchi);
     } catch(Exception err) {
       // malformed inchi, so SKIP
-      log(achem, ChemError.UNRESOLVED_R, inchi);
-      return null;
+      log(achem, ChemError.BAD_INCHIS, inchi);
+      totalChemsErrorLogged++;
+      return -1;
     }
 
+    //Remove any ionization information in the inchi
+
+
     // passed the tests.. let it go through
-    return achem;
+    long out = api.writeToOutKnowlegeGraph(achem);
+    existingChems.put(inchi, out);
+    totalChemsToDrKnow++;
+    return out;
   }
 
   private void log(Chemical chem, ChemError errcode, String error) {
@@ -59,27 +93,27 @@ public class ChemicalCleaner {
   // e.g., String fname = "/home/chris/C/vmwaredata/badinchis.txt";
   //       new ChemicalCleaner().dumpBadToFile(fname);
   private void dumpBadChemicalsToFile(String file) throws Exception {
-    FileWriter writer = new FileWriter(file);
-    Iterator<Chemical> chems = api.readChemsFromInKnowledgeGraph();
-    while(chems.hasNext()) {
-      Chemical achem = clean(chems.next());
-
-      if (achem == null) 
-        continue;
-
-      String inchi = achem.getInChI();
-      String name = achem.getFirstName();
-    
-      System.out.println(inchi);
-      StringBuilder sb = new StringBuilder();
-      sb.append(inchi);
-      sb.append("\t");
-      sb.append(name);
-      sb.append("\n");
-      writer.write(sb.toString());
-    }
-
-    writer.close();
+//    FileWriter writer = new FileWriter(file);
+//    Iterator<Chemical> chems = api.readChemsFromInKnowledgeGraph();
+//    while(chems.hasNext()) {
+//      Chemical achem = clean(chems.next());
+//
+//      if (achem == null)
+//        continue;
+//
+//      String inchi = achem.getInChI();
+//      String name = achem.getFirstName();
+//
+//      System.out.println(inchi);
+//      StringBuilder sb = new StringBuilder();
+//      sb.append(inchi);
+//      sb.append("\t");
+//      sb.append(name);
+//      sb.append("\n");
+//      writer.write(sb.toString());
+//    }
+//
+//    writer.close();
   }
 
 }
--- reachables/src/main/java/com/act/biointerpretation/ReactionCleaner.java
+++ reachables/src/main/java/com/act/biointerpretation/ReactionCleaner.java
@@ -3,133 +3,134 @@ package com.act.biointerpretation;
 import act.api.NoSQLAPI;
 import act.shared.Reaction;
 
+import java.io.File;
 import java.io.FileWriter;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.Map;
+import java.util.*;
 
 import com.ggasoftware.indigo.Indigo;
 import com.ggasoftware.indigo.IndigoInchi;
 import com.ggasoftware.indigo.IndigoObject;
 
 public class ReactionCleaner {
-  public static enum RxnError {
-    SUBSTRATE_OR_PRODUCT_FELL_OUT,
-    BREAKS_CLEANUP,
-    UNBALANCED
-  }
-
-  Map<Long, Long> validChemsMapOld2New;
-  Indigo indigo;
-  IndigoInchi iinchi;
-
-
-  public ReactionCleaner() {
-    // the valid chems are not available at construction
-    // they show up later, and we set them using the
-    // setter below
-    this.validChemsMapOld2New = null; 
-    this.indigo = new Indigo();
-    this.iinchi = new IndigoInchi(this.indigo);
-  }
-      
-  public void setValidChems(Map<Long, Long> vchems) {
-    this.validChemsMapOld2New = vchems;
-  }
-
-  /**
-   * Returns null if the reaction fails cleanup. 
-   * Bad rxn is logged based on its first error
-   * Returns either the original rxn or a new one if the 
-   * data passes through the gauntlet of validation/correction
-   */
-  public Reaction clean(Reaction rxn) {
-    boolean invalid = false;
-
-    //Check the chemicals for correctness, or fix them
-    Long[] substrates = rxn.getSubstrates();
-    Long[] products = rxn.getProducts();
-
-    // check if all substrates are in the valid set
-    for (Long substrate : substrates) {
-      if (!this.validChemsMapOld2New.containsKey(substrate))
-        invalid = true;
-    }
-
-    // check if all substrates are in the valid set
-    for (Long product: products) {
-      if (!this.validChemsMapOld2New.containsKey(product))
-        invalid = true;
-    }
-
-    Long[] ss = rxn.getSubstrates();
-    Long[] mappedSubstrates = new Long[ss.length];
-    for (int i = 0; i < ss.length; i++)
-      mappedSubstrates[i] = this.validChemsMapOld2New.get(ss[i]);
-
-    Long[] ps = rxn.getProducts();
-    Long[] mappedProducts = new Long[ps.length];
-    for (int i = 0; i < ps.length; i++)
-      mappedProducts[i] = this.validChemsMapOld2New.get(ps[i]);
-
-    rxn.setSubstrates(mappedSubstrates);
-    rxn.setProducts(mappedProducts);
-
-    // return rxn if it passed the test, otherwise null
-    if (invalid) {
-      log(rxn, RxnError.SUBSTRATE_OR_PRODUCT_FELL_OUT, rxn.getReactionName());
-      return null;
-    } else {
-      return rxn;
-    }
-  }
-
-  private boolean checkBalanced(SimpleReaction rxn) throws Exception {
-    // this checks balancing based on mono isotopic mass of the 
-    // substrates and products.
-    // 
-    // See indigo documentation: https://github.com/ggasoftware/indigo/blob/master/doc/source/indigo/concepts/mass.rst
-    // and e.g., chemical http://www.chemspider.com/Chemical-Structure.370269.html
-    // that has a mass of 827.118347 Da
-    // So by summing up the masses on either side, we get to 
-    // compare if the reaction is balanced.
-
-    double subsrateBal = 0.0;
-    for (String inchi : rxn.substrates) {
-      IndigoObject mol = iinchi.loadMolecule(inchi);
-      subsrateBal += mol.monoisotopicMass();
-    }
-
-    double productBal = 0.0;
-    for (String inchi : rxn.products) {
-      IndigoObject mol = iinchi.loadMolecule(inchi);
-      productBal += mol.monoisotopicMass();
-    }
-
-    // we should not be doing equality comparisons
-    // over doubles (or floats). See links below
-    //
-    // https://randomascii.wordpress.com/2012/06/26/doubles-are-not-floats-so-dont-compare-them/
-    // https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
-    //
-    // So instead of (subsrateBal != productBal)
-    // we do difference and threshold to account for
-    // number binary representation errors
-
-    // if the masses are equal upto 15 decimal places, we're good
-    double SMALL_EPSILON = 1e-15;
-    if (Math.abs(subsrateBal - productBal) > SMALL_EPSILON) {
-      System.err.println("Not Balanced: " + rxn.toString());
-      return false;
-    }
-
-    return true;
-  }
-
-  private static void log(Reaction rxn, RxnError errcode, String error) {
-    System.err.println(errcode.toString() + ":\n\tIncoming KG ID: " + rxn.getUUID() + "\n\tIncoming KG rxn:" + error);
-    //TODO:  append a list somewhere
-  }
-
+//  public static enum RxnError {
+//    SUBSTRATE_OR_PRODUCT_FELL_OUT,
+//    BREAKS_CLEANUP,
+//    UNBALANCED
+//  }
+//
+//    static final File logDir = new File("");
+//    Indigo indigo;
+//    IndigoInchi iinchi;
+//    ChemicalCleaner chemclean = new ChemicalCleaner();
+//    Map<String, Long> existingReactions;
+//    Map<Long, Long> oldIdToNew;
+//    int reactionsEncountered = 0;
+//    int reactionsLogged = 0;
+//
+//
+//  public ReactionCleaner() {
+//    this.indigo = new Indigo();
+//    this.iinchi = new IndigoInchi(this.indigo);
+//    existingReactions = new HashMap<>();
+//    oldIdToNew = new HashMap<>();
+//  }
+//
+//  /**
+//   * Interprets a Reaction and either puts it into an error bin, or puts data into Dr. Know
+//   */
+//  public void clean(Reaction rxn) throws Exception {
+//    boolean invalid = false;
+//
+//    //Check the chemicals for correctness, or fix them
+//    Long[] substrates = rxn.getSubstrates();
+//    Long[] products = rxn.getProducts();
+//
+//    // check if all substrate chemss are ok
+//    for (Long id : substrates) {
+//        Long newid =  chemclean.clean(id);
+//        if(newid < 0) {
+//            log(rxn, RxnError.SUBSTRATE_OR_PRODUCT_FELL_OUT, id);
+//            return;
+//        }
+//    }
+//
+//    // check if all product chems are ok
+//      for (Long id : products) {
+//          Long newid =  chemclean.clean(id);
+//          if(newid < 0) {
+//              log(rxn, RxnError.SUBSTRATE_OR_PRODUCT_FELL_OUT, id);
+//              return;
+//          }
+//      }
+//
+//    Long[] ss = rxn.getSubstrates();
+//    Long[] mappedSubstrates = new Long[ss.length];
+//    for (int i = 0; i < ss.length; i++)
+//      mappedSubstrates[i] = this.validChemsMapOld2New.get(ss[i]);
+//
+//    Long[] ps = rxn.getProducts();
+//    Long[] mappedProducts = new Long[ps.length];
+//    for (int i = 0; i < ps.length; i++)
+//      mappedProducts[i] = this.validChemsMapOld2New.get(ps[i]);
+//
+//    rxn.setSubstrates(mappedSubstrates);
+//    rxn.setProducts(mappedProducts);
+//
+//    // return rxn if it passed the test, otherwise null
+//    if (invalid) {
+//      log(rxn, RxnError.SUBSTRATE_OR_PRODUCT_FELL_OUT, rxn.getReactionName());
+//      return null;
+//    } else {
+//      return rxn;
+//    }
+//  }
+//
+//  private boolean checkBalanced(SimpleReaction rxn) throws Exception {
+//    // this checks balancing based on mono isotopic mass of the
+//    // substrates and products.
+//    //
+//    // See indigo documentation: https://github.com/ggasoftware/indigo/blob/master/doc/source/indigo/concepts/mass.rst
+//    // and e.g., chemical http://www.chemspider.com/Chemical-Structure.370269.html
+//    // that has a mass of 827.118347 Da
+//    // So by summing up the masses on either side, we get to
+//    // compare if the reaction is balanced.
+//
+//    double subsrateBal = 0.0;
+//    for (String inchi : rxn.substrates) {
+//      IndigoObject mol = iinchi.loadMolecule(inchi);
+//      subsrateBal += mol.monoisotopicMass();
+//    }
+//
+//    double productBal = 0.0;
+//    for (String inchi : rxn.products) {
+//      IndigoObject mol = iinchi.loadMolecule(inchi);
+//      productBal += mol.monoisotopicMass();
+//    }
+//
+//    // we should not be doing equality comparisons
+//    // over doubles (or floats). See links below
+//    //
+//    // https://randomascii.wordpress.com/2012/06/26/doubles-are-not-floats-so-dont-compare-them/
+//    // https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
+//    //
+//    // So instead of (subsrateBal != productBal)
+//    // we do difference and threshold to account for
+//    // number binary representation errors
+//
+//    // if the masses are equal upto 15 decimal places, we're good
+//    double SMALL_EPSILON = 1e-15;
+//    if (Math.abs(subsrateBal - productBal) > SMALL_EPSILON) {
+//      System.err.println("Not Balanced: " + rxn.toString());
+//      return false;
+//    }
+//
+//    return true;
+//  }
+//
+//  private void log(Reaction rxn, RxnError errcode, String error) {
+//      reactionsLogged++;
+//    System.err.println(errcode.toString() + ":\n\tIncoming KG ID: " + rxn.getUUID() + "\n\tIncoming KG rxn:" + error);
+//    //TODO:  append a list somewhere
+//  }
 }
 
