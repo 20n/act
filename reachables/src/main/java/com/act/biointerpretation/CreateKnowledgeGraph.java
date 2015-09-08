844727d975fb2b868280318e82c1dd1bc612e70c
J. Christopher Anderson <chris@20n.com>
2015-09-08 16:03:41 -0700

    minor

--- reachables/src/main/java/com/act/biointerpretation/CreateKnowledgeGraph.java
+++ reachables/src/main/java/com/act/biointerpretation/CreateKnowledgeGraph.java
@@ -25,8 +25,8 @@ public class CreateKnowledgeGraph {
   }
 
   public static void main(String[] args) {
-    CreateKnowledgeGraph drknow = new CreateKnowledgeGraph();
-    drknow.create();
+    CreateKnowledgeGraph kg = new CreateKnowledgeGraph();
+    kg.create();
   }
 
   public void create() {
