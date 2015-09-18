cfa8a2f14ce723999b82ebf8bc33cf99393f9813
Saurabh Srivastava <saurabh@20n.com>
2015-09-17 22:01:26 -0700

    Doing SplineInterpolation and BrentSolver to move maxima peaks, we get very close to the basePeak calling numbers. Errors over the entire set of timepoints are as: 1923 Timepoints processed. Aggregate error: mz = 0.71%, intensity = 1.82%

--- reachables/src/main/java/com/act/lcms/MzMLSmoothing.java
+++ reachables/src/main/java/com/act/lcms/MzMLSmoothing.java
@@ -8,6 +8,8 @@ import com.act.lcms.LCMSXMLParser.LCMSSpectrum;
 
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
+import org.apache.commons.math3.analysis.solvers.BrentSolver;
+import org.apache.commons.math3.analysis.UnivariateFunction;
 
 public class MzMLSmoothing {
 
@@ -23,11 +25,50 @@ public class MzMLSmoothing {
     SplineInterpolator si = new SplineInterpolator();
     PolynomialSplineFunction f = si.interpolate(x, y);
 
+    // we use a BrentSolver to find the zeros of the derivative
+    UnivariateFunction d = f.derivative();
+
+    // BrentSolver solves to a certain accuracy:
+    // assuming the accuracy is on the m/z, lets go with 0.01 Da
+    double absAccuracy = 0.0001; 
+    BrentSolver solver = new BrentSolver(absAccuracy);
+    
+    int overwritten = 0;
     List<Pair<Double, Double>> interpolated = new ArrayList<>();
-    for (int i=0; i<raw.size(); i++) {
+    // add the first element as is
+    interpolated.add(raw.get(0));
+    // iterate through [1,sz-1) and see if the peak needs to be shifted
+    // it will need shifting if there is a maxima around this mz value
+    for (int i=1; i<raw.size()-1; i++) {
+      double mzBefore = raw.get(i-1).getLeft();
+      double mzAfter = raw.get(i+1).getLeft(); 
       double mz = raw.get(i).getLeft();
-      interpolated.add(Pair.of(mz, f.value(mz)));
+      double it = f.value(mz); // spline value
+
+      // check if there was a maxima in between these two nodes
+      // if yes, we need to see where the spline root is in between and 
+      // rewrite the entry for this mz
+      if (Math.signum(d.value(mzBefore)) == 1 && Math.signum(d.value(mzAfter)) == -1) {
+        // check if there is a zero close by, if yes put that as the mz value
+        // else put whatever we already see
+        int maxEval = 10000;
+        // have to provide it a range within [lowerBound, upperBound]
+        // hence the bracketing for the left and right extremes of mzs
+        double min = mzBefore;
+        double max = mzAfter;
+        double root = solver.solve(maxEval, d, min, max);
+        // System.out.format("Brent: mz: %.4f it: %.0f brent solved mz = %.4f val = %.0f\n", mz, it, root, f.value(root));
+        // set the mz to this new value, and intensity to spline.value(mz)
+        mz = root;
+        it = f.value(root);
+        overwritten++;
+      }
+
+      interpolated.add(Pair.of(mz, it));
     }
+    // add the last element as is
+    interpolated.add(raw.get(raw.size()-1));
+    // System.out.format("Of total %d peaks, %d shifted right to the maximas.", raw.size(), overwritten);
     return interpolated;
   }
 
@@ -71,7 +112,7 @@ public class MzMLSmoothing {
     if (fileName.endsWith(".mzML")) {
       Iterator<LCMSSpectrum> iter = parser.getIterator(fileName);
       int pulled = 0;
-      while (iter.hasNext() && pulled++ < howManyToValidate) 
+      while (iter.hasNext() && (howManyToValidate == -1 || pulled++ < howManyToValidate)) 
         spectrumObjs.add(iter.next());
     } else {
       String msg = "Need a .mzML file or serialized data:\n" +
@@ -98,8 +139,8 @@ public class MzMLSmoothing {
       itErr += (itE = normalizedPcError(smoothed_it, baseline_it));
       List<Pair<Double, Double>> s = smoothed.getIntensities();
       List<Pair<Double, Double>> r = raw.getIntensities();
-      for (int k=0; k<r.size(); k++)
-        System.out.format("\t%.4f\t%.0f\t%.0f\n", s.get(k).getLeft(), s.get(k).getRight(), r.get(k).getRight());
+      // for (int k=0; k<r.size(); k++)
+        // System.out.format("\t%.4f\t%.0f\t%.4f\t%.0f\n", s.get(k).getLeft(), s.get(k).getRight(), r.get(k).getLeft(), r.get(k).getRight());
       System.out.format("T: %.4f. mz_err: %.2f%% it_err: %.2f%% s_{mz,I}: {%.4f,%.0f} b_{mz,I}: {%.4f,%.0f}\n", raw.getTimeVal(), mzE*100, itE*100, smoothed_mz, smoothed_it, baseline_mz, baseline_it);
     }
     // average out the mz and intensity errors
