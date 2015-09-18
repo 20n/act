0a7ac48b53fb22153598deaee8d5eaeca436b758
Saurabh Srivastava <saurabh@20n.com>
2015-09-17 22:03:22 -0700

    Only reporting error numbers for timepoints that have > 2% error from basePeak data.

--- reachables/src/main/java/com/act/lcms/MzMLSmoothing.java
+++ reachables/src/main/java/com/act/lcms/MzMLSmoothing.java
@@ -141,7 +141,9 @@ public class MzMLSmoothing {
       List<Pair<Double, Double>> r = raw.getIntensities();
       // for (int k=0; k<r.size(); k++)
         // System.out.format("\t%.4f\t%.0f\t%.4f\t%.0f\n", s.get(k).getLeft(), s.get(k).getRight(), r.get(k).getLeft(), r.get(k).getRight());
-      System.out.format("T: %.4f. mz_err: %.2f%% it_err: %.2f%% s_{mz,I}: {%.4f,%.0f} b_{mz,I}: {%.4f,%.0f}\n", raw.getTimeVal(), mzE*100, itE*100, smoothed_mz, smoothed_it, baseline_mz, baseline_it);
+      if (mzE > 0.02 || itE > 0.02) {
+        System.out.format("T: %.4f. mz_err: %.2f%% it_err: %.2f%% s_{mz,I}: {%.4f,%.0f} b_{mz,I}: {%.4f,%.0f}\n", raw.getTimeVal(), mzE*100, itE*100, smoothed_mz, smoothed_it, baseline_mz, baseline_it);
+      }
     }
     // average out the mz and intensity errors
     mzErr /= howManyToValidate;
