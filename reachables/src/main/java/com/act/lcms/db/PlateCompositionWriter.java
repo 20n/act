d031832fa72083e730608777eefba3b6fdde72b8
Mark T. Daly <mark@20n.com>
2015-10-14 13:14:37 -0700

    PlateCompositionWriter should handle standards plates tables as well.

--- reachables/src/main/java/com/act/lcms/db/PlateCompositionWriter.java
+++ reachables/src/main/java/com/act/lcms/db/PlateCompositionWriter.java
@@ -12,16 +12,11 @@ import java.util.List;
 public class PlateCompositionWriter {
   public static final String PLATE_TYPE_96_WELLS = "plate_96_well";
   public static final List<String> PLATE_LABELS_SAMPLE_WELLS = Arrays.asList("msid", "composition", "chemical", "note");
+  public static final List<String> PLATE_LABELS_STANDARD_WELLS = Arrays.asList("chemical", "media");
 
   public void writeSampleWellComposition(Writer outputWriter, Plate plate, List<SampleWell> wells) throws IOException {
     PrintWriter w = new PrintWriter(outputWriter, true);
-    w.format(">%s\t%s\n", "name", plate.getName());
-    w.format(">%s\t%s\n", "description", plate.getDescription());
-    w.format(">%s\t%s\n", "barcode", plate.getBarcode());
-    w.format(">%s\t%s\n", "location", plate.getLocation());
-    w.format(">%s\t%s\n", "plate_type", plate.getPlateType());
-    w.format(">%s\t%d\n", "temperature", plate.getTemperature());
-    w.println();
+    writePlateAttributes(w, plate);
 
     switch (plate.getPlateType()) {
       case PLATE_TYPE_96_WELLS:
@@ -45,6 +40,43 @@ public class PlateCompositionWriter {
         throw new RuntimeException(String.format("Found unexpected plate type: %s", plate.getPlateType()));
     }
 
+  }
+
+  public void writeStandardWellComposition(Writer outputWriter, Plate plate, List<StandardWell> wells)
+      throws IOException {
+    PrintWriter w = new PrintWriter(outputWriter, true);
+    writePlateAttributes(w, plate);
+
+    switch (plate.getPlateType()) {
+      case PLATE_TYPE_96_WELLS:
+        String[][][] tables = new String[2][8][12];
+        for (StandardWell well : wells) {
+          int y = well.getPlateRow();
+          int x = well.getPlateColumn();
+          // TODO: should we use a map for this instead to make the table name -> data correspondence clearer?
+          tables[0][y][x] = well.getChemical();
+          tables[1][y][x] = well.getMedia();
+          // TODO: add notes when we expect them.
+        }
+
+        for (int i = 0; i < PLATE_LABELS_STANDARD_WELLS.size(); i++) {
+          write96CellTable(w, PLATE_LABELS_STANDARD_WELLS.get(i), tables[i]);
+        }
+        break;
+      default:
+        // TODO: use a better exception type here.
+        throw new RuntimeException(String.format("Found unexpected plate type: %s", plate.getPlateType()));
+    }
+  }
+
+  protected void writePlateAttributes(PrintWriter w, Plate plate) throws IOException {
+    w.format(">%s\t%s\n", "name", plate.getName());
+    w.format(">%s\t%s\n", "description", plate.getDescription());
+    w.format(">%s\t%s\n", "barcode", plate.getBarcode());
+    w.format(">%s\t%s\n", "location", plate.getLocation());
+    w.format(">%s\t%s\n", "plate_type", plate.getPlateType());
+    w.format(">%s\t%d\n", "temperature", plate.getTemperature());
+    w.println();
   }
 
   protected void write96CellTable(PrintWriter w, String tableName, String[][] values) {
@@ -74,14 +106,25 @@ public class PlateCompositionWriter {
   }
 
   public static void main(String[] args) throws Exception {
-    String plateBarcode = args[0];
+    String tableType = args[0];
+    String plateBarcode = args[1];
+
+    PlateCompositionWriter writer = new PlateCompositionWriter();
 
     try (DB db = new DB().connectToDB("jdbc:postgresql://localhost:10000/lcms?user=mdaly")) {
       Plate p = Plate.getPlateByBarcode(db, plateBarcode);
-      List<SampleWell> wells = SampleWell.getSampleWellsByPlateId(db, p.getId());
-
-      PlateCompositionWriter writer = new PlateCompositionWriter();
-      writer.writeSampleWellComposition(new OutputStreamWriter(System.out), p, wells);
+      switch (tableType) {
+        case "sample":
+          List<SampleWell> sampleWells = SampleWell.getSampleWellsByPlateId(db, p.getId());
+          writer.writeSampleWellComposition(new OutputStreamWriter(System.out), p, sampleWells);
+          break;
+        case "standard":
+          List<StandardWell> stdWells = StandardWell.getStandardWellsByPlateId(db, p.getId());
+          writer.writeStandardWellComposition(new OutputStreamWriter(System.out), p, stdWells);
+          break;
+        default:
+          throw new RuntimeException(String.format("Unrecognized table type: %s", tableType));
+      }
     }
   }
 }
