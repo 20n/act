c5d0fd89dca8e25004e2a30e8820bc4a3e19954d
Mark T. Daly <mark@20n.com>
2015-10-16 14:37:57 -0700

    Add method for connecting to DB with separate host, port, user, password, and db name.

--- reachables/src/main/java/com/act/lcms/db/DB.java
+++ reachables/src/main/java/com/act/lcms/db/DB.java
@@ -13,6 +13,10 @@ public class DB implements AutoCloseable {
     ERROR,
   }
 
+  public static final String DEFAULT_HOST = "localhost";
+  public static final Integer DEFAULT_PORT = 5432;
+  public static final String DEFAULT_DB_NAME = "lcms";
+
   Connection conn;
 
   public DB connectToDB(String connStr) throws ClassNotFoundException, SQLException {
@@ -20,6 +24,17 @@ public class DB implements AutoCloseable {
      * See https://jdbc.postgresql.org/documentation/94/load.html. */
     Class.forName("org.postgresql.Driver");
     this.conn = DriverManager.getConnection(connStr);
+    return this;
+  }
+
+  public DB connectToDB(String host, Integer port, String databaseName, String user, String password)
+      throws ClassNotFoundException, SQLException {
+    Class.forName("org.postgresql.Driver");
+    String url = String.format("jdbc:postgresql://%s:%d/%s",
+        host == null ? DEFAULT_HOST : host,
+        port == null ? DEFAULT_PORT : port,
+        databaseName == null ? DEFAULT_DB_NAME : databaseName);
+    this.conn = DriverManager.getConnection(url, user, password);
     return this;
   }
 
