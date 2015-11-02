52cf743db7ac8e12f07b571065f17370f35ed8c7
J. Christopher Anderson <chris@20n.com>
2015-11-02 13:50:40 -0800

    Changed example on SplitProjector to menthol
    So that when you pull this to test SplitProjector it won't give errors.
     (Tartrate is the ambiguous test case as to what it should do due to
    being mess)

--- reachables/src/main/java/com/act/biointerpretation/step3_stereochemistry/SplitProjector.java
+++ reachables/src/main/java/com/act/biointerpretation/step3_stereochemistry/SplitProjector.java
@@ -11,19 +11,19 @@ public class SplitProjector {
     public static void main(String[] args) throws Exception {
         SplitReaction.handleLicense();
 
-        //Tartrate >> monomethyl tartrate
-        String reactantInchi = "InChI=1S/C4H6O6/c5-1(3(7)8)2(6)4(9)10/h1-2,5-6H,(H,7,8)(H,9,10)/t1?,2?";
-        String productInchi = "InChI=1/C5H8O6/c1-11-5(10)3(7)2(6)4(8)9/h2-3,6-7H,1H3,(H,8,9)/t2?,3?";
-
-        String testSubstrate = "InChI=1S/C4H6O6/c5-1(3(7)8)2(6)4(9)10/h1-2,5-6H,(H,7,8)(H,9,10)/t1-,2+";
-        String expectedProduct = "";
+//        //Tartrate >> monomethyl tartrate
+//        String reactantInchi = "InChI=1S/C4H6O6/c5-1(3(7)8)2(6)4(9)10/h1-2,5-6H,(H,7,8)(H,9,10)/t1?,2?";
+//        String productInchi = "InChI=1/C5H8O6/c1-11-5(10)3(7)2(6)4(8)9/h2-3,6-7H,1H3,(H,8,9)/t2?,3?";
+//
+//        String testSubstrate = "InChI=1S/C4H6O6/c5-1(3(7)8)2(6)4(9)10/h1-2,5-6H,(H,7,8)(H,9,10)/t1-,2+";
+//        String expectedProduct = "";
 
         //menthol >> methyl menthol
-//        String reactantInchi = "InChI=1S/C10H20O/c1-7(2)9-5-4-8(3)6-10(9)11/h7-11H,4-6H2,1-3H3/t8?,9?,10?";
-//        String productInchi = "InChI=1S/C17H24O2/c1-12(2)15-10-9-13(3)11-16(15)19-17(18)14-7-5-4-6-8-14/h4-8,12-13,15-16H,9-11H2,1-3H3/t13?,15?,16?";
-//
-//        String testSubstrate = "InChI=1S/C10H20O/c1-7(2)9-5-4-8(3)6-10(9)11/h7-11H,4-6H2,1-3H3/t8-,9+,10-/m1/s1";
-//        String expectedProduct = "InChI=1S/C17H24O2/c1-12(2)15-10-9-13(3)11-16(15)19-17(18)14-7-5-4-6-8-14/h4-8,12-13,15-16H,9-11H2,1-3H3/t13-,15+,16-/m1/s1";
+        String reactantInchi = "InChI=1S/C10H20O/c1-7(2)9-5-4-8(3)6-10(9)11/h7-11H,4-6H2,1-3H3/t8?,9?,10?";
+        String productInchi = "InChI=1S/C17H24O2/c1-12(2)15-10-9-13(3)11-16(15)19-17(18)14-7-5-4-6-8-14/h4-8,12-13,15-16H,9-11H2,1-3H3/t13?,15?,16?";
+
+        String testSubstrate = "InChI=1S/C10H20O/c1-7(2)9-5-4-8(3)6-10(9)11/h7-11H,4-6H2,1-3H3/t8-,9+,10-/m1/s1";
+        String expectedProduct = "InChI=1S/C17H24O2/c1-12(2)15-10-9-13(3)11-16(15)19-17(18)14-7-5-4-6-8-14/h4-8,12-13,15-16H,9-11H2,1-3H3/t13-,15+,16-/m1/s1";
 
         SplitReaction rxn = SplitReaction.generate(reactantInchi, productInchi);
             System.out.println(rxn.toString());
