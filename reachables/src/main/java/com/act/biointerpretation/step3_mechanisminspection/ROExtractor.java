5a2edfd7d59760d681c25049571afd99b1884769
J. Christopher Anderson <chris@20n.com>
2015-11-03 19:39:37 -0800

    With this commit, it is doing BRO + orphans
    In the original description of ERO, CRO, BRO, we assumed that the
    reaction was stoichiometrically balanced.  In the cleanup, these things
    aren't balanced, so there are orphan atoms.  The previous commit just
    identified the orphans.  Now it is truly calculating the BRO -- the
    atoms whose bonds moved, but also includes the orphans.
    
    I'm not sure yet if I want to do cleanup with this abstraction, or want
    to get more specific.  What might make sense is to bring in other atoms
    on carbons, or do full ERO.  This also ignores double bond / single
    bond differences, and I should probably consider a bond as "changed" if
    the bond order changes.

--- reachables/src/main/java/com/act/biointerpretation/step3_mechanisminspection/ROExtractor.java
+++ reachables/src/main/java/com/act/biointerpretation/step3_mechanisminspection/ROExtractor.java
@@ -4,6 +4,7 @@ import chemaxon.formats.MolExporter;
 import chemaxon.formats.MolFormatException;
 import chemaxon.formats.MolImporter;
 import chemaxon.struc.MolAtom;
+import chemaxon.struc.MolBond;
 import chemaxon.struc.Molecule;
 import chemaxon.struc.RxnMolecule;
 import com.act.biointerpretation.ChemAxonUtils;
@@ -25,7 +26,12 @@ public class ROExtractor {
 //        String reaction = "OCC1OC(OC2C(O)C(O)C(O)OC2CO)C(O)C(O)C1O>>OCC1OC(OC2OC(CO)C(O)C(O)C2O)C(O)C(O)C1O";
 //        String reaction = "CCC(=O)NC>>CCC(=O)O"; //an easy case
 //        String reaction = "CCFC(C)O>>COCNCBr"; //really non-plausible reaction keeps all atoms
-        String reaction = "OC(=O)CCC(=O)O>>COOCOOCC";  //A bad case, where atom topology is problem, but count is right
+//        String reaction = "OC(=O)CCC(=O)O>>COOCOOCC";  //A bad case, where atom topology is problem, but count is right
+//        String reaction = "CCCCCC(O)C=CC1C2CC(OO2)C1CC=CCCCC(O)=O>>CCCCCC(O)C=CC1C(O)CC(=O)C1CC=CCCCC(O)=O"; //an interesting example, breaks O-O bond
+//        String reaction = "CC(C)=CCC\\C(C)=C/COP(O)(=O)OP(O)(O)=O>>CC1(C)C2CCC1(C)C(C2)OP(O)(=O)OP(O)(O)=O"; //terpenoid cyclases give crazy ros, but still "works"
+//        String reaction = "[O-]N(=O)=O>>[O-]N=O"; //Neighbors of orphan atoms also need to move
+//        String reaction = "OC(=O)C1NCC(C=C)=C1>>CCCC1CNC(C1)C(O)=O";  //neighbors of orphan atoms also need to move
+        String reaction = "OCC(OP(O)(O)=O)C(O)=O>>OC(COP(O)(O)=O)C(O)=O"; //Finds the wrong solution
 
         String ro = new ROExtractor().extract(reaction);
         System.out.println(ro);
@@ -90,10 +96,100 @@ public class ROExtractor {
                 }
             }
 
+        //Identify all atoms in the substrate that have bonds that have changed
+        Set<Integer> binMovers = new HashSet<>();
+
+            //Index out the substate's bonds
+            Map<Integer,Set<Integer>>  subBinPositionTobinPositions = new HashMap<>();
+            for(int i=0; i<sub.getBondCount(); i++) {
+                MolBond bond = sub.getBond(i);
+                int toIndex = bond.getAtom1().getAtomMap();
+                int fromIndex = bond.getAtom2().getAtomMap();
+
+                //Index in one direction of bond
+                Set<Integer> existing = subBinPositionTobinPositions.get(toIndex);
+                if(existing == null) {
+                    existing = new HashSet<>();
+                }
+                existing.add(fromIndex);
+                subBinPositionTobinPositions.put(toIndex, existing);
+
+                //Repeat in the other direction
+                existing = subBinPositionTobinPositions.get(fromIndex);
+                if(existing == null) {
+                    existing = new HashSet<>();
+                }
+                existing.add(toIndex);
+                subBinPositionTobinPositions.put(fromIndex, existing);
+            }
+
+            //Index out the products's bonds
+            Map<Integer,Set<Integer>>  prodBinPositionTobinPositions = new HashMap<>();
+            for(int i=0; i<prod.getBondCount(); i++) {
+                MolBond bond = prod.getBond(i);
+                int toIndex = bond.getAtom1().getAtomMap();
+                int fromIndex = bond.getAtom2().getAtomMap();
+
+                //Index in one direction of bond
+                Set<Integer> existing = prodBinPositionTobinPositions.get(toIndex);
+                if(existing == null) {
+                    existing = new HashSet<>();
+                }
+                existing.add(fromIndex);
+                prodBinPositionTobinPositions.put(toIndex, existing);
+
+                //Repeat in the other direction
+                existing = prodBinPositionTobinPositions.get(fromIndex);
+                if(existing == null) {
+                    existing = new HashSet<>();
+                }
+                existing.add(toIndex);
+                prodBinPositionTobinPositions.put(fromIndex, existing);
+            }
+
+            //For every bin in the sub index, see if it has any bonds not present in the product
+            Set<Integer> allBins = new HashSet<>();
+            allBins.addAll(binToSub.keySet());
+            allBins.addAll(binToProd.keySet());
+            for(int bin : allBins) {
+                Set<Integer> subBonds = subBinPositionTobinPositions.get(bin);
+                Set<Integer> prodBonds = prodBinPositionTobinPositions.get(bin);
+
+                //For orphans, add all their neighbors; if both null, ignore this bin
+                if(subBonds != null && prodBonds == null) {
+                    binMovers.addAll(subBonds);
+                    continue;
+                } else if(subBonds == null && prodBonds != null) {
+                    binMovers.addAll(prodBonds);
+                    continue;
+                } else if(subBonds == null && prodBonds == null) {
+                    continue;
+                }
+
+                //Otherwise, both are non-null, so compare them and keep the diff
+                Set<Integer> adders = new HashSet<>();
+                adders.addAll(subBonds);
+                adders.removeAll(prodBonds);
+                binMovers.addAll(adders);
+
+                adders = new HashSet<>();
+                adders.addAll(prodBonds);
+                adders.removeAll(subBonds);
+                binMovers.addAll(adders);
+            }
+
         //Remove all atoms that atom to atom match
         Set<MolAtom> subRemove = new HashSet<>();
         Set<MolAtom> prodRemove = new HashSet<>();
         for(int subIndex : atomToAtom.keySet()) {
+
+            //Don't remove if this atom is one of the bond movers
+            int binIndex = subToBin.get(subIndex);
+            if(binMovers.contains(binIndex)) {
+                continue;
+            }
+
+            //Otherwise, remove it
             int prodIndex = atomToAtom.get(subIndex);
             subRemove.add(sub.getAtom(subIndex));
             prodRemove.add(prod.getAtom(prodIndex));
