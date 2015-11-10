0a589a286bf3c89287c9401475f0bd5077ad4943
J. Christopher Anderson <chris@20n.com>
2015-11-10 10:44:57 -0800

    Ranking function of OperatorHasher working

--- reachables/src/main/java/com/act/biointerpretation/operators/OperatorHasher.java
+++ reachables/src/main/java/com/act/biointerpretation/operators/OperatorHasher.java
@@ -1,5 +1,7 @@
 package com.act.biointerpretation.operators;
 
+import chemaxon.common.util.Pair;
+import java.util.Map.Entry;
 import java.util.*;
 
 /**
@@ -33,8 +35,68 @@ public class OperatorHasher {
         prodCo.add("proton");
 
         //Index the mock data
+        hasher.index("CI", "CO.I", subCo, prodCo, 1); //Test, amide and ester are standins for inchis
+        hasher.index("CI", "CO.I", subCo, prodCo, 2); //Test, amide and ester are standins for inchis
+
         hasher.index("CN", "CO.N", subCo, prodCo, 3); //Test, amide and ester are standins for inchis
+        hasher.index("CN", "CO.N", subCo, prodCo, 4); //Test, amide and ester are standins for inchis
+        hasher.index("CN", "CO.N", subCo, prodCo, 5); //Test, amide and ester are standins for inchis
+        hasher.index("CN", "CO.N", subCo, prodCo, 6); //Test, amide and ester are standins for inchis
+        hasher.index("CN", "CO.N", subCo, prodCo, 7); //Test, amide and ester are standins for inchis
+        hasher.index("CN", "CO.N", subCo, prodCo, 8); //Test, amide and ester are standins for inchis
+
+        hasher.index("COC", "CO.C", subCo, prodCo, 9); //Test, amide and ester are standins for inchis
+        hasher.index("COC", "CO.C", subCo, prodCo, 10); //Test, amide and ester are standins for inchis
+        hasher.index("COC", "CO.C", subCo, prodCo, 11); //Test, amide and ester are standins for inchis
+        hasher.index("COC", "CO.C", subCo, prodCo, 12); //Test, amide and ester are standins for inchis
+        hasher.index("COC", "CO.C", subCo, prodCo, 13); //Test, amide and ester are standins for inchis
+
         System.out.println();
+
+        List<Entry<Pair<String,String>, Integer>> ranked = hasher.rank();
+
+        for(Entry<Pair<String,String>, Integer> entry : ranked) {
+            int count = entry.getValue();
+            Pair<String,String> pair = entry.getKey();
+            System.out.println(pair.left() + "," + pair.right() + " : " + count);
+        }
+    }
+
+    public List<Entry<Pair<String,String>, Integer>> rank() {
+        Map<Pair<String,String>,Integer> map = new HashMap<>();
+        for(String subro : megamap.keySet()) {
+            Map<String,Map<Set<Integer>,Map<Set<Integer>,Set<Integer>>>> prodros = megamap.get(subro);
+            for(String prodro : prodros.keySet()) {
+                Map<Set<Integer>,Map<Set<Integer>,Set<Integer>>> subCOs = prodros.get(prodro);
+                Pair<String,String> pair = new Pair<>(subro,prodro);
+                int count = 0;
+
+                //Count up everything below the pair
+                for(Set<Integer> subCO : subCOs.keySet()) {
+                    Map<Set<Integer>,Set<Integer>> prodCOs = subCOs.get(subCO);
+                    for(Set<Integer> prodCO : prodCOs.keySet()) {
+                        Set<Integer> rxns = prodCOs.get(prodCO);
+                        count+= rxns.size();
+                    }
+                }
+
+                //Put the count into the hashmap
+                map.put(pair, count);
+            }
+        }
+
+        //Sort the data
+        Comparator comparator = new Comparator<Map.Entry<Pair<String,String>, Integer>>() {
+            public int compare( Map.Entry<Pair<String,String>, Integer> o1, Map.Entry<Pair<String,String>, Integer> o2 ) {
+                return (o2.getValue()).compareTo( o1.getValue() );
+            }
+        };
+
+        Set<Entry<Pair<String,String>, Integer>> set = map.entrySet();
+        List<Entry<Pair<String,String>, Integer>> list = new ArrayList<Entry<Pair<String,String>, Integer>>(set);
+        Collections.sort(list, comparator);
+
+        return list;
     }
 
     public void index(String subRO, String prodRO, Set<String> subCoStr, Set<String> prodCoStr, Integer rxn) {
