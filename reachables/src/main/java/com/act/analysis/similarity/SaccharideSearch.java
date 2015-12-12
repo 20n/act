1fcfed16a6f16d1923587f9efa3aaf1df75f342c
Mark T. Daly <mark@20n.com>
2015-12-11 16:52:44 -0800

    Update saccharide search to do strict stereochemistry matching, and adjust/add more search patterns defined by Chris.

--- reachables/src/main/java/com/act/analysis/similarity/SaccharideSearch.java
+++ reachables/src/main/java/com/act/analysis/similarity/SaccharideSearch.java
@@ -6,7 +6,9 @@ import chemaxon.license.LicenseManager;
 import chemaxon.sss.SearchConstants;
 import chemaxon.sss.search.MolSearch;
 import chemaxon.sss.search.MolSearchOptions;
+import chemaxon.struc.MolAtom;
 import chemaxon.struc.Molecule;
+import chemaxon.struc.MoleculeGraph;
 import chemaxon.util.MolHandler;
 import com.act.analysis.surfactant.TSVWriter;
 import com.act.lcms.db.io.parser.TSVParser;
@@ -28,20 +30,21 @@ public class SaccharideSearch {
 
   // These live as lists to keep them ordered.
   public static final List<String> SACCHARIDE_SMARTS = new ArrayList<String>() {{
-    add("C(O)OC[C@@H]1[C@@H](O)[C@H](O)[C@@H](O)[C@H](O)O1");
     add("CO[C@H:4]1[C@H:3](O)[C@@H:2](O)[C@H:1](O)O[C@@H:5]1[CH2:6]O");
     add("CO[C@@H:3]1[C@@H:2](O)[C@H:1](O)O[C@H:5]([CH2:6]O)[C@H:4]1O");
     add("CO[CH2:6][C@H:5]1O[C@@H:1](O)[C@H:2](O)[C@@H:3](O)[C@@H:4]1O");
     add("CO[C:1][C@@:2]1(O)O[C@H:5]([CH2:6]O)[C@@H:4](O)[C@@H:3]1O");
     add("CO[CH2:6][C@H:5]1O[C@H:1](O)[C@H:2](O)[C@@H:3](O)[C@@H:4]1O");
-    add("CO[C@@H:2]1[C@@H:1](O)O[C@H:5]([C:6]O)[C@@H:4](O)[C@@H:3]1O");
-    add("CO[C@@H:3]1[C@H:2](O)[C@@H:1](O)O[C@H:5]([C:6]O)[C@H:4]1O");
-    add("CO[C:6][C@H:5]1O[C@H:1](O)[C@@H:2](O)[C@@H:3](O)[C@@H:4]1O");
+    add("CO[C@@H:2]1[C@@H:1](O)O[C@H:5]([CH2:6]O)[C@@H:4](O)[C@@H:3]1O");
+    add("CO[C@@H:3]1[C@H:2](O)[C@@H:1](O)O[C@H:5]([CH2:6]O)[C@H:4]1O");
+    add("CO[CH2:6][C@H:5]1O[C@H:1](O)[C@@H:2](O)[C@@H:3](O)[C@@H:4]1O");
+    add("CO[C@@H:3]1[C@@H:2](O)[C@H:1](O)O[C@H:5]([CH2:6]O)[C@@H:4]1O");
+    add("CO[C@@H:4]1[C@H:3](O)[C@@H:2](O)[C@H:1](O)O[C@@H:5]1[CH2:6]O");
+    add("CO[CH2:6][C@H:5]1O[C@@H:1](O)[C@H:2](O)[C@@H:3](O)[C@H:4]1O");
   }};
 
   // Names for each structure, the first eight of which are based on the paper captions.
   public static final List<String> SACCHARIDE_HEADER_FIELDS = new ArrayList<String>() {{
-    add("beta(1->6)Glc");
     add("(1-->4)b-D-Glucopyranose");
     add("(1-->3)b-D-Glucopyranose");
     add("(1-->6)b-D-Glucopyranose");
@@ -50,29 +53,35 @@ public class SaccharideSearch {
     add("(1-->2)a-D-Mannopyranose");
     add("(1-->3)a-D-Mannopyranose");
     add("(1-->6)a-D-Mannopyranose");
+    add("(1-->3)b-D-Galactopyranose");
+    add("(1-->4)b-D-Galactopyranose");
+    add("(1-->6)b-D-Galactopyranose");
   }};
 
   // From https://docs.chemaxon.com/display/jchembase/Bond+specific+search+options.
+  /*
   public static final MolSearchOptions SEARCH_OPTIONS = new MolSearchOptions(SearchConstants.SUBSTRUCTURE);
   static {
-    SEARCH_OPTIONS.setVagueBondLevel(SearchConstants.VAGUE_BOND_LEVEL4);
-  }
+    // SEARCH_OPTIONS.setVagueBondLevel(SearchConstants.VAGUE_BOND_LEVEL2);
+    SEARCH_OPTIONS.setStereoModel(SearchConstants.STEREO_MODEL_COMPREHENSIVE);
+    SEARCH_OPTIONS.setStereoSearchType(SearchConstants.STEREO_EXACT);
+  }*/
 
   private Map<String, MolSearch> saccharideSearches = new HashMap<>(SACCHARIDE_SMARTS.size());
 
   public void init() throws IOException, MolFormatException {
     for (String smarts : SACCHARIDE_SMARTS) {
+      MolSearchOptions searchOptions = new MolSearchOptions(SearchConstants.SUBSTRUCTURE);
+      searchOptions.setStereoModel(SearchConstants.STEREO_MODEL_LOCAL);
+      searchOptions.setStereoSearchType(SearchConstants.STEREO_EXACT);
       MolSearch ms = new MolSearch();
-      ms.setSearchOptions(SEARCH_OPTIONS);
+      ms.setSearchOptions(searchOptions);
       ms.setQuery(new MolHandler(smarts, true).getMolecule());
       saccharideSearches.put(smarts, ms);
     }
   }
 
   public Map<String, Double> matchVague(Molecule target) throws Exception {
-    MolSearchOptions searchOptions = new MolSearchOptions(SearchConstants.SUBSTRUCTURE);
-    searchOptions.setVagueBondLevel(SearchConstants.VAGUE_BOND_LEVEL4);
-
     Map<String, Double> results = new HashMap<>();
     for (Map.Entry<String, MolSearch> entry : saccharideSearches.entrySet()) {
       MolSearch searcher = entry.getValue();
@@ -107,6 +116,7 @@ public class SaccharideSearch {
       SaccharideSearch matcher = new SaccharideSearch();
       matcher.init();
       int rowNum = 0;
+      int ambiguousStereoChemistryCount = 0;
       for (Map<String, String> row : parser.getResults()) {
         rowNum++;
         try {
@@ -118,6 +128,21 @@ public class SaccharideSearch {
             System.err.format("Skipping molecule %d due to exception: %s\n", rowNum, e.getMessage());
             continue;
           }
+
+          boolean hasAmbiguousStereoChemistry = false;
+          for (int i = 0; i < target.getAtomCount(); i++) {
+            if (target.getChirality(i) == MoleculeGraph.PARITY_EITHER) {
+              hasAmbiguousStereoChemistry = true;
+              break;
+            }
+          }
+
+          if (hasAmbiguousStereoChemistry) {
+            ambiguousStereoChemistryCount++;
+            System.err.format("Molecule %s has ambiguous stereochemistry, skipping\n", row.get("id"));
+            continue;
+          }
+
           Map<String, Double> results = matcher.matchVague(target);
           for (int i = 0; i < SACCHARIDE_SMARTS.size(); i++) {
             row.put(SACCHARIDE_HEADER_FIELDS.get(i), String.format("%.3f", results.get(SACCHARIDE_SMARTS.get(i))));
@@ -130,6 +155,8 @@ public class SaccharideSearch {
           throw e;
         }
       }
+
+      System.err.format("Molecules with ambiguous stereochemistry: %d\n", ambiguousStereoChemistryCount);
     } finally {
       writer.close();
     }
