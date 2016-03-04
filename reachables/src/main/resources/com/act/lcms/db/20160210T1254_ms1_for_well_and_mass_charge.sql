37e4c9d77c22704060e6ae2b00cb6ee7f7d1607f
vramakrishnan <vijay@20n.com>
2016-03-04 11:56:06 -0800

    altered migration scripts

--- reachables/src/main/resources/com/act/lcms/db/20160210T1254_ms1_for_well_and_mass_charge.sql
+++ reachables/src/main/resources/com/act/lcms/db/20160210T1254_ms1_for_well_and_mass_charge.sql
@@ -3,10 +3,10 @@ CREATE TABLE ms1_for_well_and_mass_charge (
 	plate_id integer NOT NULL references plates(id),
   	plate_row integer NOT NULL,
   	plate_column integer NOT NULL,
+  	ion_mass_charge decimal NOT NULL,
   	use_snr boolean NOT NULL,
   	scan_file text NOT NULL,
-  	chemical_name text NOT NULL,
-  	metlin_ions text NOT NULL,
+  	metlin_ions bytea NOT NULL,
   	ions_to_spectra bytea NOT NULL,
   	ions_to_integral bytea NOT NULL,
   	ions_to_max bytea NOT NULL,
@@ -17,4 +17,4 @@ CREATE TABLE ms1_for_well_and_mass_charge (
   	max_y_axis decimal NOT NULL
 );
 CREATE UNIQUE INDEX idx_ms1_for_well_and_mass_charge_id ON ms1_for_well_and_mass_charge(id);
-CREATE UNIQUE INDEX idx_ms1_for_well_with_scan_file_metlin_ions ON ms1_for_well_and_mass_charge(plate_id, plate_row, plate_column, use_snr, scan_file, chemical_name, metlin_ions);
+CREATE UNIQUE INDEX idx_ms1_for_well_with_scan_file_metlin_ions ON ms1_for_well_and_mass_charge(plate_id, plate_row, plate_column, ion_mass_charge, use_snr, scan_file, metlin_ions);
