b344dfa713bb3d03a75b16aec17112c666d4abdc
vramakrishnan <vijay@20n.com>
2016-04-27 16:13:00 -0700

    solved coefficient null issue with desalting

--- reachables/src/test/java/com/act/biointerpretation/step2_desalting/ReactionDesalterTest.java
+++ reachables/src/test/java/com/act/biointerpretation/step2_desalting/ReactionDesalterTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ReactionDesalterTest {
 
@@ -197,5 +198,51 @@ public class ReactionDesalterTest {
         mockAPI.getWrittenChemicals().get(rnxSubstrateId1).getInChI(), "InChI=1S/CH2O2/c2-1-3/h1H,(H,2,3)");
     assertEquals("The reaction written to the write DB should have the desalted inchi name",
         mockAPI.getWrittenChemicals().get(rnxSubstrateId2).getInChI(), "InChI=1S/CH2O2/c2-1-3/h1H,(H,2,3)");
+  }
+
+  @Test
+  public void testDuplicateDesaltedChemicalsInSubstratesAndProductsDoNotHaveNullCoefficients() throws Exception {
+    List<Reaction> testReactions = new ArrayList<>();
+
+    Map<Long, String> idToInchi = new HashMap<>();
+
+    // The two inchis below, once desalted, map to InChI=1S/CH2O2/c2-1-3/h1H,(H,2,3)
+    idToInchi.put(1L, "InChI=1S/C36H36N4O2.Fe/c1-9-25-21(5)29-15-30-23(7)27(13-11-19(3)41)35(39-30)18-36-28(14-12-20(4)42)24(8)32(40-36)17-34-26(10-2)22(6)31(38-34)16-33(25)37-29;/h9-10,15-18H,1-2,11-14H2,3-8H3;/q-2;+7/b29-15-,30-15-,31-16-,32-17-,33-16-,34-17-,35-18-,36-18-;");
+    idToInchi.put(2L, "InChI=1S/6CN.Fe.K.H/c6*1-2;;;/q;;;;;;;+1;");
+    idToInchi.put(3L, "InChI=1S/p+1");
+
+    idToInchi.put(4L, "InChI=1S/H2O/h1H2");
+    idToInchi.put(5L, "InChI=1S/C36H36N4O2.Fe/c1-9-25-21(5)29-15-30-23(7)27(13-11-19(3)41)35(39-30)18-36-28(14-12-20(4)42)24(8)32(40-36)17-34-26(10-2)22(6)31(38-34)16-33(25)37-29;/h9-10,15-18H,1-2,11-14H2,3-8H3;/q-2;+6/b29-15-,30-15-,31-16-,32-17-,33-16-,34-17-,35-18-,36-18-;");
+    idToInchi.put(6L, "InChI=1S/6CN.Fe.3K/c6*1-2;;;;/q;;;;;;-3;3*+1");
+
+    Integer[] substrateCoefficients = {1, 1, 1};
+    Integer[] productCoefficients = {1, 1, 1};
+
+    Long[] substrates = {4L, 5L, 6L};
+    Long[] products = {1L, 2L, 3L};
+
+    Reaction testReaction =
+        utilsObject.makeTestReaction(substrates, products, substrateCoefficients, productCoefficients, true);
+
+    testReactions.add(testReaction);
+
+    MockedNoSQLAPI mockAPI = new MockedNoSQLAPI();
+    mockAPI.installMocks(testReactions, utilsObject.SEQUENCES, utilsObject.ORGANISM_NAMES, idToInchi);
+
+    NoSQLAPI mockNoSQLAPI = mockAPI.getMockNoSQLAPI();
+
+    Desalter desalter = new Desalter();
+    desalter.initReactors();
+
+    ReactionDesalter testReactionDesalter = new ReactionDesalter(mockNoSQLAPI, desalter);
+    testReactionDesalter.run();
+
+    for (Long id : mockAPI.getWrittenReactions().get(0).getSubstrates()) {
+      assertNotNull(mockAPI.getWrittenReactions().get(0).getSubstrateCoefficient(id));
+    }
+
+    for (Long id : mockAPI.getWrittenReactions().get(0).getProducts()) {
+      assertNotNull(mockAPI.getWrittenReactions().get(0).getProductCoefficient(id));
+    }
   }
 }
