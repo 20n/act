a006fb6d292d1e62bac024c5b543ca1fac950c37
vramakrishnan <vijay@20n.com>
2016-05-24 18:13:22 -0700

    implemented labelled reactions

---
+++ reachables/src/main/java/com/act/biointerpretation/mechanisminspection/LabelledReaction.java
@@ -0,0 +1,63 @@
+package com.act.biointerpretation.mechanisminspection;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Set;
+
+public class LabelledReaction {
+
+  @JsonProperty("ecnum")
+  private String ecnum;
+
+  @JsonProperty("notes")
+  private String notes;
+
+  @JsonProperty("easy_desc")
+  private String easy_desc;
+
+  @JsonProperty("products")
+  private Set<String> products;
+
+  @JsonProperty("substrates")
+  private Set<String> substrates;
+
+  public String getEcnum() {
+    return ecnum;
+  }
+
+  public void setEcnum(String ecnum) {
+    this.ecnum = ecnum;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public void setNotes(String notes) {
+    this.notes = notes;
+  }
+
+  public String getEasy_desc() {
+    return easy_desc;
+  }
+
+  public void setEasy_desc(String easy_desc) {
+    this.easy_desc = easy_desc;
+  }
+
+  public Set<String> getProducts() {
+    return products;
+  }
+
+  public void setProducts(Set<String> products) {
+    this.products = products;
+  }
+
+  public Set<String> getSubstrates() {
+    return substrates;
+  }
+
+  public void setSubstrates(Set<String> substrates) {
+    this.substrates = substrates;
+  }
+}
---
+++ reachables/src/main/java/com/act/biointerpretation/mechanisminspection/LabelledReactionsCorpus.java
@@ -0,0 +1,57 @@
+package com.act.biointerpretation.mechanisminspection;
+
+import act.server.NoSQLAPI;
+import act.shared.Reaction;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LabelledReactionsCorpus {
+  private static final String VALIDATED_REACTIONS_FILE_PATH = "validated_reactions.json";
+  private final Class INSTANCE_CLASS_LOADER = getClass();
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private List<LabelledReaction> labelledReactions;
+
+  public List<LabelledReaction> getLabelledReactions() {
+    return labelledReactions;
+  }
+
+  public void setLabelledReactions(List<LabelledReaction> labelledReactions) {
+    this.labelledReactions = labelledReactions;
+  }
+
+  public void loadCorpus() throws IOException {
+    File validatedReactionsFile = new File(INSTANCE_CLASS_LOADER.getResource(VALIDATED_REACTIONS_FILE_PATH).getFile());
+    LabelledReactionsCorpus labelledReactionsCorpus = OBJECT_MAPPER.readValue(validatedReactionsFile, LabelledReactionsCorpus.class);
+    this.setLabelledReactions(labelledReactionsCorpus.getLabelledReactions());
+  }
+
+  public boolean checkIfReactionEqualsALabelledReaction(Long rxnId, NoSQLAPI api) {
+    Reaction reaction = api.readReactionFromInKnowledgeGraph(rxnId);
+
+    Set<String> rxnSubstrates = new HashSet<>();
+    for (Long id : reaction.getSubstrates()) {
+      rxnSubstrates.add(api.readChemicalFromInKnowledgeGraph(id).getInChI());
+    }
+
+    Set<String> rxnProducts = new HashSet<>();
+    for (Long id : reaction.getProducts()) {
+      rxnProducts.add(api.readChemicalFromInKnowledgeGraph(id).getInChI());
+    }
+
+    for (LabelledReaction labelledReaction : labelledReactions) {
+      if (reaction.getECNum().equals(labelledReaction.getEcnum()) &&
+          reaction.getReactionName().equals(labelledReaction.getEasy_desc()) &&
+          labelledReaction.getProducts().equals(rxnProducts) &&
+          labelledReaction.getSubstrates().equals(rxnSubstrates)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
