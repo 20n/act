d4ffc8fca0ea64f94efef639c8744873a5585fcc
vramakrishnan <vijay@20n.com>
2016-06-03 13:24:53 -0700

    added the desalted version of shikimate

--- reachables/src/main/java/com/act/reachables/CladeTraversal.java
+++ reachables/src/main/java/com/act/reachables/CladeTraversal.java
@@ -37,7 +37,7 @@ public class CladeTraversal {
   public static final String OPTION_OUTPUT_REACTION_FILE_NAME = "r";
   public static final String OPTION_OUTPUT_FAILED_REACTIONS_DIR_NAME = "d";
   public static final String OPTION_ACT_DATA_FILE = "a";
-  public static final String PABA_INCHI = "InChI=1S/C7H10O5/c8-4-1-3(7(11)12)2-5(9)6(4)10/h1,4-6,8-10H,2H2,(H,11,12)/t4-,5-,6-/m1/s1";
+  public static final String PABA_INCHI = "InChI=1S/C7H10O5/c8-4-1-3(7(11)12)2-5(9)6(4)10/h1,4-6,8-10H,2H2,(H,11,12)";
   public static final String DEFAULT_INCHI_FILE = "Inchis.txt";
   public static final String DEFAULT_REACTIONS_FILE = "Reactions.txt";
   public static final String DEFAULT_ACTDATA_FILE = "result.actdata";
