1c18f3925d3b8ddb432dc6f47c8f4d7f2250791f
gil <gil.goldshlager-i@20n.com>
2016-06-07 10:26:32 -0700

    Adds L2MetaboliteCorpus to gather metabolites for L2 expansion from a file.

---
+++ reachables/src/main/java/com/act/biointerpretation/l2expansion/L2MetaboliteCorpus.java
@@ -0,0 +1,47 @@
+package com.act.biointerpretation.l2expansion;
+
+import chemaxon.formats.MolImporter;
+import chemaxon.struc.Molecule;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class L2MetaboliteCorpus {
+
+  private static final String METABOLITES_FILE_PATH = "PABA_metabolites.txt";
+  private final Class INSTANCE_CLASS_LOADER = getClass();
+
+  private Map<String, Molecule> corpus = new HashMap<String, Molecule>();
+
+  /*
+   * Add the chemicals in the metabolites file to the corpus as chemaxon Molecules
+   */
+  public void buildCorpus() throws FileNotFoundException, IOException {
+    BufferedReader metaboliteReader = getMetabolitesReader();
+
+    while (metaboliteReader.ready()) {
+      String InChI = metaboliteReader.readLine();
+      corpus.put(InChI, MolImporter.importMol(InChI));
+    }
+  }
+
+  /**
+   * @return reader for the list of metabolites
+   */
+  public BufferedReader getMetabolitesReader() throws FileNotFoundException {
+    File metabolitesFile = new File(INSTANCE_CLASS_LOADER.getResource(METABOLITES_FILE_PATH).getFile());
+    FileInputStream metabolitesInputStream = new FileInputStream(metabolitesFile);
+    BufferedReader metabolitesReader = new BufferedReader(new InputStreamReader(metabolitesInputStream));
+    return metabolitesReader;
+  }
+
+  public Map<String, Molecule> getCorpus() {
+    return corpus;
+  }
+}
