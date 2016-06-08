6ef88753f0e4b499e47968d31a9c2da304bc62c3
Gil Goldshlager <gil.goldshlager-i@20n.com>
2016-06-08 11:58:13 -0700

    Added command line IO to L2ExpansionDriver

--- reachables/src/main/java/com/act/biointerpretation/l2expansion/L2ExpansionDriver.java
+++ reachables/src/main/java/com/act/biointerpretation/l2expansion/L2ExpansionDriver.java
@@ -1,27 +1,103 @@
 package com.act.biointerpretation.l2expansion;
 
+import com.act.biointerpretation.analytics.ReactionCountProvenance;
 import com.act.biointerpretation.mechanisminspection.Ero;
 import com.act.biointerpretation.mechanisminspection.ErosCorpus;
+import org.apache.commons.cli.*;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Runs L2 Expansion
  */
 public class L2ExpansionDriver {
 
-  private static final String METABOLITES_FILE = "PABA_metabolites.txt";
+  private static final Logger LOGGER = LogManager.getFormatterLogger(L2Expander.class);
+
 
   //All ROs which uniquely and perfectly match reactions in the PABA clade
   private static final Set<Integer> RO_LIST = new HashSet<Integer>(Arrays.asList(358, 33, 75, 342, 357));
 
-  private static final String OUTPUT_FILE_PATH = "/mnt/shared-data/Gil/l2_predictions.json";
+  private static final String OPTION_METABOLITES = "m";
+  private static final String OPTION_OUTPUT_PATH = "o";
+  private static final String OPTION_HELP = "h";
+
+  public static final String HELP_MESSAGE = StringUtils.join(
+          "This class is used to apply every RO from an input list to every metabolite in another input list. ",
+          "It creates a list of predicted reactions, containing the substrates, reactor, and products for each " +
+                  "(RO, metabolite) pair for which a reaction is predicted to occur. ",
+          "It takes as arguments one input file name for the ROs and one for the metabolites."
+          );
+
+  public static final List<Option.Builder> OPTION_BUILDERS = new ArrayList<Option.Builder>() {{
+    add(Option.builder(OPTION_METABOLITES)
+            .argName("metabolites file name")
+            .desc("The name of the metabolites file.  File should be in " +
+                    "resources.com.act.biointerpretation.l2expansion and contain one inchi per line.")
+            .hasArg()
+            .longOpt("metabolite_file")
+    );
+    add(Option.builder(OPTION_OUTPUT_PATH)
+            .argName("file path for output")
+            .desc("A path to which to write the predicted reactions.")
+            .hasArgs()
+            .longOpt("output_path")
+    );
+    add(Option.builder("h")
+            .argName("help")
+            .desc("Prints help message")
+            .longOpt("help")
+    );
+  }};
+
+  public static final HelpFormatter HELP_FORMATTER = new HelpFormatter();
 
   public static void main(String[] args) throws Exception {
+
+    // Build command line parser
+    Options opts = new Options();
+    for (Option.Builder b : OPTION_BUILDERS) {
+      opts.addOption(b.build());
+    }
+
+    CommandLine cl = null;
+    try {
+      CommandLineParser parser = new DefaultParser();
+      cl = parser.parse(opts, args);
+    } catch (ParseException e) {
+      LOGGER.error(String.format("Argument parsing failed: %s\n", e.getMessage()));
+      HELP_FORMATTER.printHelp(ReactionCountProvenance.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
+      System.exit(1);
+    }
+
+    // Print help
+    if (cl.hasOption(OPTION_HELP)) {
+      HELP_FORMATTER.printHelp(ReactionCountProvenance.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
+      return;
+    }
+
+    // Set metabolites file
+    String METABOLITES_FILE = "PABA_metabolites";
+    if (!cl.hasOption(OPTION_METABOLITES)) {
+      LOGGER.warn("No metabolites file given; using PABA_metabolites as default.\n" +
+              "Use -m metabolites_file to specify a different file.");
+    }
+    else{
+      METABOLITES_FILE = cl.getOptionValue(OPTION_METABOLITES);
+    }
+
+    // Set output file
+    String OUTPUT_FILE_PATH = "";
+    if (!cl.hasOption(OPTION_OUTPUT_PATH)) {
+      LOGGER.error("No output path given; usinInput -r ro_file");
+      return;
+    }
+    else{
+      OUTPUT_FILE_PATH = cl.getOptionValue(OPTION_OUTPUT_PATH);
+    }
 
     //Initialize input corpuses and expander
     L2MetaboliteCorpus metaboliteCorpus = new L2MetaboliteCorpus(METABOLITES_FILE);
