ab6106f5f43ea802e2cf112010f19b0d42aadb50
Nishant <nishant.kakar-i@20n.com>
2016-06-08 17:43:13 -0700

    added command line functionality, better documentation, and other style fixes. still working on sequence object initialization and test class

--- reachables/src/main/java/com/act/utils/parser/GenbankInterpreter.java
+++ reachables/src/main/java/com/act/utils/parser/GenbankInterpreter.java
@@ -1,10 +1,17 @@
 package com.act.utils.parser;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.act.biointerpretation.BiointerpretationDriver;
+import com.act.lcms.db.io.LoadPlateCompositionIntoDB;
+import org.apache.commons.cli.*;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.biojava.nbio.core.sequence.DNASequence;
 import org.biojava.nbio.core.sequence.compound.NucleotideCompound;
 import org.biojava.nbio.core.sequence.features.AbstractFeature;
@@ -14,35 +21,53 @@ import org.biojava.nbio.core.sequence.io.GenbankReaderHelper;
 import org.biojava.nbio.core.sequence.template.AbstractSequence;
 
 public class GenbankInterpreter {
-    private static File dnaFile;
-    private static DNASequence sequence;
+    private File dnaFile;
+    private DNASequence sequence;
 
-    public GenbankInterpreter(String file) {
+    public static final String OPTION_GENBANK_PATH = "p";
+    private static final org.apache.logging.log4j.Logger LOGGER = LogManager.getFormatterLogger(BiointerpretationDriver.class);
+
+    public GenbankInterpreter(String file) throws Exception {
         dnaFile = new File(file);
+        readSequence();
     }
 
-    public static void readSequence() throws Exception {
-        LinkedHashMap<String, DNASequence> dnaSequences = GenbankReaderHelper.readGenbankDNASequence(dnaFile);
+    /**
+     * Parses the sequence object from the Genbank File
+     * @throws Exception
+     */
+    public void readSequence() throws Exception {
+        Map<String, DNASequence> dnaSequences = GenbankReaderHelper.readGenbankDNASequence(dnaFile);
+        // ADDRESS: why do DNA sequences only have 1 entry?
         if (dnaSequences.size() == 1) {
-            Object key = dnaSequences.keySet().iterator().next();
+            String key = dnaSequences.keySet().iterator().next();
+            System.out.println(key);
             sequence = dnaSequences.get(key);
         }
     }
 
-    // prints the genetic sequence of the Genbank Entry
-    public static void printSequence() throws Exception {
+    /**
+     * Prints the genetic sequence of the Genbank Entry
+     */
+    public void printSequence() {
         if (sequence == null) {
-            readSequence();
+            String msg = String.format("Class hasn't been appropriately initialized, no sequence object");
+            LOGGER.error(msg);
+            throw new RuntimeException(msg);
         }
         System.out.println("Sequence:");
         System.out.println(sequence.getSequenceAsString());
         System.out.println("\n");
     }
 
-    // prints all the Features and corresponding Qualifiers for a particular sequence
-    public static void printFeatures() throws Exception {
+    /**
+     * Prints all the Features and corresponding Qualifiers for a particular sequence
+     */
+    public void printFeatures() {
         if (sequence == null) {
-            readSequence();
+            String msg = String.format("Class hasn't been appropriately initialized, no sequence object");
+            LOGGER.error(msg);
+            throw new RuntimeException(msg);
         }
         List<FeatureInterface<AbstractSequence<NucleotideCompound>, NucleotideCompound>> features = sequence.getFeatures();
         for (FeatureInterface<AbstractSequence<NucleotideCompound>, NucleotideCompound> feature : features) {
@@ -57,36 +82,93 @@ public class GenbankInterpreter {
         }
     }
 
-    // adds a Qualifier to a particular Feature
-    public static void addQualifier(AbstractFeature<AbstractSequence<NucleotideCompound>, NucleotideCompound> feature, String key, String name, String value) {
+    /**
+     * Adds a Qualifier to a particular Feature i.e. /organism="Escherichia Coli"
+     * @param feature
+     * @param name - i.e. organism
+     * @param value - i.e. "Escherichia Coli"
+     */
+    public void addQualifier(AbstractFeature<AbstractSequence<NucleotideCompound>, NucleotideCompound> feature, String name, String value) {
         Qualifier qual = new Qualifier(name, value);
-        feature.addQualifier(key, qual);
+        feature.addQualifier(name, qual);
     }
 
-    // constructs a Feature with a particular type and source
-    public static AbstractFeature<AbstractSequence<NucleotideCompound>, NucleotideCompound> constructFeature(String type, String source) {
+    /**
+     * Constructs a Feature with a particular type (i.e. gene) and source (i.e. 1..678)
+     * @param type
+     * @param source
+     * @return - the constructed Feature object
+     */
+    public AbstractFeature<AbstractSequence<NucleotideCompound>, NucleotideCompound> constructFeature(String type, String source) {
         return new AbstractFeature<AbstractSequence<NucleotideCompound>, NucleotideCompound>(type, source) {};
     }
 
-    // once the Feature has been constructed and all the qualifiers have been added, this method adds the feature to the sequence
-    public static void addFeature(int bioStart, int bioEnd, AbstractFeature<AbstractSequence<NucleotideCompound>, NucleotideCompound> feature) throws Exception {
+    /**
+     * Once the Feature has been constructed and all the qualifiers have been added, this method adds the feature to the sequence
+     * @param bioStart
+     * @param bioEnd
+     * @param feature
+     * @throws Exception
+     */
+    public void addFeature(int bioStart, int bioEnd, AbstractFeature<AbstractSequence<NucleotideCompound>, NucleotideCompound> feature) {
         if (sequence == null) {
-            readSequence();
+            String msg = String.format("Class hasn't been appropriately initialized, no sequence object");
+            LOGGER.error(msg);
+            throw new RuntimeException(msg);
         }
         sequence.addFeature(bioStart, bioEnd, feature);
     }
 
-    public static void main(String[] args) throws Exception {
-        // tests the reading/parsing capability
-        GenbankInterpreter reader = new GenbankInterpreter("src/main/java/act/installer/genbank/ncbi.gb");
-        reader.printSequence();
-        reader.printFeatures();
+    public static final String HELP_MESSAGE = StringUtils.join(new String[]{
+            "This class parses Genbank files. It can be used on the command line with a file path as a parameter."
+    }, "");
 
-        // tests the writing capability
-        AbstractFeature<AbstractSequence<NucleotideCompound>, NucleotideCompound> feature = reader.constructFeature("type_test", "1..678");
-        reader.addQualifier(feature, "test_key", "test_name", "test_value");
-        reader.addFeature(1, 50, feature);
-        reader.printSequence();
-        reader.printFeatures();
+    public static final List<Option.Builder> OPTION_BUILDERS = new ArrayList<Option.Builder>() {{
+        add(Option.builder(OPTION_GENBANK_PATH)
+                .argName("genbank file")
+                .desc("genbank file containing sequence and annotations")
+                .hasArg()
+                .longOpt("genbank")
+        );
+    }};
+
+    public static final HelpFormatter HELP_FORMATTER = new HelpFormatter();
+
+    static {
+        HELP_FORMATTER.setWidth(100);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Options opts = new Options();
+        for (Option.Builder b : OPTION_BUILDERS) {
+            opts.addOption(b.build());
+        }
+
+        CommandLine cl = null;
+        try {
+            CommandLineParser parser = new DefaultParser();
+            cl = parser.parse(opts, args);
+        } catch (ParseException e) {
+            System.err.format("Argument parsing failed: %s\n", e.getMessage());
+            HELP_FORMATTER.printHelp(LoadPlateCompositionIntoDB.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
+            System.exit(1);
+        }
+
+        if (cl.hasOption("genbank")) {
+            String genbankFile = cl.getOptionValue("genbank");
+            if (genbankFile == null) {
+                String msg = String.format("Genbank file path is null");
+                LOGGER.error(msg);
+                throw new RuntimeException(msg);
+            }
+            else {
+                GenbankInterpreter reader = new GenbankInterpreter(genbankFile);
+            }
+        }
+        else {
+            String msg = String.format("No Genbank file path given");
+            LOGGER.error(msg);
+            throw new RuntimeException(msg);
+        }
     }
 }
