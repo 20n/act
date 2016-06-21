339062deb72b8dd4ac7f3ebdb016ccd0f9d55d03
Mark T. Daly <mark@20n.com>
2016-06-21 14:56:35 -0700

    Updates based on PR comments.

--- reachables/src/main/java/com/act/analysis/similarity/SubstructureSearch.java
+++ reachables/src/main/java/com/act/analysis/similarity/SubstructureSearch.java
@@ -12,8 +12,6 @@ import chemaxon.sss.search.MolSearchOptions;
 import chemaxon.sss.search.SearchException;
 import chemaxon.struc.Molecule;
 import chemaxon.util.MolHandler;
-import com.act.biointerpretation.desalting.ReactionDesalter;
-import com.act.lcms.db.io.LoadPlateCompositionIntoDB;
 import com.act.utils.TSVParser;
 import com.act.utils.TSVWriter;
 import org.apache.commons.cli.CommandLine;
@@ -32,17 +30,51 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 /**
- * This class is based on Chris's substructure search from the biointerpretation branch.  The list of substructures
- * is hard-coded to find molecules that might contribute to umami-enhancement behavior.
+ * This class is based on Chris's substructure search from the biointerpretation branch.
  */
 public class SubstructureSearch {
   private static final Logger LOGGER = LogManager.getFormatterLogger(SubstructureSearch.class);
+
+  // TODO: are these options sufficient?  Are there others we might want to use?
+  /* Chemaxon exposes a very non-uniform means of configuring substructure search.  Hence the mess of lambdas below.
+   * Consumer solves the Function<T, void> problem. */
+  private static final Map<String, Consumer<MolSearchOptions>> SEARCH_OPTION_ENABLERS =
+      Collections.unmodifiableMap(new HashMap<String, Consumer<MolSearchOptions>>() {{
+        put("CHARGE_MATCHING_EXACT", (so -> so.setChargeMatching(SearchConstants.CHARGE_MATCHING_EXACT)));
+        put("CHARGE_MATCHING_IGNORE", (so -> so.setChargeMatching(SearchConstants.CHARGE_MATCHING_IGNORE)));
+        put("IMPLICIT_H_MATCHING_ENABLED", (so -> so.setImplicitHMatching(SearchConstants.IMPLICIT_H_MATCHING_ENABLED)));
+        put("IMPLICIT_H_MATCHING_DISABLED", (so -> so.setImplicitHMatching(SearchConstants.IMPLICIT_H_MATCHING_DISABLED)));
+        put("IMPLICIT_H_MATCHING_IGNORE", (so -> so.setImplicitHMatching(SearchConstants.IMPLICIT_H_MATCHING_IGNORE)));
+        put("STEREO_EXACT", (so -> so.setStereoSearchType(SearchConstants.STEREO_EXACT)));
+        put("STEREO_IGNORE", (so -> so.setStereoSearchType(SearchConstants.STEREO_IGNORE)));
+        put("STEREO_MODEL_COMPREHENSIVE", (so -> so.setStereoModel(SearchConstants.STEREO_MODEL_COMPREHENSIVE)));
+        put("STEREO_MODEL_GLOBAL", (so -> so.setStereoModel(SearchConstants.STEREO_MODEL_GLOBAL)));
+        put("STEREO_MODEL_LOCAL", (so -> so.setStereoModel(SearchConstants.STEREO_MODEL_LOCAL)));
+        put("TAUTOMER_SEARCH_ON", (so -> so.setTautomerSearch(SearchConstants.TAUTOMER_SEARCH_ON)));
+        put("TAUTOMER_SEARCH_OFF", (so -> so.setTautomerSearch(SearchConstants.TAUTOMER_SEARCH_OFF)));
+        put("TAUTOMER_SEARCH_ON_IGNORE_TAUTOMERSTEREO",
+            (so -> so.setTautomerSearch(SearchConstants.TAUTOMER_SEARCH_ON_IGNORE_TAUTOMERSTEREO)));
+        put("VAGUE_BOND_OFF", (so -> so.setVagueBondLevel(SearchConstants.VAGUE_BOND_OFF)));
+        put("VAGUE_BOND_LEVEL_HALF", (so -> so.setVagueBondLevel(SearchConstants.VAGUE_BOND_LEVEL_HALF)));
+        put("VAGUE_BOND_LEVEL1", (so -> so.setVagueBondLevel(SearchConstants.VAGUE_BOND_LEVEL1)));
+        put("VAGUE_BOND_LEVEL2", (so -> so.setVagueBondLevel(SearchConstants.VAGUE_BOND_LEVEL2)));
+        put("VAGUE_BOND_LEVEL3", (so -> so.setVagueBondLevel(SearchConstants.VAGUE_BOND_LEVEL3)));
+        put("VAGUE_BOND_LEVEL4", (so -> so.setVagueBondLevel(SearchConstants.VAGUE_BOND_LEVEL4)));
+      }});
+  public static final List<String> VALID_SEARCH_OPTION_SORTED;
+  static {
+    List<String> keys = new ArrayList<>(SEARCH_OPTION_ENABLERS.keySet());
+    Collections.sort(keys);
+    VALID_SEARCH_OPTION_SORTED = Collections.unmodifiableList(keys);
+  }
 
   public static final String OPTION_INPUT_FILE = "f";
   public static final String OPTION_INPUT_DB = "d";
@@ -51,6 +83,7 @@ public class SubstructureSearch {
   public static final String OPTION_OUTPUT_FILE = "o";
   public static final String OPTION_QUERY = "q";
   public static final String OPTION_LICENSE_FILE = "l";
+  public static final String OPTION_SEARCH_OPTIONS = "x";
 
   public static final String FIELD_INCHI = "inchi";
   public static final String FIELD_ID = "id";
@@ -110,6 +143,14 @@ public class SubstructureSearch {
         .hasArg()
         .longOpt("license")
     );
+    add(Option.builder(OPTION_SEARCH_OPTIONS)
+        .argName("search options")
+        .desc(String.format("Options to supply to the substructure search.  See " +
+            "https://www.chemaxon.com/jchem/doc/dev/java/api/chemaxon/sss/SearchConstants.html " +
+            "for explanations.  Valid options are: %s", StringUtils.join(VALID_SEARCH_OPTION_SORTED, ", "))).
+        hasArgs().valueSeparator(',').
+        longOpt("search-opts")
+    );
 
     // Everybody needs a little help from their friends.
     add(Option.builder("h")
@@ -119,20 +160,28 @@ public class SubstructureSearch {
     );
   }};
 
+  MolSearch ms = new MolSearch();
   // From https://docs.chemaxon.com/display/jchembase/Bond+specific+search+options.
-  public static final MolSearchOptions SEARCH_OPTIONS = new MolSearchOptions(SearchConstants.SUBSTRUCTURE);
-  // TODO: consider adding options for search options like VAGUE_BOND_LEVEL4, STEREO_MODEL_LOCAL, and STEREO_EXACT.
+  private final MolSearchOptions searchOptions = new MolSearchOptions(SearchConstants.SUBSTRUCTURE);
 
-  MolSearch ms = null;
+  public void init(String smilesQuery, List<String> extraOpts)
+      throws IllegalArgumentException, IOException, MolFormatException {
+    // Apply all the specified extra search options using the key -> function mapping above.
+    for (String opt : extraOpts) {
+      if (!SEARCH_OPTION_ENABLERS.containsKey(opt)) {
+        throw new IllegalArgumentException(String.format("Unrecognized search option: %s", opt));
+      }
+      SEARCH_OPTION_ENABLERS.get(opt).accept(searchOptions);
+    }
 
-  public void init(String smilesQuery) throws IOException, MolFormatException {
-    ms = new MolSearch();
-    ms.setSearchOptions(SEARCH_OPTIONS);
+    ms.setSearchOptions(searchOptions);
     ms.setQuery(new MolHandler(smilesQuery, true).getMolecule());
   }
 
-  public boolean matchVague(Molecule target) throws SearchException {
+  public boolean matchSubstructure(Molecule target) throws SearchException {
     ms.setTarget(target);
+    /* hits are arrays of atom ids in the target that matched the query.  If multiple sites in the target matched,
+     * then there should be multiple arrays of atom ids (but we don't care since we're just looking for any match). */
     int[][] hits = ms.findAll();
     if (hits != null) {
       for (int i = 0; i < hits.length; i++) {
@@ -156,12 +205,12 @@ public class SubstructureSearch {
       cl = parser.parse(opts, args);
     } catch (ParseException e) {
       System.err.format("Argument parsing failed: %s\n", e.getMessage());
-      HELP_FORMATTER.printHelp(LoadPlateCompositionIntoDB.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
+      HELP_FORMATTER.printHelp(SubstructureSearch.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
       System.exit(1);
     }
 
     if (cl.hasOption("help")) {
-      HELP_FORMATTER.printHelp(ReactionDesalter.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
+      HELP_FORMATTER.printHelp(SubstructureSearch.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
       return;
     }
 
@@ -169,12 +218,31 @@ public class SubstructureSearch {
       LicenseManager.setLicenseFile(cl.getOptionValue(OPTION_LICENSE_FILE));
     }
 
+    List<String> searchOpts = Collections.emptyList();
+    if (cl.hasOption(OPTION_SEARCH_OPTIONS)) {
+      searchOpts = Arrays.asList(cl.getOptionValues(OPTION_SEARCH_OPTIONS));
+    }
+
+    // Make sure we can initialize correctly before opening any file handles for writing.
+    SubstructureSearch matcher = new SubstructureSearch();
+    try {
+      matcher.init(cl.getOptionValue(OPTION_QUERY), searchOpts);
+    } catch (IllegalArgumentException e) {
+      System.err.format("Unable to initialize substructure search.  %s\n", e.getMessage());
+      HELP_FORMATTER.printHelp(SubstructureSearch.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
+      System.exit(1);
+    } catch (MolFormatException e) {
+      System.err.format("Invalid SMILES structure query.  %s\n", e.getMessage());
+      HELP_FORMATTER.printHelp(SubstructureSearch.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
+      System.exit(1);
+    }
+
     Pair<List<String>, Iterator<Map<String, String>>> iterPair = null;
     if (cl.hasOption(OPTION_INPUT_FILE)) {
       File inFile = new File(cl.getOptionValue(OPTION_INPUT_FILE));
       if (!inFile.exists()) {
         System.err.format("File at %s does not exist", inFile.getAbsolutePath());
-        HELP_FORMATTER.printHelp(LoadPlateCompositionIntoDB.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
+        HELP_FORMATTER.printHelp(SubstructureSearch.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
         System.exit(1);
       }
       iterPair = iterateOverTSV(inFile);
@@ -186,7 +254,7 @@ public class SubstructureSearch {
       );
     } else {
       System.err.format("Must specify either input TSV file or input DB from which to read.\n");
-      HELP_FORMATTER.printHelp(LoadPlateCompositionIntoDB.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
+      HELP_FORMATTER.printHelp(SubstructureSearch.class.getCanonicalName(), HELP_MESSAGE, opts, null, true);
       System.exit(1);
     }
 
@@ -196,8 +264,6 @@ public class SubstructureSearch {
     LOGGER.info("Seaching for substructure '%s'", cl.getOptionValue(OPTION_QUERY));
 
     try {
-      SubstructureSearch matcher = new SubstructureSearch();
-      matcher.init(cl.getOptionValue(OPTION_QUERY));
       int rowNum = 0;
       while (iterPair.getRight().hasNext()) {
         Map<String, String> row = iterPair.getRight().next();
@@ -211,13 +277,13 @@ public class SubstructureSearch {
             LOGGER.warn("Skipping molecule %d due to exception: %s\n", rowNum, e.getMessage());
             continue;
           }
-          // Don't output if not a match.
-          if (!matcher.matchVague(target)) {
+          if (matcher.matchSubstructure(target)) {
+            writer.append(row);
+            writer.flush();
+          } else {
+            // Don't output if not a match.
             LOGGER.debug("Found non-matching molecule: %s", inchi);
-            continue;
           }
-          writer.append(row);
-          writer.flush();
         } catch (SearchException e) {
           LOGGER.error("Exception on input line %d: %s\n", rowNum, e.getMessage());
           throw e;
