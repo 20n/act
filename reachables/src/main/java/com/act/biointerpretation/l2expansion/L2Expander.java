d0caca042e048704267bac2eadaab9927fc3b923
vramakrishnan <vijay@20n.com>
2016-06-21 10:46:34 -0700

    removed third debugger

--- reachables/src/main/java/com/act/biointerpretation/l2expansion/L2Expander.java
+++ reachables/src/main/java/com/act/biointerpretation/l2expansion/L2Expander.java
@@ -189,9 +189,6 @@ public class L2Expander {
         System.out.println(String.format("Counter2 value is: %d", counter2));
 
         for (Map.Entry<Chemical, Molecule> chemToMol2 : inchiToMoleculeFull.entrySet()) {
-          counter3++;
-          System.out.println(String.format("Counter3 value is: %d", counter3));
-
           if (roIdToChemicalIds.get(ro.getId()).contains(chemToMol1.getKey().getUuid()) &&
               roIdToChemicalIds.get(ro.getId()).contains(chemToMol2.getKey().getUuid())) {
             Molecule[] substrates = new Molecule[2];
