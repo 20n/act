d01745df012fdda18bbe3c0c775ca8ce265974c6
vramakrishnan <vijay@20n.com>
2016-06-22 16:29:15 -0700

    added writer

--- reachables/src/main/java/com/act/reachables/ConditionalReachabilityInterpreter.java
+++ reachables/src/main/java/com/act/reachables/ConditionalReachabilityInterpreter.java
@@ -1,7 +1,11 @@
 package com.act.reachables;
 
 import act.server.NoSQLAPI;
+import com.act.utils.TSVWriter;
+import org.apache.commons.collections4.map.HashedMap;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -37,9 +41,9 @@ public class ConditionalReachabilityInterpreter {
 //    List<Long> roots = conditionalReachabilityInterpreter.getRoots(parents, children);
   }
 
-  private Map<Long, Set<Long>> constructParentToChildrenAssociations() {
-
+  private Map<Long, Set<Long>> constructParentToChildrenAssociations() throws IOException {
     Set<Long> rootLevelChemicals = new HashSet<>();
+
     Map<Long, Set<Long>> parentToChildrenAssociations = new HashMap<>();
     for (Map.Entry<Long, Long> childIdToParentId : this.actData.getActTree().parents.entrySet()) {
       Long parentId = childIdToParentId.getValue();
@@ -58,9 +62,52 @@ public class ConditionalReachabilityInterpreter {
       childIds.add(childId);
     }
 
+    Map<Long, Set<Long>> rootToAllDescendants = new HashMap<>();
     for (Long id : rootLevelChemicals) {
-      System.out.println(id);
+      Set<Long> children = parentToChildrenAssociations.get(id);
+      while (children.size() > 0) {
+        Set<Long> descendants = rootToAllDescendants.get(id);
+        if (descendants == null) {
+          descendants = new HashSet<>();
+          rootToAllDescendants.put(id, descendants);
+        }
+        descendants.addAll(children);
+
+        Set<Long> newChildren = new HashSet<>();
+        for (Long child : children) {
+          Set<Long> res = parentToChildrenAssociations.get(child);
+          if (res != null) {
+            newChildren.addAll(res);
+          }
+        }
+
+        children = newChildren;
+      }
     }
+
+    List<String> header = new ArrayList<>();
+    header.add("Target Inchi");
+    header.add("Input Inchi");
+
+    TSVWriter<String, String> writer = new TSVWriter<>(header);
+    writer.open(new File("result.tsv"));
+
+    for (Map.Entry<Long, Set<Long>> rootToDescendants : rootToAllDescendants.entrySet()) {
+      String rootInchi = db.readChemicalFromInKnowledgeGraph(rootToDescendants.getKey()).getInChI();
+
+      if (rootInchi.equals("InChI=1S/C10H16N5O13P3/c11-8-5-9(13-2-12-8)15(3-14-5)10-7(17)6(16)4(26-10)1-25-30(21,22)28-31(23,24)27-29(18,19)20/h2-4,6-7,10,16-17H,1H2,(H,21,22)(H,23,24)(H2,11,12,13)(H2,18,19,20)/t4-,6-,7-,10-/m1/s1")) {
+        continue;
+      }
+
+      for (Long descendant : rootToDescendants.getValue()) {
+        Map<String, String> res = new HashMap<>();
+        res.put(db.readChemicalFromInKnowledgeGraph(descendant).getInChI(), rootInchi);
+        writer.append(res);
+        writer.flush();
+      }
+    }
+
+    writer.close();
 
     return parentToChildrenAssociations;
   }
