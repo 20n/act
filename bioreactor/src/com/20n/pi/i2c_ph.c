e442ee97bdb46bebc3617b667bb6cc32fe09c4a6
Mark T. Daly <mark@20n.com>
2016-06-22 10:33:57 -0700

    WIP: trying to get the Pi to talk I2C.  Probably going to abandon WiringPi.

--- bioreactor/src/com/20n/pi/i2c_ph.c
+++ bioreactor/src/com/20n/pi/i2c_ph.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <errno.h>
 #include <readline/readline.h>
+#include <signal.h>
 
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
@@ -28,7 +29,16 @@ int write_string(int fd, char* str);
 int read_string(int fd, char* buf, int max_len);
 int read_int(int fd);
 
+static void handle_sigint(int sig) {
+  fprintf(stderr, "Exiting.");
+}
+
 int main(int argc, char** argv) {
+  if (signal(SIGINT, handle_sigint) == SIG_ERR) {
+    fprintf(stderr, "Unable to install interrupt handler, exiting.\n");
+    return 1;
+  }
+
   int fd = wiringPiI2CSetup(I2C_ADDRESS);
 
   if (fd == -1) {
@@ -40,13 +50,24 @@ int main(int argc, char** argv) {
   printf("Set up wiringPi with dev id %d, resulting fd number is %d\n", I2C_ADDRESS, fd);
 
   char* input = NULL;
+  char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+  if (buffer == NULL) {
+    fprintf(stderr, "Unable to allocate buffer for reading, exiting.\n");
+    return 1;
+  }
+
   do {
     // readline() mallocs new string buffers with each line, so free before reading.
     if (input != NULL) {
       free(input);
     }
     input = readline(PROMPT);
-    if (strcmp("exit", input) == 0 && strcmp("quit", input) == 0) {
+    // input should be null if the user types ctrl-d.
+    if (input == NULL) {
+      break;
+    }
+
+    if (strcmp("exit", input) == 0 || strcmp("quit", input) == 0) {
       break;
     }
 
@@ -69,11 +90,11 @@ int main(int argc, char** argv) {
     }
 
     if (input[0] == 'r' || input[0] == 'R') {
-      delay(1000); // Delay 1000 ms for a pH reading.
+      delay(2000); // Delay 1000 ms for a pH reading.
     } else if (input[0] == 'c' || input[0] == 'C') {
-      delay(1800); // Calibration ops need 1.8s to complete.
+      delay(2000); // Calibration ops need 1.8s to complete.
     } else {
-      delay(300); // Delay 300 ms for all other operations.
+      delay(1000); // Delay 300 ms for all other operations.
     }
 
     int status = read_int(fd);
@@ -89,21 +110,41 @@ int main(int argc, char** argv) {
         printf("Action failed\n");
         break;
       case RX_READY:
+        printf("Action ready\n");
+        buffer[0] = '\0';
+        int i;
+        for (i = 0; i < BUFFER_SIZE; i++) {
+          int iv = wiringPiI2CRead(fd);
+          fprintf(stderr, " < %d\n", iv);
+          char c = (char) iv;
+          buffer[i] = c;
+          if (c == '\0') {
+            break;
+          }
+        }
+        printf("  <[(%d)] %s\n", i, buffer);
         // TODO: do better than this simple reporting.
         break;
     }
 
+    /*
     if (status == RX_READY) {
-      char buffer[BUFFER_SIZE];
+      buffer[0] = '\0';
       int bytes_read = read_string(fd, buffer, BUFFER_SIZE);
       printf("  <[(%d)] %s\n", bytes_read, buffer);
     }
+    */
 
   } while (input != NULL);
 
   if (input != NULL) {
     free(input);
   }
+  if (buffer != NULL) {
+    free(buffer);
+  }
+
+  printf("Exiting.\n");
 
   // TOOD: continue here.
   return 0;
@@ -116,10 +157,11 @@ int main(int argc, char** argv) {
 int write_char(int fd, char c) {
   int res, ci;
   ci = (int) c;
+  fprintf(stderr, "Writing %d\n", ci);
   res = wiringPiI2CWrite(fd, ci);
-  if (res != 1) {
-    fprintf(stderr, "wiringPiI2CWrite returned unexpected result code: %d\n", res);
-  }
+  //if (res != 1) {
+  //  fprintf(stderr, "wiringPiI2CWrite returned unexpected result code: %d\n", res);
+  //}
   return res;
 }
 
@@ -135,7 +177,9 @@ int write_string(int fd, char* str) {
 }
 
 char read_char(int fd) {
-  return (char) wiringPiI2CRead(fd);
+  int res = wiringPiI2CRead(fd);
+  fprintf(stderr, " < %d\n", res);
+  return (char) res;
 }
 
 int read_int(int fd) {
