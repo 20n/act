578306bdb4093cadc3063fb8d43722d25789c78d
thomas-20n <thomas@20n.com>
2016-06-21 17:58:26 -0700

    Added instance variable

--- reachables/src/main/java/act/installer/bing/BingSearchResults.java
+++ reachables/src/main/java/act/installer/bing/BingSearchResults.java
@@ -8,11 +8,13 @@ import com.mongodb.BasicDBObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.HttpStatus;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -57,9 +59,13 @@ public class BingSearchResults {
   private static ObjectMapper mapper = new ObjectMapper();
 
   private BingCacheMongoDB bingCacheMongoDB;
+  private BasicHttpClientConnectionManager basicConnManager;
+  private HttpClientContext context;
 
   public BingSearchResults() {
     bingCacheMongoDB = new BingCacheMongoDB("localhost", MONGO_PORT, BING_CACHE_MONGO_DATABASE);
+    basicConnManager = new BasicHttpClientConnectionManager();
+    context = HttpClientContext.create();
   }
 
   /** This function gets the account key located on the NAS
