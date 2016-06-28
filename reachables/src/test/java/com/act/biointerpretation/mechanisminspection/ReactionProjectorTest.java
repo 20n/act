ff28c2210d78b68b936219f718de7c6bd04c1547
vramakrishnan <vijay@20n.com>
2016-06-28 12:07:30 -0700

    removed test

--- reachables/src/test/java/com/act/biointerpretation/mechanisminspection/ReactionProjectorTest.java
+++ reachables/src/test/java/com/act/biointerpretation/mechanisminspection/ReactionProjectorTest.java
@@ -193,35 +193,4 @@ public class ReactionProjectorTest {
     Assert.assertNull("The products from the projector should be null", products);
 
   }
-
-  @Test
-  public void testFastProjectionOfTwoSubstrateRoOntoTwoMolecules() throws Exception {
-    String testRO =
-        "[C:1](=[O:7])1[C,c:2]=[C,c:3][C:4](=[O:8])[c:5][c:6]1.[C:9](H)[C:10](H)>>[c:1]([OH:7])1[c:2][c:3][c:4]([OH:8])[c:5][c:6]1.[C:9]=[C:10]";
-
-    String substrate1 = "InChI=1S/C10H6O2/c11-9-5-6-10(12)8-4-2-1-3-7(8)9/h1-6H";
-    String substrate2 = "InChI=1S/C6H8N2/c7-8-6-4-2-1-3-5-6/h1-5,8H,7H2";
-
-    String[] substratesCombination = new String[] {
-        substrate1,
-        substrate2
-    };
-
-    Molecule[] molSubstrates = new Molecule[substratesCombination.length];
-    int counter = 0;
-    for (String substrate : substratesCombination) {
-      Molecule mol = MolImporter.importMol(substrate, "inchi");
-      Cleaner.clean(mol, 2);
-      molSubstrates[counter] = mol;
-      counter++;
-    }
-
-    Reactor reactor = new Reactor();
-    reactor.setReactionString(testRO);
-
-    List<Molecule[]> products = ReactionProjector.fastProjectionOfTwoSubstrateRoOntoTwoMolecules(molSubstrates, reactor);
-    for (Molecule[] prod : products) {
-      int j = 0;
-    }
-  }
 }
