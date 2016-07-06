2f52ae8d3a11f71c407008698f71e082407e598e
MichaelLampe20n <Michael@20n.com>
2016-07-06 14:05:08 -0700

    Formatting

--- reachables/src/main/scala/com/act/analysis/proteome/sequences/DNA.scala
+++ reachables/src/main/scala/com/act/analysis/proteome/sequences/DNA.scala
@@ -8,7 +8,7 @@ object DNA {
   // DNA -> RNA conversion
   private val characterConversions = Map(('a', 'u'), ('c', 'g'), ('t', 'a'), ('g', 'c'))
   private val characterConversionsComplement = Map(('a', 'a'), ('g', 'g'), ('t', 'u'), ('c', 'c'))
-  
+
   // Complement is default true because this is how most files are supplied
   def translate(sequence: String, complement: Boolean = true): String = {
     require(sequence.length > 0, message = "Sequence must be of a length of at least 1.")
