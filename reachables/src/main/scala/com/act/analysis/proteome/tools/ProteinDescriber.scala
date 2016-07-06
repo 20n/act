9fa8b4933b48fdc3331275693fbf58d86cdaf52c
MichaelLampe20n <Michael@20n.com>
2016-07-06 08:58:59 -0700

    Takes in a protein sequence and outputs biochemical statistics.  Things such as pI, Molecular Weight, expected half-life, among other things can be generally estimated based on sequence alone and could be useful for protein characterization

---
+++ reachables/src/main/scala/com/act/analysis/proteome/tools/ProteinDescriber.scala
@@ -0,0 +1,8 @@
+package com.act.analysis.proteome.tools
+
+/**
+  * Takes in a protein sequence and outputs a physio-chemical characterization of the protein based on known equations
+  */
+object ProteinDescriber {
+
+}
