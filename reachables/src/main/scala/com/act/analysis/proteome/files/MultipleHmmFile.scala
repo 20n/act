6153f1c2e7a5396e82579c84c2d8a717a6212ae5
MichaelLampe20n <Michael@20n.com>
2016-07-06 15:12:43 -0700

    Code review update on MultipleHmmFile.  Deletion of ProteinFile as it currently has no utility.
    Primary updates to MultipleHmmFile:
    
    1) Changed import style to no _
    2) Hmm regex now only matches at the end
    3) HMM is not filled on class creation, but when we write the individual files to files.

--- reachables/src/main/scala/com/act/analysis/proteome/files/MultipleHmmFile.scala
+++ reachables/src/main/scala/com/act/analysis/proteome/files/MultipleHmmFile.scala
@@ -1,6 +1,6 @@
 package com.act.analysis.proteome.files
 
-import java.io._
+import java.io.{File, PrintWriter}
 
 import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
@@ -9,14 +9,27 @@ import scala.collection.mutable.ListBuffer
   * Used to take a HMM file with multiple HMM profiles and extract each individually,
   * placing them in a directory as individual files.
   *
-  * @param sourceDirectory - The directory where we will find the HMM file by the name of hmmFileName
-  * @param hmmFileName     - Name of the actual HMM file
+  * The basic unit of a HMM File is as shown below
+  * <Header Elements>
+  * <HMM Information>
+  * <Ending character (//)>
+  *
+  * This is then repeated for each HMM in the file.
+  *
+  * @param sourceDirectory The directory where we will find the HMM file by the name of hmmFileName
+  * @param hmmFileName     Name of the actual HMM file
   */
 class MultipleHmmFile(var sourceDirectory: String, hmmFileName: String) {
-  // Add trailing slash for nice file pathing
-  if (!sourceDirectory.endsWith("/")) sourceDirectory = sourceDirectory + "/"
-  val hmms = readMultipleHmmFile()
-  val hmmPrefixName = hmmFileName.replace(".hmm", "") + "/"
+  val hmmPrefixName = hmmFileName.replaceAll("\\.hmm$", "")
+
+  /**
+    * Takes all the HMMs this class owns and maps them to individual files
+    */
+  def writeIndividualHmmsToFiles(): Unit = {
+    // Writes the files to the location of the original HMM file, but in a directory
+    val hmms = read()
+    hmms.map(x => writeHmmToFile(x))
+  }
 
   /**
     * Takes the source directory indicated on class creation and reads a
@@ -24,12 +37,12 @@ class MultipleHmmFile(var sourceDirectory: String, hmmFileName: String) {
     *
     * @return List of tuples of (Pfam Name, HMM Information)
     */
-  def readMultipleHmmFile(): List[(String, String)] = {
+  def read(): List[(String, String)] = {
     /**
       * Needs to be tail recursive otherwise will almost always stack overflow given large files
       *
-      * @param lines  - The lines still left in the file to process
-      * @param buffer - A buffer we add processed files to. This is needed to make things tail recursive
+      * @param lines  The lines still left in the file to process
+      * @param buffer A buffer we add processed files to. This is needed to make things tail recursive
       * @return A list buffer containing all the HMMs we could find in the file.
       */
     @tailrec
@@ -40,20 +53,24 @@ class MultipleHmmFile(var sourceDirectory: String, hmmFileName: String) {
       var header = lines.next()
       val (currentHmm, leftovers) = lines.span(x => !x.startsWith("//"))
 
-      /*
-       The double slash indicates the end of the HMM, so we get rid of it when we find it in the header.
-       The first element does not contain the double slash,
-       so we need to catch this case and keep the header if it isn't the double slash.
-        */
-      if (header == "//") header = "" else header = header + "\n"
 
-      val currentHmmList = List() ++ currentHmm
+      // Current HMM is an iterator, this allows us to save the rest of the file while we look for interesting things.
+      val currentHmmList = currentHmm.toList
 
       // Unique ID for protein family will be the file name.
       val keyword = "ACC"
       // Processes the keyword and pretties the string up so it is just the protein family
-      val hmmKeyword = currentHmmList.find(x => x.startsWith(keyword)).mkString.split(keyword)(1).trim()
+      val hmmKeywordMaybe = currentHmmList.find(x => x.startsWith(keyword))
 
+      val hmmKeyword = if (hmmKeywordMaybe.isDefined) hmmKeywordMaybe.get.split(keyword)(1).trim
+      else "ProteinAtFilePosition_" + buffer.length
+
+      /*
+     The double slash indicates the end of the HMM, so we get rid of it when we find it in the header.
+     The first element does not contain the double slash,
+     so we need to catch this case and keep the header if it isn't the double slash.
+      */
+      if (header == "//") header = "" else header = header + "\n"
       buffer.append((hmmKeyword, s"$header${currentHmmList.mkString(sep = "\n")}\n//"))
 
       // Keep recursively adding to the buffer
@@ -61,15 +78,8 @@ class MultipleHmmFile(var sourceDirectory: String, hmmFileName: String) {
     }
 
     // Recursively parse and add to a dynamic buffer.  Convert to immutable list in the end
-    parse(scala.io.Source.fromFile(sourceDirectory + hmmFileName).getLines(), new ListBuffer[(String, String)]()).toList
-  }
-
-  /**
-    * Takes all the HMMs this class owns and maps them to individual files
-    */
-  def writeIndividualHmmsToFiles(): Unit = {
-    // Writes the files to the location of the original HMM file, but in a directory
-    hmms.map(x => writeHmmToFile(x))
+    parse(scala.io.Source.fromFile(new File(sourceDirectory, hmmFileName)).getLines(),
+      new ListBuffer[(String, String)]()).toList
   }
 
   /**
@@ -77,14 +87,16 @@ class MultipleHmmFile(var sourceDirectory: String, hmmFileName: String) {
     * @param hmmTuple An individual tuple as composed from parse. Tuple = (Pfam name, HMM information)
     */
   def writeHmmToFile(hmmTuple: (String, String)) {
-    val filePath = s"$sourceDirectory$hmmPrefixName${hmmTuple._1}.hmm"
-
     // Need to make sure the file path exists.
-    val file = new File(filePath)
-    file.getParentFile.mkdirs()
+    val storageDirectory = new File(sourceDirectory, hmmPrefixName)
+    storageDirectory.mkdirs()
+
+    val file = new File(storageDirectory.getAbsolutePath, hmmTuple._1 + ".hmm")
+
 
     /*
-    TODO Appears scala doesn't have try with resources, so it might be worthwhile to rewrite this as a normal try clause with a finally to close the writer to ensure it gets closed.
+    TODO Appears scala doesn't have try with resources, so it might be worthwhile to
+    rewrite this as a normal try clause with a finally to close the writer to ensure it gets closed.
      */
     val writer = new PrintWriter(file)
     writer.write(hmmTuple._2)
