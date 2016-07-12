88ad12f3aa4e16908431769762f822604e9c9d6a
vramakrishnan <vijay@20n.com>
2016-07-11 17:04:14 -0700

    got jackson deserialization of date to work

--- bioreactor/src/main/java/com/twentyn/bioreactor/pH/ControlSystem.java
+++ bioreactor/src/main/java/com/twentyn/bioreactor/pH/ControlSystem.java
@@ -14,7 +14,7 @@ import org.joda.time.DateTime;
 
 public class ControlSystem {
 
-  private static final Integer TOTAL_DURATION_OF_RUN_IN_MILLISECONDS = 10000;
+  private static final Integer TOTAL_DURATION_OF_RUN_IN_MILLISECONDS = 60000;
   private static final String SENSOR_READING_FILE_LOCATION = "/tmp/sensors/v1/pH/reading.json";
   private ObjectMapper OBJECT_MAPPER = new ObjectMapper();
   private static final Logger LOGGER = LogManager.getFormatterLogger(ControlSystem.class);
@@ -56,15 +56,15 @@ public class ControlSystem {
     DateTime currTime = new DateTime();
 
     while (currTime.getMillis() - startTime.getMillis() < TOTAL_DURATION_OF_RUN_IN_MILLISECONDS) {
-
       try {
         Double phValue = readPHValue();
+        LOGGER.info("PH value is %d", phValue);
 
         if (phValue > TARGET_PH) {
+          LOGGER.info("Pump more solution");
           pumpEnablePin.high();
           Thread.sleep(1000);
         }
-
 
       } catch (IOException e) {
         LOGGER.error("Could not read pH value due to IOException. Error is %s:", e.getMessage());
@@ -81,61 +81,5 @@ public class ControlSystem {
   public static void main(String[] args) throws Exception {
     ControlSystem controlSystem = new ControlSystem();
     controlSystem.run();
-
-//    System.out.println("<--Pi4J--> GPIO Control Example ... started.");
-//
-//    // create gpio controller
-//    final GpioController gpio = GpioFactory.getInstance();
-//
-//    // provision gpio pin #01 as an output pin and turn on
-//    final GpioPinDigitalOutput pin11 =
-//        gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "Motor", PinState.LOW);
-//
-//    final GpioPinDigitalOutput pin12 =
-//        gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Motor1", PinState.LOW);
-//
-//    final GpioPinDigitalOutput pin7 =
-//        gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "Motor2", PinState.LOW);
-//
-//    // set shutdown state for pins
-//    pin11.setShutdownOptions(true, PinState.LOW);
-//    pin12.setShutdownOptions(true, PinState.LOW);
-//    pin7.setShutdownOptions(true, PinState.LOW);
-//
-//    pin12.high();
-//    pin7.high();
-//
-//    System.out.println("--> GPIO state should be: Motor running");
-//
-//    Thread.sleep(1000);
-//
-//    // turn off gpio pin #01
-//    pin7.low();
-//    System.out.println("--> GPIO state should be: Motor stopped");
-//
-//    Thread.sleep(1000);
-//
-//    // toggle the current state of gpio pin #01 (should turn on)
-//    pin7.toggle();
-//    System.out.println("--> GPIO state should be: Motor running");
-//
-//    Thread.sleep(1000);
-//
-//    // toggle the current state of gpio pin #01  (should turn off)
-//    pin7.toggle();
-//    System.out.println("--> GPIO state should be: Motor stopped");
-//
-//    Thread.sleep(1000);
-//
-//    // // turn on gpio pin #01 for 1 second and then off
-//    // System.out.println("--> GPIO state should be: ON for only 1 second");
-//    // pin7.pulse(1000, true); // set second argument to 'true' use a blocking call
-//
-//    pin7.low();
-//    Thread.sleep(500);
-//
-//    // stop all GPIO activity/threads by shutting down the GPIO controller
-//    // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-//    gpio.shutdown();
   }
 }
