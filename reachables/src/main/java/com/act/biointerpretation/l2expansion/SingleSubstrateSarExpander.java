97a297b0545e4d34c60762a249aa586b2c2a01a9
Gil Goldshlager <gil.goldshlager-i@20n.com>
2016-07-13 13:43:51 -0700

    Added single substrate sar expander

--- reachables/src/main/java/com/act/biointerpretation/l2expansion/SingleSubstrateSarExpander.java
+++ reachables/src/main/java/com/act/biointerpretation/l2expansion/SingleSubstrateSarExpander.java
@@ -10,39 +10,45 @@ import com.act.biointerpretation.mechanisminspection.Ero;
 import com.act.biointerpretation.mechanisminspection.ErosCorpus;
 import com.act.biointerpretation.sars.CharacterizedGroup;
 import com.act.biointerpretation.sars.Sar;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
-public class SarExpander {
+public class SingleSubstrateSarExpander implements L2Expander {
 
-  private static final Logger LOGGER = LogManager.getFormatterLogger(SarExpander.class);
+  private static final Logger LOGGER = LogManager.getFormatterLogger(SingleSubstrateSarExpander.class);
 
   final Iterable<CharacterizedGroup> sarGroups;
   final Iterable<String> inchis;
   final ErosCorpus roCorpus;
-  final ReactionProjector projector;
+  final PredictionGenerator generator;
 
-  public SarExpander(Iterable<CharacterizedGroup> sarGroups,
-                     Iterable<String> inchis,
-                     ErosCorpus roCorpus,
-                     ReactionProjector projector) {
+  public SingleSubstrateSarExpander(Iterable<CharacterizedGroup> sarGroups,
+                                    Iterable<String> inchis,
+                                    ErosCorpus roCorpus,
+                                    PredictionGenerator generator) {
     this.sarGroups = sarGroups;
     this.inchis = inchis;
     this.roCorpus = roCorpus;
-    this.projector = projector;
+    this.generator = generator;
   }
 
-  public L2PredictionCorpus buildPredictionCorpus() throws ReactionException {
-    Map<Integer, Reactor> roIdToReactor = new HashMap<>();
+  public L2PredictionCorpus getPredictions() {
+
+    Map<Integer, Ero> roIdToRo = new HashMap<>();
     for (Ero ro : roCorpus) {
-      roIdToReactor.put(ro.getId(), getReactor(ro));
+      roIdToRo.put(ro.getId(), ro);
     }
+
+    L2PredictionCorpus result = new L2PredictionCorpus();
 
     for (CharacterizedGroup sarGroup : sarGroups) {
       Sar sar = sarGroup.getSar();
@@ -52,25 +58,23 @@ public class SarExpander {
 
         for (Integer roId : roIds) {
           try {
-            Reactor reactor = roIdToReactor.get(roId);
-            Molecule substrate = getMolecule(inchi);
-            Map<Molecule[], List<Molecule[]>> subToProd =
-                projector.getRoProjectionMap(new Molecule[] {substrate}, reactor);
+            Ero ro = roIdToRo.get(roId);
+            List<Molecule> substrates = Arrays.asList(getMolecule(inchi));
+
+            result.addAll(generator.getPredictions(substrates, ro, Optional.of(sar)));
 
           } catch (IOException e) {
+            // If there is an error on a certain RO, metabolite pair, we should log the error, but the the expansion may
+            // produce some valid results, so no error is thrown.
             LOGGER.warn("Error projecting RO %d on substrate %s", roId, inchi);
-            continue;
+          } catch (ReactionException e) {
+            LOGGER.warn("Error projecting RO %d on substrate %s", roId, inchi);
           }
         }
       }
     }
-    return new L2PredictionCorpus();
-  }
 
-  private Reactor getReactor(Ero ro) throws ReactionException {
-    Reactor reactor = new Reactor();
-    reactor.setReactionString(ro.getRo());
-    return reactor;
+    return result;
   }
 
   private Molecule getMolecule(String inchi) throws MolFormatException {
