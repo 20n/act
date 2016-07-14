bf13e77b2b4163d620a518980117ba17f381a082
thomas-20n <thomas@20n.com>
2016-07-14 12:17:47 -0700

    Added DOSensor / DOSensorData classes

---
+++ bioreactor/src/main/java/com/twentyn/bioreactor/pH/DOSensorData.java
@@ -0,0 +1,26 @@
+package com.twentyn.bioreactor.pH;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.joda.time.DateTime;
+
+public class DOSensorData {
+
+  @JsonProperty("dissolved_oxygen")
+  private Double dissolvedOxygen;
+
+  @JsonProperty("saturation_percentage")
+  private Double saturationPercentage;
+
+  @JsonProperty("device")
+  private String deviceName;
+
+  @JsonProperty("timestamp")
+  private DateTime timeOfReading;
+
+  public DOSensorData(Double dissolvedOxygen, Double saturationPercentage, String deviceName, DateTime timeOfReading) {
+    this.dissolvedOxygen = dissolvedOxygen;
+    this.saturationPercentage = saturationPercentage;
+    this.deviceName = deviceName;
+    this.timeOfReading = timeOfReading;
+  }
+}
