472ebba832dccf2bd1768415c9edceaf15724460
thomas-20n <thomas@20n.com>
2016-07-14 12:53:56 -0700

    Moved Sensor related classes to their own package

--- bioreactor/src/main/java/com/twentyn/bioreactor/sensors/Sensor.java
+++ bioreactor/src/main/java/com/twentyn/bioreactor/sensors/Sensor.java
@@ -1,4 +1,4 @@
-package com.twentyn.bioreactor.pH;
+package com.twentyn.bioreactor.sensors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
