da42fde083080b8fa9edba1f253d51b69b6f2a5a
Mark T. Daly <mark@20n.com>
2016-07-27 16:07:25 -0700

    Add missing dependencies post-rebase.

--- reachables/src/main/java/act/installer/pubchem/PubchemTTLMerger.java
+++ reachables/src/main/java/act/installer/pubchem/PubchemTTLMerger.java
@@ -1,6 +1,15 @@
 package act.installer.pubchem;
 
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
