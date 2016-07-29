fe900cd002776c30209c851a16b8daaf184caa22
Gil Goldshlager <gil.goldshlager-i@20n.com>
2016-07-29 15:32:19 -0700

    Add logging and mass threshold

--- reachables/src/main/java/com/act/biointerpretation/l2expansion/L2Expander.java
+++ reachables/src/main/java/com/act/biointerpretation/l2expansion/L2Expander.java
@@ -34,7 +34,13 @@ public abstract class L2Expander {
   public L2PredictionCorpus getPredictions() {
     L2PredictionCorpus result = new L2PredictionCorpus();
 
+    int counter = 0;
     for (PredictionSeed seed : getPredictionSeeds()) {
+      if (counter % 1000 == 0) {
+        LOGGER.info("Processed %d seeds", counter);
+      }
+      counter++;
+
       // Apply reactor to substrate if possible
       try {
         result.addAll(generator.getPredictions(seed));
@@ -66,8 +72,27 @@ public abstract class L2Expander {
       }
     }
 
-    LOGGER.info("Proceeding with %d %d substrate ROs.", nSubstrateReactions.size(), n);
+    LOGGER.info("Proceeding with %d substrate ROs.", nSubstrateReactions.size(), n);
     return nSubstrateReactions;
+  }
+
+  protected List<Molecule> getMolecules(List<String> inchis, Integer massCutoff) {
+    List<Molecule> result = new ArrayList<>();
+    for (String inchi : inchis) {
+      try {
+        Molecule mol = importMolecule(inchi);
+        if (mol.getMass() > massCutoff) {
+          LOGGER.warn("Throwing out molecule %s because of mass %f and %d atoms.", inchi, mol.getMass(), mol.getAtomCount());
+          continue;
+        }
+        result.add(importMolecule(inchi));
+      } catch (MolFormatException e) {
+        LOGGER.error("MolFormatException on metabolite %s. %s", inchi, e.getMessage());
+        continue;
+      }
+    }
+    LOGGER.info("Imported %d inchis.", result.size());
+    return result;
   }
 
   /**
@@ -77,6 +102,7 @@ public abstract class L2Expander {
    * @return The resulting Molecule.
    * @throws MolFormatException
    */
+
   protected Molecule importMolecule(String inchi) throws MolFormatException {
     return MolImporter.importMol(inchi, INCHI_IMPORT_SETTINGS);
   }
