4d395d608126bcd03fb722c85e63f60636648124
Gil Goldshlager <gil.goldshlager-i@20n.com>
2016-07-29 18:14:36 -0700

    Add workflow for clustering results of a corpus

--- reachables/src/main/java/com/act/biointerpretation/sars/sartrees/LibMcsClustering.java
+++ reachables/src/main/java/com/act/biointerpretation/sars/sartrees/LibMcsClustering.java
@@ -3,12 +3,27 @@ package com.act.biointerpretation.sars.sartrees;
 import chemaxon.clustering.LibraryMCS;
 import chemaxon.formats.MolFormatException;
 import chemaxon.formats.MolImporter;
+import chemaxon.reaction.ReactionException;
+import chemaxon.reaction.Reactor;
 import chemaxon.struc.Molecule;
+import com.act.biointerpretation.l2expansion.L2PredictionCorpus;
+import com.act.biointerpretation.mechanisminspection.ErosCorpus;
+import com.act.biointerpretation.sars.CharacterizedGroup;
+import com.act.biointerpretation.sars.OneSubstrateSubstructureSar;
+import com.act.biointerpretation.sars.Sar;
+import com.act.biointerpretation.sars.SarCorpus;
+import com.act.biointerpretation.sars.SerializableReactor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LibMcsClustering {
 
@@ -20,7 +35,7 @@ public class LibMcsClustering {
   private static final String INCHI_IMPORT_SETTINGS = "inchi";
 
 
-  public SarTree buildSarTree(LibraryMCS libMcs, List<String> inchiList) throws InterruptedException, IOException {
+  public static SarTree buildSarTree(LibraryMCS libMcs, Collection<String> inchiList) throws InterruptedException, IOException {
 
     for (String inchi : inchiList) {
       try {
@@ -44,11 +59,39 @@ public class LibMcsClustering {
     return sarTree;
   }
 
-  public Molecule importMolecule(String inchi) throws MolFormatException {
+  public static Molecule importMolecule(String inchi) throws MolFormatException {
     return MolImporter.importMol(inchi, INCHI_IMPORT_SETTINGS);
   }
 
   public static void main(String[] args) throws Exception {
+    L2PredictionCorpus predictionCorpus = L2PredictionCorpus.readPredictionsFromJsonFile(new File(PREDICTIONS_FILE));
+    ErosCorpus roCorpus = new ErosCorpus();
+    roCorpus.loadValidationCorpus();
 
+    Map<String, L2PredictionCorpus> corpusesByRos = predictionCorpus.splitCorpus(prediction -> prediction.getProjectorName());
+    Map<String, SarCorpus> sarCorpusesByRo = new HashMap<>();
+
+    for (String projectorName : corpusesByRos.keySet()) {
+      sarCorpusesByRo.put(projectorName, getSarCorpus(projectorName, corpusesByRos.get(projectorName), roCorpus));
+    }
+  }
+
+  public static SarCorpus getSarCorpus(String projectorName, L2PredictionCorpus predictionCorpus, ErosCorpus roCorpus) throws ReactionException, IOException, InterruptedException {
+    Integer roId = Integer.parseInt(projectorName);
+    Reactor reactor = roCorpus.getEro(roId).getReactor();
+    SerializableReactor serReactor = new SerializableReactor(reactor, roId);
+
+    SarTree sarTree = buildSarTree(new LibraryMCS(), predictionCorpus.getUniqueProductInchis());
+
+    SarCorpus sarCorpus = new SarCorpus();
+    for (SarTreeNode node : sarTree.getNodes()) {
+      Molecule substructure = node.getSubstructure();
+      List<Sar> sarContainer = Arrays.asList(new OneSubstrateSubstructureSar(substructure));
+      String name = node.getHierarchyId();
+      CharacterizedGroup group = new CharacterizedGroup(name, sarContainer, serReactor);
+      sarCorpus.addCharacterizedGroup(group);
+    }
+
+    return sarCorpus;
   }
 }
