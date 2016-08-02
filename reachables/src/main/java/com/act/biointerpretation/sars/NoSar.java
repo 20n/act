0aa4deee25684470ac975a6afdf8d4d000c127a5
Mark T. Daly <mark@20n.com>
2016-08-01 23:20:00 -0700

    Add missing class.

---
+++ reachables/src/main/java/com/act/biointerpretation/sars/NoSar.java
@@ -0,0 +1,15 @@
+package com.act.biointerpretation.sars;
+
+import chemaxon.struc.Molecule;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class NoSar implements Sar, Serializable {
+  private static final long serialVersionUID = -7309106064265051106L;
+
+  @Override
+  public boolean test(List<Molecule> substrates) {
+    return true;
+  }
+}
