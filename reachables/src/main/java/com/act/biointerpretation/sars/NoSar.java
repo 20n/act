bd7f277284d790e24060fdb1be26d16ae2d65930
Mark T. Daly <mark@20n.com>
2016-08-01 23:32:48 -0700

    Maybe the no-SAR class's lack of fields is creating a problem?

--- reachables/src/main/java/com/act/biointerpretation/sars/NoSar.java
+++ reachables/src/main/java/com/act/biointerpretation/sars/NoSar.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class NoSar implements Sar, Serializable {
   private static final long serialVersionUID = -7309106064265051106L;
 
+  Molecule substructure = null;
+
   @Override
   public boolean test(List<Molecule> substrates) {
     return true;
