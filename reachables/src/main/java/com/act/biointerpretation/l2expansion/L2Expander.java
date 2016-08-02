49b1cc79f1b39b357a609a14e2d799e81d8697c0
Mark T. Daly <mark@20n.com>
2016-08-01 18:01:42 -0700

    WIP: trying to fix a jackson bug.

--- reachables/src/main/java/com/act/biointerpretation/l2expansion/L2Expander.java
+++ reachables/src/main/java/com/act/biointerpretation/l2expansion/L2Expander.java
@@ -20,12 +20,12 @@ import java.util.List;
 
 public abstract class L2Expander {
 
-  private static final Logger LOGGER = LogManager.getFormatterLogger(L2Expander.class);
+  private static transient final Logger LOGGER = LogManager.getFormatterLogger(L2Expander.class);
 
-  private static final String INCHI_IMPORT_SETTINGS = "inchi";
+  private static transient final String INCHI_IMPORT_SETTINGS = "inchi";
 
   // This SAR accepts every substrate
-  protected static final Sar NO_SAR = new Sar() {
+  protected static transient final Sar NO_SAR = new Sar() {
     @Override
     public boolean test(List<Molecule> substrates) {
       return true;
