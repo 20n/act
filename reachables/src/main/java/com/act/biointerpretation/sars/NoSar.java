00e05d48f1f41b767d16e897cfbf6efe9221ebed
Mark T. Daly <mark@20n.com>
2016-08-01 23:40:19 -0700

    Maybe an explicit property will help?

--- reachables/src/main/java/com/act/biointerpretation/sars/NoSar.java
+++ reachables/src/main/java/com/act/biointerpretation/sars/NoSar.java
@@ -1,6 +1,7 @@
 package com.act.biointerpretation.sars;
 
 import chemaxon.struc.Molecule;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 public class NoSar implements Sar, Serializable {
   private static final long serialVersionUID = -7309106064265051106L;
 
+  @JsonProperty("substructure")
   Molecule substructure = null;
 
   @Override
