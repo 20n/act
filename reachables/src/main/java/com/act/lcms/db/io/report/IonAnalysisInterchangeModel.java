4509506ba760d8de8c1afb58f8859d3c58686f4d
vramakrishnan <vijay@20n.com>
2016-08-02 14:48:08 -0700

    took down snr threshold

--- reachables/src/main/java/com/act/lcms/db/io/report/IonAnalysisInterchangeModel.java
+++ reachables/src/main/java/com/act/lcms/db/io/report/IonAnalysisInterchangeModel.java
@@ -75,8 +75,8 @@ public class IonAnalysisInterchangeModel {
       List<HitOrMiss> model2Mols = model2.getResults().get(i).getMolecules();
 
       for (int j = 0; j < model1Mols.size(); j++) {
-        if (model1Mols.get(j).getIntensity() > 10000.0 && model1Mols.get(j).getSnr() > 10000.0 && model1Mols.get(j).getTime() > 15.0 &&
-            model2Mols.get(j).getIntensity() > 10000.0 && model2Mols.get(j).getSnr() > 10000.0 && model2Mols.get(j).getTime() > 15.0) {
+        if (model1Mols.get(j).getIntensity() > 10000.0 && model1Mols.get(j).getSnr() > 1000.0 && model1Mols.get(j).getTime() > 15.0 &&
+            model2Mols.get(j).getIntensity() > 10000.0 && model2Mols.get(j).getSnr() > 1000.0 && model2Mols.get(j).getTime() > 15.0) {
           resultSet.add(model1Mols.get(j).getInchi());
         }
       }
