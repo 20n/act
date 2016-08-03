a79a9577d65a3970bb34d1e8c2819a76e3f66add
Mark T. Daly <mark@20n.com>
2016-08-02 18:00:12 -0700

    Whoops, didn't realize f"" is required for format strings.  Cleanup and logging improvements too.

--- reachables/src/main/java/com/act/biointerpretation/sars/NoSar.java
+++ reachables/src/main/java/com/act/biointerpretation/sars/NoSar.java
@@ -6,6 +6,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * This trivial class represents a SAR with no constraints.
+ *
+ * Warning: this class has been structured to play nicely with Jackson.  While making it its own top-level class may
+ * seem excessive and its single `substructure` field pointless, this gives Jackson a means of dispatching its
+ * serializers/deserializers that keeps the library happy.  This is particularly important when using the L2Expander
+ * with Spark: Spark jobs will crash if Jackson doesn't know how to process the expander's default NO_SAR value.
+ */
 public class NoSar implements Sar, Serializable {
   private static final long serialVersionUID = -7309106064265051106L;
 
