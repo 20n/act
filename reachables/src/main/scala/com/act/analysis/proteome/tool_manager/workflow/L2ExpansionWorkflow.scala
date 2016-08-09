425b1c4d2ec712630615cac29813c30d1905ac51
vramakrishnan <vijay@20n.com>
2016-08-08 18:51:40 -0700

    listify

--- reachables/src/main/scala/com/act/analysis/proteome/tool_manager/workflow/L2ExpansionWorkflow.scala
+++ reachables/src/main/scala/com/act/analysis/proteome/tool_manager/workflow/L2ExpansionWorkflow.scala
@@ -7,16 +7,16 @@ import org.apache.commons.cli.CommandLine
 class L2ExpansionWorkflow extends Workflow {
   def defineWorkflow(commandLine: CommandLine): Job = {
 
-    val command = "/usr/local/software/spark-1.5.2-bin-hadoop2.6/bin/spark-submit " +
+    val command:String = "/usr/local/software/spark-1.5.2-bin-hadoop2.6/bin/spark-submit " +
       "--driver-class-path /home/vijay/act/reachables/act/reachables/target/scala-2.10/reachables-assembly-0.1.jar " +
       "--class com.act.biointerpretation.l2expansion.SparkSingleSubstrateROProjector " +
       "--master spark://10.0.20.19:7077 --deploy-mode client --executor-memory 4G " +
       "/home/vijay/act/reachables/act/reachables/target/scala-2.10/reachables-assembly-0.1.jar " +
       "--substrates-list /home/vijay/act/reachables/output_inchis.txt -s -o /home/vijay/output_spark/ " +
-      "-l /mnt/shared-data/3rdPartySoftware/Chemaxon/license_Start-up.cxl".split(" ").toList
+      "-l /mnt/shared-data/3rdPartySoftware/Chemaxon/license_Start-up.cxl"
 
     // Print working directory
-    val job1 = ShellWrapper.shellCommand(command)
+    val job1 = ShellWrapper.shellCommand(command.split(" ").toList)
 
     // See which files are available
     val job2 = ShellWrapper.shellCommand(List("ls"))
