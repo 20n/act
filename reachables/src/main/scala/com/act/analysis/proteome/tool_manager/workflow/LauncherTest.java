77c3277ca4251e71559375d2a5d76e8256684dd4
vramakrishnan <vijay@20n.com>
2016-08-09 16:06:29 -0700

    added stuff

--- reachables/src/main/scala/com/act/analysis/proteome/tool_manager/workflow/LauncherTest.java
+++ reachables/src/main/scala/com/act/analysis/proteome/tool_manager/workflow/LauncherTest.java
@@ -1,19 +1,23 @@
 package com.act.analysis.proteome.tool_manager.workflow;
 
 import org.apache.spark.launcher.SparkLauncher;
+import org.apache.spark.deploy.SparkSubmit;
 
 public class LauncherTest {
 
   public static void main(String[] args) throws Exception {
-    Process spark = new SparkLauncher()
-        .setSparkHome("/usr/local/software/spark-1.5.2-bin-hadoop2.6")
-        .setAppResource("/home/vijay/act/reachables/target/scala-2.10/reachables-assembly-0.1.jar")
-        .setMainClass("com.act.biointerpretation.l2expansion.SparkSingleSubstrateROProjector")
-        .setMaster("local")
-        .setDeployMode("client")
-        .addAppArgs(args)
-        .launch();
 
-    spark.waitFor();
+    SparkSubmit.main(args);
+
+//    Process spark = new SparkLauncher()
+//        .setSparkHome("/usr/local/software/spark-1.5.2-bin-hadoop2.6")
+//        .setAppResource("/home/vijay/act/reachables/target/scala-2.10/reachables-assembly-0.1.jar")
+//        .setMainClass("com.act.biointerpretation.l2expansion.SparkSingleSubstrateROProjector")
+//        .setMaster("local")
+//        .setDeployMode("client")
+//        .addAppArgs(args)
+//        .launch();
+//
+//    spark.waitFor();
   }
 }
