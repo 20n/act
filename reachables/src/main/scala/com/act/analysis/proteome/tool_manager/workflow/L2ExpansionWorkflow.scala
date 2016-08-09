f665f8505c4409a80a1d6b468a18b08aaa30a3fc
vramakrishnan <vijay@20n.com>
2016-08-09 14:54:37 -0700

    added stuff

--- reachables/src/main/scala/com/act/analysis/proteome/tool_manager/workflow/L2ExpansionWorkflow.scala
+++ reachables/src/main/scala/com/act/analysis/proteome/tool_manager/workflow/L2ExpansionWorkflow.scala
@@ -3,6 +3,7 @@ package com.act.analysis.proteome.tool_manager.workflow
 import com.act.analysis.proteome.tool_manager.jobs.Job
 import com.act.analysis.proteome.tool_manager.tool_wrappers.ShellWrapper
 import org.apache.commons.cli.CommandLine
+import org.apache.spark.launcher.SparkLauncher
 
 class L2ExpansionWorkflow extends Workflow {
   def defineWorkflow(commandLine: CommandLine): Job = {
@@ -15,8 +16,22 @@ class L2ExpansionWorkflow extends Workflow {
       "--substrates-list /home/vijay/act/reachables/output_inchis.txt -s -o /mnt/shared-data/Vijay/l2projection/ " +
       "-l /mnt/shared-data/3rdPartySoftware/Chemaxon/license_Start-up.cxl"
 
+    val spark = new SparkLauncher()
+      .setSparkHome("/usr/local/software/spark-1.5.2-bin-hadoop2.6")
+      .setAppResource("/home/vijay/act/reachables/target/scala-2.10/reachables-assembly-0.1.jar")
+      .setMainClass("com.act.biointerpretation.l2expansion.SparkSingleSubstrateROProjector")
+      .setMaster("spark://10.0.20.19:7077")
+      .setDeployMode("client")
+      .addAppArgs("--substrates-list /home/vijay/act/reachables/output_inchis.txt -s -o /mnt/shared-data/Vijay/l2projection/ -l /mnt/shared-data/3rdPartySoftware/Chemaxon/license_Start-up.cxl")
+      .launch();
+
+    spark.waitFor();
+
     // Print working directory
-    val job1 = ShellWrapper.shellCommand(command.split(" ").toList)
+    //val job1 = ShellWrapper.shellCommand(command.split(" ").toList)
+
+    val job1 = ShellWrapper.shellCommand(List("ls"))
+
 
     // See which files are available
     val job2 = ShellWrapper.shellCommand(List("ls"))
