84d431ee69fb2176685689c659ab05ca8c4ec527
vramakrishnan <vijay@20n.com>
2016-08-09 14:18:00 -0700

    added stuff

--- reachables/src/main/scala/com/act/analysis/proteome/tool_manager/workflow/L2ExpansionWorkflow.scala
+++ reachables/src/main/scala/com/act/analysis/proteome/tool_manager/workflow/L2ExpansionWorkflow.scala
@@ -12,7 +12,7 @@ class L2ExpansionWorkflow extends Workflow {
       "--class com.act.biointerpretation.l2expansion.SparkSingleSubstrateROProjector " +
       "--master spark://10.0.20.19:7077 --deploy-mode client --executor-memory 4G " +
       "/home/vijay/act/reachables/target/scala-2.10/reachables-assembly-0.1.jar " +
-      "--substrates-list /home/vijay/act/reachables/output_inchis.txt -s -o /home/vijay/output_spark/ " +
+      "--substrates-list /home/vijay/act/reachables/output_inchis.txt -s -o /mnt/shared-data/Vijay/l2projection/ " +
       "-l /mnt/shared-data/3rdPartySoftware/Chemaxon/license_Start-up.cxl"
 
     // Print working directory
