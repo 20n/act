ff9aaca043cd6577aa86a8d99a5b49c19882fcda
Mark T. Daly <mark@20n.com>
2016-08-10 16:52:56 -0700

    Add an explanation of why we jump through so many hoops to get Spark to project ROs how we want it to.

--- reachables/src/main/scala/com/act/biointerpretation/l2expansion/SparkSingleSubstrateROProjector.scala
+++ reachables/src/main/scala/com/act/biointerpretation/l2expansion/SparkSingleSubstrateROProjector.scala
@@ -216,8 +216,42 @@ object SparkSingleSubstrateROProjector {
         (ero, results._1, results._2)
       })
 
-    /* Ugh.  Run an aggregation on the results to force their evaluation.  Then we can iterate by partition.
-     * Make sure to call persist() or Spark will likely try to recompute the whole thing. */
+    /* This next part represents us jumping through some hoops (that are possibly on fire) in order to make Spark do
+     * the thing we want it to do: project in parallel but stream results back for storage partitioned by RO.
+     *
+     * All operations on RDDs are performed lazily.  Only operations that require some data to be returned to the driver
+     * process will initiate the application of those RDD operations on the cluster.  That is, functions like `count()`
+     * and `collect()` initiate the evaluation of map() on an RDD.
+     * For this job, we'd like Spark to project all of the single substrate RDDs in parallel, and then send the results
+     * back to the driver so that we can write those projections out into files on the local machine.  Unfortunately,
+     * `collect()` will wait for and then load into memory *all* of the contents of an RDD.  If we use a chain of calls
+     * like `rdd.map.collect()`, Spark will compute the projections in parallel but we'll run out of memory before we're
+     * able to manifest and store those projections.
+     *
+     * Spark does allow us to iterate over work units (partitions) of an RDD one at a time using `toLocalIterator()`.
+     * Using `toLocalIterator()`, we can slurp in and write out one partition at a time, which uses *much* less memory.
+     * However, thanks again to laziness, the partitions will only be evaluated as the driver asks to read them.  This
+     * puts the job into a mode where the projections are done on the cluster's work nodes, but they're run serially
+     * as `toLocalIterator()` requests them.  Yikes.
+     *
+     * To work around this mess, we start the job by running an aggregation (`count()`) on the RDD to force projection
+     * evaluation in parallel.  We chain that call with a `persist()` call to make sure Spark knows we're going to
+     * do something else with the resultsRDD after the `count()` call is complete--if we don't `persist()`, Spark will
+     * likely try to recompute the whole thing when we iterate over the partitions.  We call `unpersist()` at the end to
+     * tell Spark that we're done with the RDD and the memory it consumes can be reclaimed.
+     *
+     * Thus our workflow amounts to:
+     *   rdd.map(doSomeWork).persist().count()
+     *   rdd.toLocalIterator(writeOutTheRDD)
+     *   rdd.unpersist()
+     *
+     * This is clunky and perhaps ugly, but effective.  The projection is done in parallel while the driver is able to
+     * stream the results back a piece at a time.  The streaming adds a few minutes to the total runtime of the job, but
+     * it's a small price to pay for reducing the driver's memory consumption to a fraction of what it would be if we
+     * had to call `collect()`.
+     *
+     * See http://stackoverflow.com/questions/31383904/how-can-i-force-spark-to-execute-code/31384084#31384084
+     * for more context on Spark's laziness. */
     val resultCount = resultsRDD.persist().count()
     LOGGER.info(s"Projection completed with ${resultCount} results")
 
