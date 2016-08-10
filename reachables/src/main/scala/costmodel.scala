d4a214d608a1d21c1175b94293819f4da17eb23f
Saurabh Srivastava <saurabh@20n.com>
2016-08-09 23:11:20 -0700

    moving to typed model using package

--- reachables/src/main/scala/costmodel.scala
+++ reachables/src/main/scala/costmodel.scala
@@ -14,29 +14,13 @@ import squants.market.Money
 import squants.market.USD
 import squants.market.Price
 import squants.Ratio
+import squants.LikeRatio
 import squants.thermal.Temperature
 import squants.thermal.TemperatureConversions._
+import squants.Quantity
 
 class costmodel(modelName: String) {
 
-  // Better utilize the type system to enforce type constraints 
-  // e.g., USD / Kg = USDPerKg, LiterDay = Liter * Day
-  type Liter = Double
-  type Kg = Double
-  type GramPerLiter = Double
-  type GramPerKg = Double
-  type USD = Double
-  type Cents = Double
-  type USDPerKg = Double
-  type USDPerTon = Double
-  type USDPerBatch = Double
-  type KgPerBatch = Double
-  type Percentage = Double
-  type Hour = Double
-  type Day = Double
-  type Year = Double
-  type LiterDay = Double
-  type DegreeCentigrade = Double
   type KiloWattPerMeterCubed = Double
   type KiloJoulePerMMol = Double
   type MMolPerLiterHour = Double
@@ -45,20 +29,13 @@ class costmodel(modelName: String) {
   *  Unit Conversions 
   ********************************************************************************************/
 
-  case class Titer(base: Mass, counter: Volume) extends Ratio[Mass, Volume]
-  case class Yield(base: Mass, counter: Mass) extends Ratio[Mass, Mass]
+  case class Yield(base: Mass, counter: Mass) extends LikeRatio[Mass]
+  case class Titer(base: Mass, counter: Volume) extends Ratio[Mass, Volume] {
+    def *(that: Volume): Mass = base * (that.value / counter.value)
+  }
 
-  val Times1000 = { x: Double => x * 1000 }
-  val By1000 = { x: Double => x/1000 }
-  val By100 = { x: Double => x/100 }
-  val DaysToHours = { x: Double => 24 * x }
-  def VolumeToMass(x: Volume): Mass = x * (1.kilograms / 1.litres)
-  val GramToKg = By1000
-  val MgToGram = By1000
-  val KgToTon = By1000
-  val CentsToUSD = By100
-  val M3toLiter = Times1000
-  val PercentToRatio = By100
+  def VolumeToMass(x: Volume): Mass = Kilograms((x in Litres).value)
+  def MassToVolume(x: Mass): Volume = Litres((x in Kilograms).value)
 
   /********************************************************************************************
   *  Constants 
@@ -72,26 +49,30 @@ class costmodel(modelName: String) {
   val DefaultFermRunTime: Time = 10 days
   val DefaultBrothMassPerBatch: Mass = VolumeToMass(360 cubicMeters)
 
-  sealed trait Location { def name: String; def rentalRate: Cents }
+  def literDay(v: Volume, t: Time): Double = {
+    v.toLitres * t.toDays
+  }
+  sealed trait Location { def name: String; def rentalRate: Money }
+  def cmoRate(centsPerLiterDay: Double): Money = { USD(centsPerLiterDay / 100.00) }
   // See email thread "Model" between Saurabh, Jeremiah, Tim Revak for cost quotes from various places
-  case object GER extends Location { val name = "Germany"; val rentalRate = 5.0 }
-  case object ITL extends Location { val name = "Italy"; val rentalRate = 8.0 }
-  case object IND extends Location { val name = "India"; val rentalRate = 10.0 }
-  case object CHN extends Location { val name = "China"; val rentalRate = 5.3 }
-  case object MID extends Location { val name = "Midwest"; val rentalRate = 8.3 }
-  case object MEX extends Location { val name = "Mexico"; val rentalRate = 8.3 }
+  case object GER extends Location { val name = "Germany"; val rentalRate = cmoRate(5.0) }
+  case object ITL extends Location { val name = "Italy"; val rentalRate = cmoRate(8.0) }
+  case object IND extends Location { val name = "India"; val rentalRate = cmoRate(10.0) }
+  case object CHN extends Location { val name = "China"; val rentalRate = cmoRate(5.3) }
+  case object MID extends Location { val name = "Midwest"; val rentalRate = cmoRate(8.3) }
+  case object MEX extends Location { val name = "Mexico"; val rentalRate = cmoRate(8.3) }
 
   /************************************ Fermentation ****************************************/
   // Productivity and Titer
   val vesselSize: Volume = 200 cubicMeters
   val pcOfVesselUsed: Dimensionless = 90.0 percent
-  val hoursPerYear: Hour = (350 days) to Hours // all plants have two week downtime, so 365-14 days
+  val operationalTimePerYear: Time = 350 days // all plants have two week downtime, so 365-14 days
   val fermTemp: Temperature = 25 C
-  val hoursForCIP: Hour = 12 // Clean In Place
-  val finalDryCellWeight: GramPerKg = 170
-  val fermenterAspect: Dimensionless = 3.0 // height/diameter
+  val hoursForCIP: Time = 12 hours // Clean In Place
+  val finalDryCellWeight: Ratio[Mass, Mass] = mediaElem(170 g)
+  val fermenterAspect: Double = 3.0 // height/diameter
   // Compressor Power
-  val airFlow: Dimensionless = 2.0 
+  val airFlow: Double = 2.0 
   // Agitator Power
   val agitationRate: KiloWattPerMeterCubed = 0.75
   // Microbial Heat
@@ -100,13 +81,13 @@ class costmodel(modelName: String) {
   // Ammonia Use
   val ammoniaPerGlucose: Dimensionless = 6 percent
   // Steam Use
-  val initialTemp: DegreeCentigrade = 20
-  val sterilizedTemp: DegreeCentigrade = 121
+  val initialTemp: Temperature = 20 C
+  val sterilizedTemp: Temperature = 121 C
 
   // Labor
   val numOfEmployees: Integer = 14
   // Capital
-  val DepreciationLife: Year = 15
+  val DepreciationLife: Time = (15 * 365) days
 
   // Media composition and unit prices
   sealed trait MediaComp { def n: String; def amount: Ratio[Mass, Mass]; def cost: Price[Mass] }
@@ -139,8 +120,8 @@ class costmodel(modelName: String) {
   val allMediaComponents = List(KH2PO4, MgSO47H2O, NH42SO4, EDTA, FeSO47H2O, ZnSO47H2O, CaCl22H2O, CuSO4, Na2MoO42H2O, CoCl26H20, MnCl24H2O, Biotin, CaPantothe, NicotinicAc, Myoinositol, ThiamineHCl, PyroxidolHC)
   val unitCostOfMedia = allMediaComponents.map(x => x.amount * x.cost).reduce(_ + _)
 
-  case object Ammonia     { val n = "Ammonia"; val cost = 1.00 }
-  case object Glucose     { val n = "Glucose"; val cost = 0.32 }
+  case object Ammonia     { val n = "Ammonia"; val cost = alibaba(1.00) }
+  case object Glucose     { val n = "Glucose"; val cost = alibaba(0.32) }
   case object Antifoam    { val n = "Antifoam"; }
 
   /********************************************************************************************
@@ -153,7 +134,12 @@ class costmodel(modelName: String) {
   var brothMassPerBatch: Mass = DefaultBrothMassPerBatch
   var location: Location = GER;
 
-  def getPerTonCost(yields: Yield, titers: Titer): USDPerTon = {
+  def getPerTonCost(y: Double, t: Double): Double = {
+    val cost: Price[Mass] = getPerTonCost(Yield(y grams, 100 grams), Titer(t grams, 1 litres))
+    (cost * (1 tonnes)).value
+  }
+
+  def getPerTonCost(yields: Yield, titers: Titer): Price[Mass] = {
     strainTiter = titers
     strainYield = yields
     RentalModelCost()
@@ -162,50 +148,50 @@ class costmodel(modelName: String) {
   /********************************************************************************************
   *  Consumptions and cost per batch 
   ********************************************************************************************/
-  def workingVolume: Volume = vesselSize * pcOfVesselUsed 
-  def finalByInitialVol: Ratio = brothMassPerBatch / VolumeToMass(workingVolume)
-  def literDaysPerBatch = (fermRunTime, vesselSize)
-  def productPerBatch: Mass = brothMassPerBatch * strainTiter.value
-  def glcConsumedPerBatch: Mass = productPerBatch / strainYield.value
+  def workingVolume: Volume = vesselSize * pcOfVesselUsed.value
+  def finalByInitialVol: Double = brothMassPerBatch.value / VolumeToMass(workingVolume).value
+  def literDaysPerBatch = literDay(vesselSize, fermRunTime)
+  def productPerBatch: Mass = strainTiter * MassToVolume(brothMassPerBatch)
+  def glcConsumedPerBatch: Mass = strainYield.inverseRatio * productPerBatch
   def glcBatchedPerBatch: Mass = 0 grams // Glc in media = 0kgs: TODO: check if this is correct
   def glcFedPerBatch: Mass = glcConsumedPerBatch - glcBatchedPerBatch
-  def ammoniaUsedPerBatch: Mass = ammoniaPerGlucose * glcConsumedPerBatch
+  def ammoniaUsedPerBatch: Mass = ammoniaPerGlucose.value * glcConsumedPerBatch
 
-  def mediaPerBatch: Money = brothMassPerBatch * unitCostOfMedia
-  def glcPerBatch: USDPerBatch = Glucose.cost * glcFedPerBatch
-  def ammoniaPerBatch: USDPerBatch = Ammonia.cost * ammoniaUsedPerBatch 
-  def consumablesPerBatch: USDPerBatch = mediaPerBatch + glcPerBatch + ammoniaPerBatch
+  def mediaPerBatch: Money = unitCostOfMedia * brothMassPerBatch
+  def glcPerBatch: Money = Glucose.cost * glcFedPerBatch
+  def ammoniaPerBatch: Money = Ammonia.cost * ammoniaUsedPerBatch 
+  def consumablesPerBatch: Money = mediaPerBatch + glcPerBatch + ammoniaPerBatch
 
   /********************************************************************************************
   *  Rental Model  
   ********************************************************************************************/
-  def RentalModelCost(): USDPerTon = {
-    val rentPerBatch = literDaysPerBatch * CentsToUSD(location.rentalRate)
+  def RentalModelCost(): Price[Mass] = {
+    val rentPerBatch = location.rentalRate * literDaysPerBatch
 
     // brothvol = working vol + num_draws * (working vol)/2 => num_draws = 2(brothvol/working - 1)
-    def numDraws: Ratio = 2 * (finalByInitialVol - 1)
+    def numDraws: Double = 2 * (finalByInitialVol - 1)
     // final volume depends on num of draws, and there cannot be more than one draw a day (can there?)
     // which puts an upper bound on the final volume you can extract from a fermentation over x days
     // Do a sanity check that we did not get a input "final volume" which is insane compared to runtime
-    assert(numDraws <= fermRunTime)
+    assert(numDraws <= (fermRunTime in Days).value)
 
-    val costPerBatch: USDPerBatch = consumablesPerBatch + rentPerBatch
-    val costPerTon: USDPerTon = costPerBatch / KgToTon(productPerBatch)
+    val costPerBatch: Money = consumablesPerBatch + rentPerBatch
+    val costPerTon: Price[Mass] = costPerBatch / productPerBatch
     costPerTon
   }
 
   /********************************************************************************************
   *  Bottom Up Model 
   ********************************************************************************************/
-  def BottomUpModelCost(): USDPerTon = {
-    val electrical: USDPerBatch = 0
-    val cooling: USDPerBatch = 0
-    val steam: USDPerBatch = 0
-    val labor: USDPerBatch = 0
-    val depreciation: USDPerBatch = 0
+  def BottomUpModelCost(): Price[Mass] = {
+    val electrical: Money = USD(0) 
+    val cooling: Money = USD(0) 
+    val steam: Money = USD(0) 
+    val labor: Money = USD(0)
+    val depreciation: Money = USD(0)
 
-    val costPerBatch: USDPerBatch = consumablesPerBatch + electrical + cooling + steam + labor + depreciation
-    val costPerTon: USDPerTon = costPerBatch / KgToTon(productPerBatch)
+    val costPerBatch: Money = consumablesPerBatch + electrical + cooling + steam + labor + depreciation
+    val costPerTon: Price[Mass] = costPerBatch / productPerBatch
     costPerTon
   }
 
