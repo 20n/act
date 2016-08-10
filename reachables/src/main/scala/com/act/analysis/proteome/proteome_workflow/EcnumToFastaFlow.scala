496d9b6ebecd4d754b6b82c435b60c14c63776d6
MichaelLampe20n <Michael@20n.com>
2016-08-10 16:26:57 -0700

    Multiple EC nums

--- reachables/src/main/scala/com/act/analysis/proteome/proteome_workflow/EcnumToFastaFlow.scala
+++ reachables/src/main/scala/com/act/analysis/proteome/proteome_workflow/EcnumToFastaFlow.scala
@@ -13,14 +13,14 @@ class EcnumToFastaFlow extends Workflow
   with WorkingDirectoryUtility {
 
   override val HELP_MESSAGE = "Workflow to convert EC Numbers into an unaligned FASTA file."
-  private val OPTION_EC_NUM_ARG_PREFIX = "e"
-  private val OPTION_OUTPUT_FASTA_FILE_PREFIX = "f"
-  private val OPTION_WORKING_DIRECTORY_PREFIX = "w"
-  private val OPTION_DATABASE_PREFIX = "d"
+  private val OPTION_EC_NUM = "e"
+  private val OPTION_OUTPUT_FASTA_FILE = "f"
+  private val OPTION_WORKING_DIRECTORY = "w"
+  private val OPTION_DATABASE = "d"
 
   override def getCommandLineOptions: Options = {
     val options = List[CliOption.Builder](
-      CliOption.builder(OPTION_EC_NUM_ARG_PREFIX).
+      CliOption.builder(OPTION_EC_NUM).
         required(true).
         hasArgs.
         valueSeparator(',').
@@ -30,18 +30,18 @@ class EcnumToFastaFlow extends Workflow
           "it will assume you want all reactions within a subgroup, " +
           "such as the value 6.1.1 will match 6.1.1.1 as well as 6.1.1.2"),
 
-      CliOption.builder(OPTION_OUTPUT_FASTA_FILE_PREFIX).
+      CliOption.builder(OPTION_OUTPUT_FASTA_FILE).
         hasArg.
         longOpt("output-fasta-from-ecnum-location").
         desc("The file path to write the FASTA file " +
           "containing all the enzyme sequences that catalyze a reaction within the ecnum."),
 
-      CliOption.builder(OPTION_WORKING_DIRECTORY_PREFIX).
+      CliOption.builder(OPTION_WORKING_DIRECTORY).
         hasArg.
         longOpt("working-directory").
         desc("Run and create all files from a working directory you designate."),
 
-      CliOption.builder(OPTION_DATABASE_PREFIX).
+      CliOption.builder(OPTION_DATABASE).
         longOpt("database").
         hasArg.desc("The name of the MongoDB to use for this query.").
         required(true),
@@ -57,13 +57,15 @@ class EcnumToFastaFlow extends Workflow
 
   def defineWorkflow(cl: CommandLine): Job = {
     // Grab the ec number
-    val workingDir = cl.getOptionValue(OPTION_WORKING_DIRECTORY_PREFIX, null)
+    val ec_num = cl.getOptionValue(OPTION_EC_NUM)
+
+    val workingDir = cl.getOptionValue(OPTION_WORKING_DIRECTORY, null)
 
     // Setup all the constant paths here
     def defineEcNumberJob(ecnum: String): ScalaJob = {
       val outputFastaPath = defineOutputFilePath(
         cl,
-        OPTION_OUTPUT_FASTA_FILE_PREFIX,
+        OPTION_OUTPUT_FASTA_FILE,
         "EC_" + ecnum,
         "output.fasta",
         workingDir
@@ -71,10 +73,12 @@ class EcnumToFastaFlow extends Workflow
 
       // Create the FASTA file out of all the relevant sequences.
       ScalaJobWrapper.wrapScalaFunction(
-        writeFastaFileFromEnzymesMatchingEcnums(ecnum, outputFastaPath, cl.getOptionValue(OPTION_DATABASE_PREFIX)) _)
+        writeFastaFileFromEnzymesMatchingEcnums(ecnum, outputFastaPath, cl.getOptionValue(OPTION_DATABASE)) _)
     }
 
-    headerJob.thenRunBatch(cl.getOptionValues(OPTION_EC_NUM_ARG_PREFIX).toList.map(defineEcNumberJob))
+
+    // Create the FASTA file out of all the relevant sequences.
+    headerJob.thenRunBatch(cl.getOptionValues(OPTION_EC_NUM).toList.map(defineEcNumberJob))
 
     headerJob
   }
