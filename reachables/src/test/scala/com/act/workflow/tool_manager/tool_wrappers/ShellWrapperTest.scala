62bbee1f410a8b296be035942ea39f45a3c7497c
MichaelLampe20n <Michael@20n.com>
2016-08-15 10:46:59 -0700

    Added verbosity setting.

--- reachables/src/test/scala/com/act/workflow/tool_manager/tool_wrappers/ShellWrapperTest.scala
+++ reachables/src/test/scala/com/act/workflow/tool_manager/tool_wrappers/ShellWrapperTest.scala
@@ -1,10 +1,14 @@
 package com.act.workflow.tool_manager.tool_wrappers
 
 import com.act.workflow.tool_manager.jobs.Job
-import com.act.workflow.tool_manager.jobs.management.JobManager
+import com.act.workflow.tool_manager.jobs.management.{JobManager, LoggingVerbosity}
 import org.scalatest._
 
 class ShellWrapperTest extends FlatSpec with Matchers with BeforeAndAfterEach {
+  override def beforeEach(): Unit = {
+    JobManager.setVerbosity(LoggingVerbosity.Off)
+  }
+
   override def afterEach(): Unit = {
     JobManager.clearManager()
   }
@@ -20,6 +24,8 @@ class ShellWrapperTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
   "The ShellWrapper" should "not start jobs prior to start being called" in {
     val command = ShellWrapper.shellCommand("date", List("date"))
+    command.doNotWriteOutputStream()
+    command.doNotWriteErrorStream()
 
     command.getJobStatus.isSuccessful should be(false)
     command.getJobStatus.isFailed should be(false)
@@ -31,6 +37,8 @@ class ShellWrapperTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
   "The ShellWrapper" should "indicate valid commands complete" in {
     val command = ShellWrapper.shellCommand("date", List("date"))
+    command.doNotWriteOutputStream()
+    command.doNotWriteErrorStream()
 
     JobManager.awaitUntilAllJobsComplete(command)
 
@@ -40,6 +48,9 @@ class ShellWrapperTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
   "The ShellWrapper" should "report that commands that fail on shell fail" in {
     val command = ShellWrapper.shellCommand("cp", List("cp"))
+    command.doNotWriteOutputStream()
+    command.doNotWriteErrorStream()
+
     JobManager.awaitUntilAllJobsComplete(command)
 
     command.getJobStatus.isSuccessful should be(false)
@@ -52,6 +63,9 @@ class ShellWrapperTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
   "The ShellWrapper" should "indicate a job is running when it is running" in {
     val command = ShellWrapper.shellCommand("sleep", List("sleep", "5"))
+    command.doNotWriteOutputStream()
+    command.doNotWriteErrorStream()
+
     command.start()
 
     command.getJobStatus.isSuccessful should be(false)
@@ -65,6 +79,12 @@ class ShellWrapperTest extends FlatSpec with Matchers with BeforeAndAfterEach {
   "The ShellWrapper" should "allow for chaining of jobs" in {
     val command1 = ShellWrapper.shellCommand("date", List("date"))
     val command2 = ShellWrapper.shellCommand("ls", List("ls"))
+
+    command1.doNotWriteOutputStream()
+    command1.doNotWriteErrorStream()
+
+    command2.doNotWriteOutputStream()
+    command2.doNotWriteErrorStream()
 
     command1.thenRun(command2)
 
