7be1d34108469f56c4fcd4d735724a1fe684999d
MichaelLampe20n <Michael@20n.com>
2016-08-15 09:48:53 -0700

    Moved Job status management outside of the Job class to clean things up a bit.

--- reachables/src/test/scala/com/act/workflow/tool_manager/tool_wrappers/ShellWrapperTest.scala
+++ reachables/src/test/scala/com/act/workflow/tool_manager/tool_wrappers/ShellWrapperTest.scala
@@ -12,19 +12,24 @@ class ShellWrapperTest extends FlatSpec with Matchers with BeforeAndAfterEach {
     JobManager.clearManager()
   }
 
-  override def beforeEach(): Unit = {
-    JobManager.setVerbosity(0)
+  def successfulJob(command: Job): Unit = {
+    command.getJobStatus.isSuccessful should be(true)
+    command.getJobStatus.isFailed should be(false)
+    command.getJobStatus.isCompleted should be(true)
+    command.getJobStatus.isRunning should be(false)
+    command.getJobStatus.isUnstarted should be(false)
+    command.getReturnCode should be(0)
   }
 
   "The ShellWrapper" should "not start jobs prior to start being called" in {
     val command = ShellWrapper.shellCommand("date", List("date"))
 
-    command.isSuccessful should be(false)
-    command.isFailed should be(false)
-    command.isCompleted should be(false)
-    command.isRunning should be(false)
-    command.isUnstarted should be(true)
-    command.returnCode should be(-1)
+    command.getJobStatus.isSuccessful should be(false)
+    command.getJobStatus.isFailed should be(false)
+    command.getJobStatus.isCompleted should be(false)
+    command.getJobStatus.isRunning should be(false)
+    command.getJobStatus.isUnstarted should be(true)
+    command.getReturnCode should be(-1)
   }
 
   "The ShellWrapper" should "indicate valid commands complete" in {
@@ -40,24 +45,24 @@ class ShellWrapperTest extends FlatSpec with Matchers with BeforeAndAfterEach {
     val command = ShellWrapper.shellCommand("cp", List("cp"))
     JobManager.awaitUntilAllJobsComplete(command)
 
-    command.isSuccessful should be(false)
-    command.isFailed should be(true)
-    command.isCompleted should be(true)
-    command.isRunning should be(false)
-    command.isUnstarted should be(false)
-    command.returnCode shouldNot be(0)
+    command.getJobStatus.isSuccessful should be(false)
+    command.getJobStatus.isFailed should be(true)
+    command.getJobStatus.isCompleted should be(true)
+    command.getJobStatus.isRunning should be(false)
+    command.getJobStatus.isUnstarted should be(false)
+    command.getReturnCode shouldNot be(0)
   }
 
   "The ShellWrapper" should "indicate a job is running when it is running" in {
     val command = ShellWrapper.shellCommand("sleep", List("sleep", "5"))
     command.start()
 
-    command.isSuccessful should be(false)
-    command.isFailed should be(false)
-    command.isCompleted should be(false)
-    command.isRunning should be(true)
-    command.isUnstarted should be(false)
-    command.returnCode shouldNot be(0)
+    command.getJobStatus.isSuccessful should be(false)
+    command.getJobStatus.isFailed should be(false)
+    command.getJobStatus.isCompleted should be(false)
+    command.getJobStatus.isRunning should be(true)
+    command.getJobStatus.isUnstarted should be(false)
+    command.getReturnCode shouldNot be(0)
   }
 
   "The ShellWrapper" should "allow for chaining of jobs" in {
@@ -68,13 +73,18 @@ class ShellWrapperTest extends FlatSpec with Matchers with BeforeAndAfterEach {
 
     JobManager.awaitUntilAllJobsComplete(command1)
 
-      command1.thenRun(command2)
+    command1.getJobStatus.isSuccessful should be(true)
+    command1.getJobStatus.isFailed should be(false)
+    command1.getJobStatus.isCompleted should be(true)
+    command1.getJobStatus.isRunning should be(false)
+    command1.getJobStatus.isUnstarted should be(false)
+    command1.getReturnCode should be(0)
 
-    command2.isSuccessful should be(true)
-    command2.isFailed should be(false)
-    command2.isCompleted should be(true)
-    command2.isRunning should be(false)
-    command2.isUnstarted should be(false)
-    command2.returnCode should be(0)
+    command2.getJobStatus.isSuccessful should be(true)
+    command2.getJobStatus.isFailed should be(false)
+    command2.getJobStatus.isCompleted should be(true)
+    command2.getJobStatus.isRunning should be(false)
+    command2.getJobStatus.isUnstarted should be(false)
+    command2.getReturnCode should be(0)
   }
 }
