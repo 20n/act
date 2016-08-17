fc906cf489632b2d6db646bab2ca9a13f0238b80
MichaelLampe20n <Michael@20n.com>
2016-08-17 12:36:27 -0700

    Unixified shell tests

--- reachables/src/test/scala/com/act/workflow/tool_manager/tool_wrappers/ShellWrapperTest.scala
+++ reachables/src/test/scala/com/act/workflow/tool_manager/tool_wrappers/ShellWrapperTest.scala
@@ -2,6 +2,7 @@ package com.act.workflow.tool_manager.tool_wrappers
 
 import com.act.workflow.tool_manager.jobs.Job
 import com.act.workflow.tool_manager.jobs.management.JobManager
+import org.apache.commons.lang.SystemUtils.IS_OS_UNIX
 import org.scalatest._
 import org.scalatest.concurrent.{ThreadSignaler, TimeLimitedTests}
 import org.scalatest.time.SpanSugar._
@@ -20,83 +21,93 @@ class ShellWrapperTest extends FlatSpec with Matchers with TimeLimitedTests with
 
 
   def successfulJob(command: Job): Unit = {
-    command.getInternalState.status.isSuccessful should be(true)
-    command.getInternalState.status.isFailed should be(false)
-    command.getInternalState.status.isCompleted should be(true)
-    command.getInternalState.status.isRunning should be(false)
-    command.getInternalState.status.isUnstarted should be(false)
-    command.getInternalState.returnCode should be(0)
+    command.internalState.statusManager.isSuccessful should be(true)
+    command.internalState.statusManager.isFailed should be(false)
+    command.internalState.statusManager.isCompleted should be(true)
+    command.internalState.statusManager.isRunning should be(false)
+    command.internalState.statusManager.isNotStarted should be(false)
+    command.internalState.getReturnCode should be(0)
   }
 
   "The ShellWrapper" should "not start jobs prior to start being called" in {
-    val command = ShellWrapper.shellCommand("date", List("date"))
-    command.doNotWriteOutputStream()
-    command.doNotWriteErrorStream()
+    if (IS_OS_UNIX) {
+      val command = ShellWrapper.shellCommand("date", List("date"))
+      command.doNotWriteOutputStream()
+      command.doNotWriteErrorStream()
 
-    command.getInternalState.status.isSuccessful should be(false)
-    command.getInternalState.status.isFailed should be(false)
-    command.getInternalState.status.isCompleted should be(false)
-    command.getInternalState.status.isRunning should be(false)
-    command.getInternalState.status.isUnstarted should be(true)
-    command.getInternalState.returnCode should be(-1)
+      command.internalState.statusManager.isSuccessful should be(false)
+      command.internalState.statusManager.isFailed should be(false)
+      command.internalState.statusManager.isCompleted should be(false)
+      command.internalState.statusManager.isRunning should be(false)
+      command.internalState.statusManager.isNotStarted should be(true)
+      command.internalState.getReturnCode should be(-1)
+    }
   }
 
   "The ShellWrapper" should "indicate valid commands complete" in {
-    val command = ShellWrapper.shellCommand("date", List("date"))
-    command.doNotWriteOutputStream()
-    command.doNotWriteErrorStream()
+    if (IS_OS_UNIX) {
+      val command = ShellWrapper.shellCommand("date", List("date"))
+      command.doNotWriteOutputStream()
+      command.doNotWriteErrorStream()
 
-    JobManager.startJobAndAwaitUntilWorkflowComplete(command)
+      JobManager.startJobAndAwaitUntilWorkflowComplete(command)
 
-    successfulJob(command)
+      successfulJob(command)
+    }
   }
 
 
   "The ShellWrapper" should "report that commands that fail on shell fail" in {
-    val command = ShellWrapper.shellCommand("cp", List("cp"))
-    command.doNotWriteOutputStream()
-    command.doNotWriteErrorStream()
+    if (IS_OS_UNIX) {
+      val command = ShellWrapper.shellCommand("cp", List("cp"))
+      command.doNotWriteOutputStream()
+      command.doNotWriteErrorStream()
 
-    JobManager.startJobAndAwaitUntilWorkflowComplete(command)
+      JobManager.startJobAndAwaitUntilWorkflowComplete(command)
 
-    command.getInternalState.status.isSuccessful should be(false)
-    command.getInternalState.status.isFailed should be(true)
-    command.getInternalState.status.isCompleted should be(true)
-    command.getInternalState.status.isRunning should be(false)
-    command.getInternalState.status.isUnstarted should be(false)
-    command.getInternalState.returnCode shouldNot be(0)
+      command.internalState.statusManager.isSuccessful should be(false)
+      command.internalState.statusManager.isFailed should be(true)
+      command.internalState.statusManager.isCompleted should be(true)
+      command.internalState.statusManager.isRunning should be(false)
+      command.internalState.statusManager.isNotStarted should be(false)
+      command.internalState.getReturnCode shouldNot be(0)
+    }
   }
 
   "The ShellWrapper" should "indicate a job is running when it is running" in {
-    val command = ShellWrapper.shellCommand("sleep", List("sleep", "5"))
-    command.doNotWriteOutputStream()
-    command.doNotWriteErrorStream()
+    if (IS_OS_UNIX) {
+      val command = ShellWrapper.shellCommand("sleep", List("sleep", "5"))
+      command.doNotWriteOutputStream()
+      command.doNotWriteErrorStream()
 
-    command.start()
+      command.start()
 
-    command.getInternalState.status.isSuccessful should be(false)
-    command.getInternalState.status.isFailed should be(false)
-    command.getInternalState.status.isCompleted should be(false)
-    command.getInternalState.status.isRunning should be(true)
-    command.getInternalState.status.isUnstarted should be(false)
-    command.getInternalState.returnCode shouldNot be(0)
+      command.internalState.statusManager.isSuccessful should be(false)
+      command.internalState.statusManager.isFailed should be(false)
+      command.internalState.statusManager.isCompleted should be(false)
+      command.internalState.statusManager.isRunning should be(true)
+      command.internalState.statusManager.isNotStarted should be(false)
+      command.internalState.getReturnCode shouldNot be(0)
+    }
   }
 
   "The ShellWrapper" should "allow for chaining of jobs" in {
-    val command1 = ShellWrapper.shellCommand("date", List("date"))
-    val command2 = ShellWrapper.shellCommand("ls", List("ls"))
+    if (IS_OS_UNIX) {
+      val command1 = ShellWrapper.shellCommand("date", List("date"))
+      val command2 = ShellWrapper.shellCommand("ls", List("ls"))
 
-    command1.doNotWriteOutputStream()
-    command1.doNotWriteErrorStream()
+      command1.doNotWriteOutputStream()
+      command1.doNotWriteErrorStream()
 
-    command2.doNotWriteOutputStream()
-    command2.doNotWriteErrorStream()
+      command2.doNotWriteOutputStream()
+      command2.doNotWriteErrorStream()
 
-    command1.thenRun(command2)
+      command1.thenRun(command2)
 
-    JobManager.startJobAndAwaitUntilWorkflowComplete(command1)
+      JobManager.startJobAndAwaitUntilWorkflowComplete(command1)
 
-    successfulJob(command1)
-    successfulJob(command2)
+      successfulJob(command1)
+      successfulJob(command2)
+    }
   }
 }
