bf7d699c760eaade59b1ba22303187bb66bb1763
thomas-20n <thomas@20n.com>
2016-08-31 12:33:36 -0700

    Updating hostnames

--- reachables/src/main/java/act/installer/bing/BingSearchResults.java
+++ reachables/src/main/java/act/installer/bing/BingSearchResults.java
@@ -52,7 +52,7 @@ public class BingSearchResults {
   private static final Integer TOP_N = 50;
   
   // The centralized location for caching Bing Search queries.
-  private static final String BING_CACHE_HOST = "chimay";
+  private static final String BING_CACHE_HOST = "bing-cache";
   private static final int BING_CACHE_MONGO_PORT = 27777;
   private static final String BING_CACHE_MONGO_DATABASE = "bingsearch";
 
