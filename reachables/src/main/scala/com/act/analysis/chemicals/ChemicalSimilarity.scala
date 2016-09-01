7865acdcd0ae573f1a10cd52bdc24ef01189a6b2
MichaelLampe20n <Michael@20n.com>
2016-09-01 16:05:55 -0700

    Clean up code and allow multiple similarity settings.

--- reachables/src/main/scala/com/act/analysis/chemicals/ChemicalSimilarity.scala
+++ reachables/src/main/scala/com/act/analysis/chemicals/ChemicalSimilarity.scala
@@ -5,48 +5,75 @@ import chemaxon.struc.Molecule
 
 import scala.collection.concurrent.TrieMap
 
+/**
+  * Concurrency safe cached molecule
+  */
 object ChemicalSimilarity {
 
-  private val cfp = new CFParameters()
-  cfp.setLength(2048)
-  cfp.setBondCount(15)
-  cfp.setBitCount(4)
-
-  // Instead of remaking for all SARs, we cache our calculators on by their query chemicals.
-  private val factoryCache = TrieMap[Molecule, SimilarityCalculator[Array[Int]]]()
+  // Instead of remaking for all SARs, we cache our calculators by their query chemicals.
+  private val factoryCache = TrieMap[String, TrieMap[Molecule, SimilarityCalculator[Array[Int]]]]()
+  _cfp.setLength(2048)
+  _cfp.setBondCount(15)
+  _cfp.setBitCount(4)
+  // If a chemical's Array[Int] form has been calculated, cache it for reuse later (Really speeds up SAR tree traversal).
   private val chemicalCache = TrieMap[Molecule, Array[Int]]()
+  // Var
+  private var _cfp = new CFParameters()
 
-  def calculateSimilarity(query: Molecule, target: Molecule): Double = {
-    val cachedSimCalc = factoryCache.get(query)
-
-    val simCalc: SimilarityCalculator[Array[Int]] =
-      if (cachedSimCalc.isDefined) cachedSimCalc.get else SimilarityCalculatorFactory.create("TVERSKY;0.33;0.99")
-
-    // Setup calculator if it is new
-    if (cachedSimCalc.isEmpty) {
+  /**
+    * For two molecules, use a calculator to determine their closeness.
+    *
+    * @param calculatorSettings The settings with which to apply the similarity calculation with
+    *                           The current default value was chosen based on the Chemaxon tutorial example.
+    * @param query              Molecule to use as the query molecule.
+    * @param target             Molecule you are targeting to see how similar it is to the query.
+    *
+    * @return Similarity value between 0 and 1.
+    */
+  def calculateSimilarity(calculatorSettings: String = "TVERSKY;0.33;0.99")(query: Molecule, target: Molecule): Double = {
+    val simCalc = SimilarityCalculatorFactory.create(calculatorSettings)
       simCalc.setQueryFingerprint(moleculeToIntArray(query))
-      factoryCache.putIfAbsent(query, simCalc)
+    simCalc.getSimilarity(moleculeToIntArray(target))
     }
 
-    simCalc.getSimilarity(moleculeToIntArray(target))
-  }
-
+  /**
+    * Utility to convert from a molecule to an Array[Int] using its chemical footprint.
+    *
+    * @param mol Input molecule
+    *
+    * @return Array representing the input molecule.
+    */
   private def moleculeToIntArray(mol: Molecule): Array[Int] = {
     val intArray: Option[Array[Int]] = chemicalCache.get(mol)
 
+    // If it doesn't exist, we generate it
     if (intArray.isEmpty) {
       val qfp = new ChemicalFingerprint(cfp)
 
-      // Chemaxon uses some internal modules that can cause concurrent issues.
+      // Chemaxon uses some internal modules that can cause concurrency issues on only this method call.
       this.synchronized {
         qfp.generate(mol)
       }
 
       val result = qfp.toFloatArray map (x => x.toInt)
       chemicalCache.putIfAbsent(mol, result)
+
+      // Return the result so we don't ask the cache for more than we need (Don't put then ask for it right away).
       return result
     }
 
     intArray.get
+  }
+
+  def cfp: CFParameters = {
+    this.synchronized {
+      _cfp
+    }
+  }
+
+  def cfp_=(value: CFParameters): Unit = {
+    this.synchronized {
+      _cfp = value
+    }
   }
 }
