88585d9b6c6902b038f72dafb3b1b794febb5565
MichaelLampe20n <Michael@20n.com>
2016-09-06 11:52:59 -0700

    Allow Smiles, add test to verify that functionality.

--- reachables/src/test/scala/com/act/analysis/chemicals/ChemicalSimilarityTest.scala
+++ reachables/src/test/scala/com/act/analysis/chemicals/ChemicalSimilarityTest.scala
@@ -6,6 +6,7 @@ class ChemicalSimilarityTest extends FlatSpec with Matchers {
   val benzeneInchi = "InChI=1S/C6H6/c1-2-4-6-5-3-1/h1-6H"
   val acetaminophenInchi = "InChI=1S/C8H9NO2/c1-6(10)9-7-2-4-8(11)5-3-7/h2-5,11H,1H3,(H,9,10)"
   val fluorinePerchlorateInchi = "InChI=1S/ClFO4/c2-6-1(3,4)5"
+  val tolueneSmiles = "Cc1ccccc1"
 
   ChemicalSimilarity.init()
 
@@ -32,5 +33,9 @@ class ChemicalSimilarityTest extends FlatSpec with Matchers {
 
   "ChemicalSimilarity" should "score completely different InChIs as being completely different." in {
     ChemicalSimilarity.calculateDissimilarity(fluorinePerchlorateInchi, benzeneInchi) should be(1.0)
+  }
+
+  "ChemicalSimilarity" should "be able to accept Smiles that have been." in {
+    ChemicalSimilarity.calculateSimilarity(tolueneSmiles, tolueneSmiles) should be(1.0)
   }
 }
