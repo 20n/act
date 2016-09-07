6848d2cbc28ed0d0a4dc3bf70734f6241ef099b4
thomas-20n <thomas@20n.com>
2016-09-07 15:57:37 -0700

    Adding ion mode selection and output target mass

--- reachables/src/main/r/LCMSDataVisualisation/server.R
+++ reachables/src/main/r/LCMSDataVisualisation/server.R
@@ -3,12 +3,17 @@ library(plot3D)
 library(mzR)
 library(dplyr)
 
-kHydrogenMass <- 1.007276
+kHMass <- 1.007276
+kNaMass <- 22.989771
+kLiMass <- 7.016004
+kKMass <- 38.963707
+kOMass <- 15.994915
+
 kChartLabelSizeFactor <- 1.3
 kLabelFactor <- 1.2
 
 shinyServer(function(input, output, session) {
- 
+  
   output$logo <- renderImage({
     # Return a list containing the filename
     list(src = "../../resources/20n.png",
@@ -17,8 +22,8 @@ shinyServer(function(input, output, session) {
          height = "120",
          alt = "20n Logo")
   }, deleteFile = FALSE)
-
-
+  
+  
   # Reactive value, loading all the scans in memory.
   # Recomputed only when filename changes
   full.data <- reactive({
@@ -29,35 +34,35 @@ shinyServer(function(input, output, session) {
     ms1.scans <- peaks(msfile, ms1) # list of num matrix
     list(hd = hd, ms1.scans = ms1.scans)
   })
-
+  
   # Based on the retention time range, select relevant scans reactively
   # Recomputed only when retention time range changes
   scans.and.header <- reactive({
     min.rt <- input$retention.time.range[1]
     max.rt <- input$retention.time.range[2]
-
+    
     full.data <- full.data()
-
+    
     # Extract the relevant scans from the full dataset
     header <- full.data$hd
     ms1 <- which(header$msLevel == 1)
     rtsel <- header$retentionTime[ms1] > min.rt & header$retentionTime[ms1] < max.rt # vector of boolean
     scans <- full.data$ms1.scans[rtsel]
-
+    
     # We need to replicate the retention time as many times as the length of each scan
     scan.lengths <- unlist(lapply(scans, nrow))
     retention.time <- rep(header$retentionTime[rtsel], scan.lengths)
     list(retention.time = retention.time, scans = scans)
   })
-
+  
   # We compute the data in long format
   # Recomputed only when the target mass or the mz band halfwidth changes
   data.long <- reactive({
-
+    
     # We need to transform the monoisotopic mass to the ionic mass for matching with LCMS m/z values
-    target.ionic.mass <- input$target.monoisotopic.mass + kHydrogenMass
-    min.ionic.mass <- target.ionic.mass - input$mz.band.halfwidth
-    max.ionic.mass <- target.ionic.mass + input$mz.band.halfwidth
+    target.mz.value <- target.mz()
+    min.ionic.mass <- target.mz.value - input$mz.band.halfwidth
+    max.ionic.mass <- target.mz.value + input$mz.band.halfwidth
     scans.header <- scans.and.header()
     data <- with(scans.header, {
       mz <- unlist(lapply(scans, function(x) x[, "mz"]))
@@ -65,27 +70,41 @@ shinyServer(function(input, output, session) {
       data.frame(mz = mz, retention.time = retention.time, intensity = intensity)
     })
     
-    data %>%
-      filter(mz < max.ionic.mass & mz > min.ionic.mass) %>%
-      mutate(monoisotopic.masses = mz - kHydrogenMass)
+    data %>% 
+      filter(mz < max.ionic.mass & mz > min.ionic.mass)
   })
-    
+  
+  target.mz <- reactive({
+    target.mass <- input$target.monoisotopic.mass
+    switch(input$mode,
+           "M" = target.mass,
+           "M+H" = target.mass + kHMass,
+           "M+Na" = target.mass + kNaMass,
+           "M+Li" = target.mass + kLiMass,
+           "M+H+H2O" = target.mass + 3 * kHMass + kOMass)
+  })
+  
   output$plot <- renderPlot({
     data <- data.long()
     with(data, {
       # Label factor, used to plot labels a little above points
       zlim.up <- max(intensity) * kLabelFactor
-      scatter3D(retention.time, monoisotopic.masses, intensity, pch = 16, cex = 1.5, type = "h",
+      scatter3D(retention.time, mz, intensity, pch = 16, cex = 1.5, type = "h",
                 colkey = list(side = 1, length = 0.5, width = 0.5, cex.clab = 0.75), expand = 0.5,
                 cex.lab=kChartLabelSizeFactor, cex.axis=kChartLabelSizeFactor,
                 cex.main=kChartLabelSizeFactor, cex.sub=kChartLabelSizeFactor,
                 zlab = "Intensity", xlab = "Retention time", ylab = "m/z (Monoisotopic mass)",
                 theta = input$angle.theta, phi = input$angle.phi, ticktype = "detailed", zlim = c(0, zlim.up))
       top.points <- data %>% top_n(1, intensity)
+      
       if (input$top.value) {
         # Display additional layer with top peak label
-        with(top.points, text3D(retention.time, monoisotopic.masses, intensity * kLabelFactor, add = TRUE, labels = round(monoisotopic.masses, 6)))
+        with(top.points, text3D(retention.time, mz, intensity * kLabelFactor, add = TRUE, labels = round(monoisotopic.masses, 6)))
       }
     })
+  })
+
+  output$target.mz <- renderText({
+    paste0("Corresponding m/z value is ", target.mz())
   })
 })
