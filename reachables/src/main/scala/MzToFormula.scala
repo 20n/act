5d47c989f84fa0a35e62298de6468971a35a9304
Saurabh Srivastava <saurabh@20n.com>
2016-09-06 21:43:18 -0700

    setting the stage for z3 solving

---
+++ reachables/src/main/scala/MzToFormula.scala
@@ -0,0 +1,10 @@
+package act.shared
+
+import act.shared.{CmdLineParser, OptDesc}
+import act.shared.ChemicalSymbols.{Atom, C, H, N, O, P, S, AminoAcid, AllAminoAcids}
+import act.shared.ChemicalSymbols.{Gly, Ala, Pro, Val, Cys, Ile, Leu, Met, Phe, Ser} 
+import act.shared.ChemicalSymbols.{Thr, Tyr, Asp, Glu, Lys, Trp, Asn, Gln, His, Arg}
+import act.shared.ChemicalSymbols.Helpers.{fromSymbol, computeMassFromAtomicFormula, computeFormulaFromElements}
+
+object MzToFormula {
+}
