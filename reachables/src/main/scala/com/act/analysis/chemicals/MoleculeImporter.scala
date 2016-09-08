9690ef19c358fc6bede19a8935a25803c35cb6fe
MichaelLampe20n <Michael@20n.com>
2016-09-08 10:41:51 -0700

    Now allows multiple molecule types to be imported at the same time.

--- reachables/src/main/scala/com/act/analysis/chemicals/MoleculeImporter.scala
+++ reachables/src/main/scala/com/act/analysis/chemicals/MoleculeImporter.scala
@@ -18,15 +18,40 @@ object MoleculeImporter {
   // Overload for easy java interop.
   @throws[MolFormatException]
   def importMolecule(mol: String): Molecule = {
-    importMolecule(mol, ChemicalSetting.Inchi)
+    importMolecule(mol, ChemicalFormat.Inchi)
+  }
+
+  private implicit def toMolecule(chemical: Chemical): String = chemical.getInChI
+
+  @throws[MolFormatException]
+  def importMolecule(mol: String, formats: List[ChemicalFormat.MoleculeType]): Molecule = {
+    val resultingInchis: List[Molecule] = formats.flatMap(format => {
+      try {
+        val importedMolecule = Option(importMolecule(mol, format))
+        importedMolecule
+      } catch {
+        case e: MolFormatException => None
+      }
+    })
+
+    if (resultingInchis.isEmpty) {
+      throw new MolFormatException()
+    }
+
+    if (resultingInchis.length > 1) {
+      throw new MolFormatException("Multiple format types matched this string, " +
+        s"so we couldn't decide which one was correct. String was $mol.")
+    }
+
+    resultingInchis.head
   }
 
   @throws[MolFormatException]
-  def importMolecule(mol: String, setting: ChemicalSetting.MoleculeType): Molecule = {
+  def importMolecule(mol: String, format: ChemicalFormat.MoleculeType): Molecule = {
     val molecule = moleculeCache.get(mol)
 
     if (molecule.isEmpty) {
-      val newMolecule = MolImporter.importMol(mol, setting)
+      val newMolecule = MolImporter.importMol(mol, format)
       moleculeCache.put(mol, newMolecule)
       return newMolecule
     }
@@ -34,9 +59,7 @@ object MoleculeImporter {
     molecule.get
   }
 
-  private implicit def toMolecule(chemical: Chemical): String = chemical.getInChI
-
-  object ChemicalSetting extends Enumeration {
+  object ChemicalFormat extends Enumeration {
     type MoleculeType = String
     val Inchi = "inchi"
     val Smiles = "smiles"
