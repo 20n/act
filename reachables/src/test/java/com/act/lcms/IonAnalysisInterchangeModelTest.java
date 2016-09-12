308ae3e16012c42ed87fd0b6aeb1eb68eed3d106
vramakrishnan <vijay@20n.com>
2016-09-12 13:08:21 -0700

    WIP: implementing test

---
+++ reachables/src/test/java/com/act/lcms/IonAnalysisInterchangeModelTest.java
@@ -0,0 +1,19 @@
+package com.act.lcms;
+
+import com.act.lcms.db.io.report.IonAnalysisInterchangeModel;
+import org.junit.Test;
+
+public class IonAnalysisInterchangeModelTest {
+
+  @Test
+  public void testAliphaticRODoesNotMatchWithAromaticCompoundAndViceVersa() throws Exception {
+
+
+    IonAnalysisInterchangeModel.filterAndOperateOnMoleculesFromMultipleReplicateResultFiles();
+
+
+
+
+
+  }
+}
