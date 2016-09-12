2b7a760d24e912e90af80ff222c0dc3a3bcb444f
MichaelLampe20n <Michael@20n.com>
2016-09-12 16:50:31 -0700

    Code for assigning RO to a given set of reactions w/ a prediction corpus

--- reachables/src/main/scala/com/act/biointerpretation/rsmiles/DataSerializationJsonProtocol.scala
+++ reachables/src/main/scala/com/act/biointerpretation/rsmiles/DataSerializationJsonProtocol.scala
@@ -5,5 +5,5 @@ import spray.json
 object DataSerializationJsonProtocol extends json.DefaultJsonProtocol {
   implicit val cFormat = jsonFormat2(AbstractChemicals.ChemicalInformation)
   implicit val rFormat = jsonFormat3(AbstractReactions.ReactionInformation)
-  implicit val roAssignmentFormat = jsonFormat2(SparkAssignRoToReactions.RoAssignments)
+//  implicit val roAssignmentFormat = jsonFormat2(AssignRoToReactions.RoAssignments)
 }
