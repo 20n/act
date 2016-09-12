db9d050afcf216c397d3a2828856d9e510763eaa
MichaelLampe20n <Michael@20n.com>
2016-09-12 16:50:31 -0700

    Code for assigning RO to a given set of reactions w/ a prediction corpus

---
+++ reachables/src/main/scala/com/act/biointerpretation/rsmiles/AssignRoToReactions.scala
@@ -0,0 +1,82 @@
+package com.act.biointerpretation.rsmiles
+
+import java.io.File
+
+import com.act.biointerpretation.l2expansion.L2PredictionCorpus
+import com.act.biointerpretation.rsmiles.AbstractChemicals.ChemicalInformation
+import com.act.biointerpretation.rsmiles.AbstractReactions.ReactionInformation
+import org.apache.log4j.LogManager
+import spray.json._
+import com.act.biointerpretation.rsmiles.DataSerializationJsonProtocol._
+
+import scala.collection.JavaConverters._
+
+object AssignRoToReactions {
+  val LOGGER = LogManager.getLogger(getClass)
+  def main(args: Array[String]) {
+    //val chemaxonLicenseFile: String = args(0)
+
+    val predictionFiles: List[File] = new File("/Volumes/shared-data/Michael/Rsmiles/TTT/ProjectionResults/").listFiles().filter(_.isFile).toList
+    val reactionsFile = new File("/Volumes/shared-data/Michael/Rsmiles/TTT/FromDatabasemarvin.AbstractReactions1.txt")
+
+    //val predictionFiles: List[File] = new File(args(1)).listFiles.filter(_.isFile).toList
+    //val reactionsFile: File = new File(args(2))
+
+
+    //chemaxonLicenseFile: String)(predictionFiles: List[File])(reactionsFile: File
+
+    val reactionInputFile = scala.io.Source.fromFile(reactionsFile)
+    val reactions: List[ReactionInformation] = reactionInputFile.getLines().mkString.parseJson.convertTo[List[ReactionInformation]]
+
+
+    val roPredictions: List[(Int, L2PredictionCorpus)] = predictionFiles.map(file => {
+
+      val corpy = L2PredictionCorpus.readPredictionsFromJsonFile(file)
+      val ro = file.getName.toInt
+
+      (ro, corpy)
+    })
+
+    // Prefilter so only matching substrates are looked at
+    val possibleReactions = roPredictions.map(roPrediction => {
+      // Multiple predictions for a given RO
+      val substrateInchis: List[Set[String]] = roPrediction._2.getCorpus.asScala.map(prediction => prediction.getSubstrateInchis.asScala.toSet).toList
+
+
+      // Only get reactions with matching substrates.  Each substrate => the same nubmer
+      val validSubstrateReactions = reactions.filter(reaction => substrateInchis.exists(s => equalSets(s, reaction.getSubstrates)))
+
+      if (validSubstrateReactions.isEmpty) {
+        (roPrediction._1, List())
+      }
+      else {
+        // Do same thing with products
+        val productInchis: List[Set[String]] = roPrediction._2.getCorpus.asScala.map(prediction => prediction.getProductInchis.asScala.toSet).toList
+
+        val total = validSubstrateReactions.filter(
+          // For each reaction
+          reaction =>
+            // There exists a reaction that is fully contained within a predictedProduct set
+            productInchis.exists(
+              predictedProduct =>
+                containsAllElements(predictedProduct, reaction.getProducts))
+        )
+        LOGGER.info(s"Found ${total.length} matching reactions")
+        total
+      }
+    })
+
+
+
+  }
+
+  def containsAllElements(elementA: Set[String], elementB: List[ChemicalInformation]): Boolean = {
+    if (elementB.isEmpty) return false
+    // Ensure that all of chemicalB is in a single elementA
+    elementB.forall(chemical => elementA(chemical.getString))
+  }
+
+  def equalSets(elementA: Set[String], elementB: List[ChemicalInformation]): Boolean = {
+    elementA.equals(elementB.map(chemical => chemical.getString).toSet)
+  }
+}
