536da262d0ad06b1f4f88ab794d1789761352fe3
MichaelLampe20n <Michael@20n.com>
2016-09-13 15:37:50 -0700

    Minimize substrate file size.

--- reachables/src/main/scala/com/act/biointerpretation/rsmiles/AbstractChemicalsToReactions.scala
+++ reachables/src/main/scala/com/act/biointerpretation/rsmiles/AbstractChemicalsToReactions.scala
@@ -21,23 +21,28 @@ object AbstractChemicalsToReactions {
     val db = Mongo.connectToMongoDatabase()
     val abstractChemicals = AbstractChemicals.getAbstractChemicals(db)
     val abstractReactions = AbstractReactions.getAbstractReactions(db)(abstractChemicals, substrateCount)
-    logger.info(s"Found ${abstractReactions.size} matching reactions with $substrateCount substrates. Writing them to disk.")
+    logger.info(s"Found ${abstractReactions.size} matching reactions with $substrateCount substrates. " +
+      s"Writing both the substrates and reactions to disk.")
     writeSubstrateStringsForSubstrateCount(db)(abstractReactions.seq.toList, outputSubstrateFile)
     writeAbstractReactionsToJsonCorpus(abstractReactions.seq.toList, outputReactionCorpus)
   }
 
-  def writeAbstractReactionsToJsonCorpus(abstractReactions: List[ReactionInformation], outputReactionsLocation: File): Unit ={
+  def writeAbstractReactionsToJsonCorpus(abstractReactions: List[ReactionInformation],
+                                         outputReactionsLocation: File): Unit ={
     val outputFile = new BufferedWriter(new FileWriter(outputReactionsLocation))
     outputFile.write(abstractReactions.toJson.prettyPrint)
     outputFile.close()
   }
 
-  // Keeps track of other concurrent jobs that have previously determined the number of substrates in a reaction.
-
-  def writeSubstrateStringsForSubstrateCount(mongoDb: MongoDB)(reactions: List[ReactionInformation], outputFile: File): Unit = {
+  def writeSubstrateStringsForSubstrateCount(mongoDb: MongoDB)
+                                            (reactions: List[ReactionInformation], outputFile: File): Unit = {
     require(!outputFile.isDirectory, "The file you designated to output your files " +
       "to is a directory and therefore is not a valid path.")
-    val substrates: Seq[String] = reactions.flatMap(_.getSubstrates.map(_.getString)).seq
+    // We need to make sure this is a set so that we remove as many duplicates as possible.
+    logger.info(s"Quickly checking for duplicate substrates to minimize the size of our substrate corpus.  " +
+      s"Current size is ${reactions.length}")
+    val substrates: Set[String] = reactions.flatMap(_.getSubstrates.map(_.getString)).seq.toSet
+    logger.info(s"After removing duplicates, ${substrates.size} exist.")
     new L2InchiCorpus(substrates).writeToFile(outputFile)
   }
 
