3abababd335c5260b32a1dae65e1a2d6add1ded9
MichaelLampe20n <Michael@20n.com>
2016-09-13 18:36:33 -0700

    Clear after too so last one doesn't pollute

--- reachables/src/test/scala/com/act/analysis/chemicals/molecules/MoleculeFormatTest.scala
+++ reachables/src/test/scala/com/act/analysis/chemicals/molecules/MoleculeFormatTest.scala
@@ -8,6 +8,11 @@ class MoleculeFormatTest extends FlatSpec with Matchers with BeforeAndAfterEach 
     MoleculeExporter.clearCache()
   }
 
+  override def afterEach(): Unit = {
+    MoleculeImporter.clearCache()
+    MoleculeExporter.clearCache()
+  }
+
   "MoleculeFormat" should "should have import and export strings for all values." in {
     // We implicitly test that all the values can be hit by
     // enumerating over the values and testing if they match to an export and an import.
