defe2205162d14e9c6d90f1d7ce89ee03f36a3a5
MichaelLampe20n <Michael@20n.com>
2016-09-15 12:41:37 -0700

    serialization

--- reachables/src/main/scala/com/act/biointerpretation/rsmiles/DataSerializationJsonProtocol.scala
+++ reachables/src/main/scala/com/act/biointerpretation/rsmiles/DataSerializationJsonProtocol.scala
@@ -1,12 +1,12 @@
 package com.act.biointerpretation.rsmiles
 
-import com.act.biointerpretation.rsmiles.abstract_chemicals.AbstractChemicals.ChemicalInformation
-import com.act.biointerpretation.rsmiles.processing.ReactionProcessing.ReactionInformation
+import com.act.biointerpretation.rsmiles.abstract_chemicals.AbstractChemicals
+import com.act.biointerpretation.rsmiles.processing.ReactionProcessing
 import com.act.biointerpretation.rsmiles.sar_construction.ReactionRoAssignment
 import spray.json
 
 object DataSerializationJsonProtocol extends json.DefaultJsonProtocol {
-  implicit val cFormat = jsonFormat2(ChemicalInformation)
-  implicit val rFormat = jsonFormat3(ReactionInformation)
+  implicit val cFormat = jsonFormat2(AbstractChemicals.ChemicalInformation)
+  implicit val rFormat = jsonFormat3(ReactionProcessing.ReactionInformation)
   implicit val roAssignmentFormat = jsonFormat2(ReactionRoAssignment.RoAssignments)
 }
