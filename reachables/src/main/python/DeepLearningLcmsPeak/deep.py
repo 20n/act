b805e854d4d76c363bcb48173f2e46c3e2e4490e
MichaelLampe20n <Michael@20n.com>
2016-09-23 18:11:57 -0700

    windowing algorithm improved to better align peaks

--- reachables/src/main/python/DeepLearningLcmsPeak/deep.py
+++ reachables/src/main/python/DeepLearningLcmsPeak/deep.py
@@ -18,22 +18,17 @@ if __name__ == "__main__":
                         help="The size of the window to include over time. Each unit is about 0.2 seconds here.",
                         default=90)
 
-    parser.add_argument("-s", "--stepSize",
-                        type=int,
-                        help="The number of time points to skip between making each window",
-                        default=10)
-
     parser.add_argument("-e", "--encodingSize", type=int,
                         help="The size of the NN's encoding layer. "
                              "This is the compressed plot's representation and how many neurons it has to move around.",
-                        default=20)
+                        default=5)
     parser.add_argument("-z", "--mzSplit", type=float,
                         help="The level of granularity when dividing LCMS plots up.",
                         default=0.01)
     parser.add_argument("-c", "--clusterNumber",
                         type=int,
                         help="Number of kMeans clusters to cluster on.",
-                        default=500)
+                        default=200)
 
     parser.add_argument("-n", "--mzMin", type=int, help="The lowest M/Z value allowed.", default=49)
     parser.add_argument("-x", "--mzMax", type=int, help="The highest M/Z value allowed.", default=950)
@@ -45,7 +40,6 @@ if __name__ == "__main__":
     output_directory = args.outputDirectory
 
     block_size = args.lcmsWindowSize
-    step_size = args.stepSize
     encoding_size = args.encodingSize
     mz_division = args.mzSplit
     mz_min = args.mzMin
@@ -55,7 +49,7 @@ if __name__ == "__main__":
     # Train matrix
     autoencoder = LcmsAutoencoder(lcms_directory, lcms_plate_name, output_directory, block_size, encoding_size)
     row_matrix = autoencoder.process_lcms_trace(mz_division, mz_min, mz_max)
-    training_data, validation_data = autoencoder.prepare_matrix_for_encoding(row_matrix, block_size, step_size)
+    training_data, validation_data = autoencoder.prepare_matrix_for_encoding(row_matrix, block_size)
     autoencoder.train(training_data, validation_data)
     autoencoder.cluster(number_clusters, block_size, mz_division, mz_min)
     autoencoder.visualize(number_clusters)
