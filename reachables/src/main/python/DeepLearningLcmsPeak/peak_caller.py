f123aa6fd9996a2e0e2f80fc483dd8382178a717
MichaelLampe20n <Michael@20n.com>
2016-09-26 11:10:31 -0700

    Format to work with one sample and saved model

--- reachables/src/main/python/DeepLearningLcmsPeak/peak_caller.py
+++ reachables/src/main/python/DeepLearningLcmsPeak/peak_caller.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     parser.add_argument("lcmsDirectory", help="The LCMS plate directory.")
     parser.add_argument("lcmsPlateName", help="The name of the LCMS plate file.  Currently supports '.nc' format.")
     parser.add_argument("lcmsAutoEncoderModel", help="Path to a prepared LcmsEncoderModel prepared using deep.py.")
+    parser.add_argument("validPeaks", help="Path to a file containing individual peaks, one per line, listed of "
+                                           "valid clusters as defined by the lcmsAutoEncoderModel.")
     parser.add_argument("outputDirectory", help="Where to save all intermediate and final files.")
 
     args = parser.parse_args()
@@ -18,6 +20,7 @@ if __name__ == "__main__":
     lcms_directory = args.lcmsDirectory
     lcms_plate_name = args.lcmsPlateName
     model = args.lcmsAutoEncoderModel
+    peaks = args.validPeaks
     output_directory = args.outputDirectory
 
     autoencoder = pickle.load(model)
@@ -28,7 +31,9 @@ if __name__ == "__main__":
 
     encoded_samples = autoencoder.predict(processed_samples)
 
+    valid_peak_array = []
+    with open(peaks) as f:
+        lines = f.readlines()
+
     autoencoder.predict_clusters(encoded_samples, processed_samples,
                                  auxilariy_information, retention_times, lcms_plate_name.split(".nc")[0])
-
-    autoencoder.visualize()
