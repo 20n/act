45a82d4c5d0114acfa50a722f971aa1656498f2e
MichaelLampe20n <Michael@20n.com>
2016-09-28 14:13:58 -0700

    updates in line with differential analysis

--- reachables/src/main/python/DeepLearningLcmsPeak/magic.py
+++ reachables/src/main/python/DeepLearningLcmsPeak/magic.py
@@ -1,18 +1,23 @@
+from __future__ import absolute_import, division, print_function
+
 """
 Deep.py Default Values
 """
 cluster_number = 200
 mz_split = 0.01
-encoding_size = 5
-window_size = 90
+encoding_size = 2
+window_size = 10
 mz_min = 49
 mz_max = 951
+time_min = 0
+time_max = 450
+time_step = 2.5
 
 """
 Lcms_Autoencoder.py Default Values
 """
 # The lowest max value a window can have before we drop that window.
-lowest_encoded_window_max_value = 1e3
+lowest_encoded_window_max_value = 1e4
 
 loss_function = "mse"
 metrics = ["accuracy"]
--- reachables/src/main/python/DeepLearningLcmsPeak/peak_caller.py
+++ reachables/src/main/python/DeepLearningLcmsPeak/peak_caller.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         autoencoder = dill.loads("".join(f.readlines()))
         autoencoder.set_output_directory(output_directory)
 
-    row_matrix, retention_times = autoencoder.process_lcms_trace(lcms_directory, lcms_plate_name)
+    row_matrix = autoencoder.process_lcms_trace(lcms_directory, lcms_plate_name)
 
     processed_samples, auxilariy_information = autoencoder.prepare_matrix_for_encoding(row_matrix)
 
@@ -40,6 +40,5 @@ if __name__ == "__main__":
         lines = f.readlines()
         valid_peak_array = [int(cluster.strip()) for cluster in lines]
 
-    autoencoder.predict_clusters(encoded_samples, processed_samples,
-                                 auxilariy_information, retention_times, lcms_plate_name.split(".nc")[0],
-                                 valid_peak_array)
+    autoencoder.predict_clusters(encoded_samples, processed_samples, auxilariy_information,
+                                 lcms_plate_name.split(".nc")[0], valid_peak_array)
