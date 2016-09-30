daef144788a06db6d189dcabc4f0c04de10caae2
MichaelLampe20n <Michael@20n.com>
2016-09-30 13:23:44 -0700

    cli args to filter.py

--- reachables/src/main/python/DeepLearningLcmsPeak/filter.py
+++ reachables/src/main/python/DeepLearningLcmsPeak/filter.py
@@ -1,20 +1,26 @@
 import os
+import sys
 
 import pandas as pd
 
-plates = ["differential_expression"]
-input_directory = "/Volumes/shared-data-1/Michael/DifferentialExpressionDeepLearning/FigureOutPersteinData/"
-output_directory = os.path.join(input_directory, "Filtered_By_Cluster/")
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
 
-for plate in plates:
-    print("Filtering plate {}".format(plate))
-    in_fi_name = plate + ".tsv"
-    in_fi = input_directory + "/" + in_fi_name
+def filter_by_valid_clusters(input_dir):
+    plates = ["differential_expression"]
+    output_directory = os.path.join(input_dir, "Filtered_By_Cluster/")
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
-    df = pd.read_csv(in_fi, sep="\t")
-    # df = df[df["rt"] > 15]
-    df = df[df.cluster.isin([4, 5, 9, 11, 13, 16, 18])]
+    for plate in plates:
+        print("Filtering plate {}".format(plate))
+        in_fi_name = plate + ".tsv"
+        in_fi = os.path.join(input_dir, in_fi_name)
 
-    df.to_csv(os.path.join(output_directory, in_fi_name), sep="\t", index=False)
+        df = pd.read_csv(in_fi, sep="\t")
+        df = df[df.cluster.isin([0, 1, 3, 4, 11, 12, 13, 16, 18, 19])]
+
+        df.to_csv(os.path.join(output_directory, in_fi_name), sep="\t", index=False)
+
+
+if "__main__" == __name__:
+    input_directory = sys.argv[1]
+    filter_by_valid_clusters(input_directory)
