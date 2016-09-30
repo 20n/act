226a5da61800c5bb585b30de9cf4da6f84962e7d
thomas-20n <thomas@20n.com>
2016-09-29 19:16:29 -0700

    Actually adding molecule renderer

---
+++ reachables/src/main/r/LCMSDataVisualisation/molecule_renderer.R
@@ -0,0 +1,26 @@
+kMolStructureCacheFolder <- "/home/thomas/data/mol-structure-cache/"
+
+moleculeRenderer <- function(input, output, session, inchiString, height) {
+  
+  imageFilepath <- reactive({
+    inchiHash <- digest(inchiString())
+    filepath <- paste0(c(kMolStructureCacheFolder, inchiHash, ".png"), collapse = "")
+    if (!file.exists(filepath)) {
+      saveMoleculeStructure(inchiString(), filepath)
+    }
+    filepath
+  })
+  
+  output$molecule <- renderImage({
+    
+    list(src = imageFilepath(),
+         contentType = "image/png",
+         height = height,
+         alt = "molecule")
+  }, deleteFile = FALSE)
+}
+
+moleculeRendererUI <- function(id) {
+  ns <- NS(id)
+  imageOutput(ns("molecule"))
+}
