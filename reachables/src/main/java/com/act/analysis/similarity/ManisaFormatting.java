d2aebdbe9405122eff6ae55c95a59eab06764f72
vramakrishnan <vijay@20n.com>
2016-10-20 16:36:04 -0700

    resolved bug

--- reachables/src/main/java/com/act/analysis/similarity/ManisaFormatting.java
+++ reachables/src/main/java/com/act/analysis/similarity/ManisaFormatting.java
@@ -117,7 +117,7 @@ public class ManisaFormatting {
           List<String> scanFilesForViz = new ArrayList<>();
           scanFilesForViz.add(scanFile);
 
-          VizRepresentation.VizLayout layout = new VizRepresentation.VizLayout(includeIons.size(), 0);
+          VizRepresentation.VizLayout layout = new VizRepresentation.VizLayout(1, 0);
 
           VizRepresentation representation = new VizRepresentation(fileNames, peaks, layout);
 
