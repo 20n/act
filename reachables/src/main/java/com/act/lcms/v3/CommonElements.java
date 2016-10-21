2e8860c9f75da686916a936b11dd08853afda873
Thomas Legrand <thomas@20n.com>
2016-10-21 11:31:22 -0700

    Adding common elements implementation

--- reachables/src/main/java/com/act/lcms/v3/CommonElements.java
+++ reachables/src/main/java/com/act/lcms/v3/CommonElements.java
@@ -1,13 +1,47 @@
 package com.act.lcms.v3;
 
+import chemaxon.struc.PeriodicSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by thomas on 10/20/16.
  */
-public enum CommonElements {
+public enum CommonElements implements Element {
+  C(6), H(1), N, O, P, S,
+  ;
 
-
+  private String symbol;
+  private Integer atomicNumber;
 
   CommonElements() {
+    this.symbol = this.name();
+    this.atomicNumber = PeriodicSystem.findAtomicNumber(this.symbol);
+  }
 
+  CommonElements(int atomicNumber) {
+    this.symbol = this.name();
+    this.atomicNumber = atomicNumber;
+  }
+
+  @Override
+  public String getSymbol() {
+    return this.symbol;
+  }
+
+  @Override
+  public Integer getAtomicNumber() {
+    return this.atomicNumber;
+  }
+
+  @Override
+  public List<ElementIsotope> getElementIsotopes() {
+    return new ArrayList<>();
+  }
+
+  @Override
+  public Integer getValency() {
+    return -1;
   }
 }
