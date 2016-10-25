f607c877a936cde6bf40e52b6b1b29a77e99903a
MichaelLampe20n <Michael@20n.com>
2016-10-25 16:31:43 -0700

    metabolite update

--- reachables/src/main/java/com/act/biointerpretation/networkanalysis/Metabolite.java
+++ reachables/src/main/java/com/act/biointerpretation/networkanalysis/Metabolite.java
@@ -3,22 +3,53 @@ package com.act.biointerpretation.networkanalysis;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Represents a metabolite in the metabolite network.
  * For now this class only stores an inchi, but in the future it can represent multiple levels of abstraction: a
  * structure in any format, a chemical formula, or only a mass.
  */
 public class Metabolite {
+  private static AtomicInteger uuidAssigner = new AtomicInteger();
 
   @JsonProperty("inchi")
   private String inchi;
+  @JsonProperty("mass")
+  private Double mass;
+  @JsonProperty("uuid")
+  private Long uuid = (long) uuidAssigner.incrementAndGet();
 
   @JsonCreator
-  public Metabolite(@JsonProperty("inchi") String inchi) {
+  public Metabolite(Double mass, String inchi) {
+    this.mass = mass;
     this.inchi = inchi;
+  }
+
+  @JsonCreator
+  public Metabolite(String inchi) {
+    this.mass = null;
+    this.inchi = inchi;
+  }
+
+
+
+  @JsonCreator
+  public Metabolite(Double mass){
+    this(mass, null);
   }
 
   public String getInchi() {
     return inchi;
   }
+
+  public Long getUUID() {
+    return uuid;
+  }
+
+  public Double getMass() {
+    return mass;
+  }
+
+
 }
