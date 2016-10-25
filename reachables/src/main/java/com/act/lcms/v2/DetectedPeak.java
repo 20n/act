5cbb7517b447bfe5ec7875ea6a050f4f490239d3
thomas-20n <thomas@20n.com>
2016-10-25 16:29:48 -0700

    Added detected peak capacities

--- reachables/src/main/java/com/act/lcms/v2/DetectedPeak.java
+++ reachables/src/main/java/com/act/lcms/v2/DetectedPeak.java
@@ -1,5 +1,12 @@
 package com.act.lcms.v2;
 
+import com.act.biointerpretation.networkanalysis.Metabolite;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Created by tom on 10/25/16.
  */
@@ -9,7 +16,16 @@ public class DetectedPeak {
   private Double retentionTime;
   private Double mzWindow;
   private Double retentionTimeWindow;
+  private Double moleculeMass;
+  private List<Metabolite> metabolites;
 
-
-
+  public void parseFromJsonNode(JsonNode node, Map<Long, List<List<String>>> matchingInchis) {
+    this.mz = node.get("mz").asDouble();
+    this.retentionTime = node.get("rt").asDouble();
+    this.mzWindow = node.get("mz_band").asDouble();
+    this.retentionTimeWindow = node.get("rt_band").asDouble();
+    this.moleculeMass = node.get("moleculeMass").asDouble();
+    this.metabolites = matchingInchis.get(node.get("matching_inchis").asLong())
+        .stream().map(namedInchis -> new Metabolite(namedInchis.get(0))).collect(Collectors.toList());
+  }
 }
