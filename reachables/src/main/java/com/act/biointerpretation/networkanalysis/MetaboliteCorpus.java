6a61b64d4af43743bc7cba9461672a97292a09ac
thomas-20n <thomas@20n.com>
2016-10-25 17:15:45 -0700

    Parsing of JSON config file works! Commit before getting newer version of the Metabolite class

--- reachables/src/main/java/com/act/biointerpretation/networkanalysis/MetaboliteCorpus.java
+++ reachables/src/main/java/com/act/biointerpretation/networkanalysis/MetaboliteCorpus.java
@@ -1,5 +1,7 @@
 package com.act.biointerpretation.networkanalysis;
 
+import com.act.lcms.v2.DetectedPeak;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
@@ -9,8 +11,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 
 public class MetaboliteCorpus {
@@ -19,37 +23,63 @@ public class MetaboliteCorpus {
   private static final String DEFAULT_FILE_LOCATION = "/Volumes/shared-data/Michael/LowThresholdForRegression/dl.toIonMatchesFormulasNoMinusCholesterolMarkList.json.txt";
   private static transient final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
+  public List<Metabolite> getCorpus() {
+    return corpus;
+  }
+
   private List<Metabolite> corpus;
 
-  MetaboliteCorpus(){}
+  MetaboliteCorpus(){
+    corpus = new ArrayList<>();
+  }
 
   public JsonNode getJsonNodeFromFile(File inputFile) throws IOException {
     String fileString = new String(Files.readAllBytes(inputFile.toPath()));
     return MetaboliteCorpus.OBJECT_MAPPER.readTree(fileString);
   }
 
-  public void populateFromJsonNode(JsonNode node) {
+  public List<DetectedPeak> getDetectedPeaksFromJsonNode(JsonNode node) {
     JsonNode peaksNode = node.get("peaks");
     int size = peaksNode.size();
     LOGGER.info(size);
     Iterator<JsonNode> peaks = peaksNode.elements();
-    // "moleculeMass": 516.3827,
-    //    "rt": 209.61999893188477,
-    //    "raw_mz": 517.39,
-    //    "mz_band": 0.02,
-     //   "mz": 517.39,
 
-    List<Metabolite> l = new ArrayList<>(peaksNode.size());
+    JsonNode matchingInchisNode = node.get("matching_inchi_hashes");
+    Iterator<JsonNode> matchingInchis = matchingInchisNode.elements();
+    Map<Long, List<String>> m = new HashMap<>();
+    while (matchingInchis.hasNext()) {
+      JsonNode inchis = matchingInchis.next();
+      Long code = inchis.get("code").asLong();
+      List<String> vals = new ArrayList<>();
+      Iterator<JsonNode> valsIte = inchis.get("vals").elements();
+      while (valsIte.hasNext()) {
+        vals.add(valsIte.next().get(0).toString());
+      }
+
+      m.put(code, vals);
+    }
+
+    List<DetectedPeak> detectedPeaks = new ArrayList<>();
 
     while (peaks.hasNext()) {
+      DetectedPeak peak = new DetectedPeak();
+      peak.parseFromJsonNode(peaks.next(), m);
+      detectedPeaks.add(peak);
     }
+
+    return detectedPeaks;
   }
 
+  public void populateFromDetectedPeaks(List<DetectedPeak> detectedPeaks) throws JsonProcessingException {
+    detectedPeaks.forEach(p -> corpus.addAll(p.getMetabolites()));
+  }
 
   public static void main(String[] args) throws IOException {
     MetaboliteCorpus metaboliteCorpus = new MetaboliteCorpus();
     File inputFile = new File(DEFAULT_FILE_LOCATION);
     JsonNode node = metaboliteCorpus.getJsonNodeFromFile(inputFile);
-    metaboliteCorpus.populateFromJsonNode(node);
+    List<DetectedPeak> peaks = metaboliteCorpus.getDetectedPeaksFromJsonNode(node);
+    metaboliteCorpus.populateFromDetectedPeaks(peaks);
+    System.out.println(OBJECT_MAPPER.writeValueAsString(metaboliteCorpus.getCorpus()));
   }
 }
--- reachables/src/main/java/com/act/lcms/v2/DetectedPeak.java
+++ reachables/src/main/java/com/act/lcms/v2/DetectedPeak.java
@@ -7,9 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-/**
- * Created by tom on 10/25/16.
- */
+
 public class DetectedPeak {
 
   private Double mz;
@@ -19,13 +17,61 @@ public class DetectedPeak {
   private Double moleculeMass;
   private List<Metabolite> metabolites;
 
-  public void parseFromJsonNode(JsonNode node, Map<Long, List<List<String>>> matchingInchis) {
+  public void parseFromJsonNode(JsonNode node, Map<Long, List<String>> matchingInchis) {
     this.mz = node.get("mz").asDouble();
     this.retentionTime = node.get("rt").asDouble();
     this.mzWindow = node.get("mz_band").asDouble();
     this.retentionTimeWindow = node.get("rt_band").asDouble();
     this.moleculeMass = node.get("moleculeMass").asDouble();
     this.metabolites = matchingInchis.get(node.get("matching_inchis").asLong())
-        .stream().map(namedInchis -> new Metabolite(namedInchis.get(0))).collect(Collectors.toList());
+        .stream().map(namedInchis -> new Metabolite(namedInchis)).collect(Collectors.toList());
+  }
+
+  public Double getMz() {
+    return mz;
+  }
+
+  public void setMz(Double mz) {
+    this.mz = mz;
+  }
+
+  public Double getRetentionTime() {
+    return retentionTime;
+  }
+
+  public void setRetentionTime(Double retentionTime) {
+    this.retentionTime = retentionTime;
+  }
+
+  public Double getMzWindow() {
+    return mzWindow;
+  }
+
+  public void setMzWindow(Double mzWindow) {
+    this.mzWindow = mzWindow;
+  }
+
+  public Double getRetentionTimeWindow() {
+    return retentionTimeWindow;
+  }
+
+  public void setRetentionTimeWindow(Double retentionTimeWindow) {
+    this.retentionTimeWindow = retentionTimeWindow;
+  }
+
+  public Double getMoleculeMass() {
+    return moleculeMass;
+  }
+
+  public void setMoleculeMass(Double moleculeMass) {
+    this.moleculeMass = moleculeMass;
+  }
+
+  public List<Metabolite> getMetabolites() {
+    return metabolites;
+  }
+
+  public void setMetabolites(List<Metabolite> metabolites) {
+    this.metabolites = metabolites;
   }
 }
