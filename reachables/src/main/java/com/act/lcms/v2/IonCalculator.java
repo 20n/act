d008dd367764bce5fbd2144f34ab88a13d0f04c9
thomas-20n <thomas@20n.com>
2016-10-26 12:25:21 -0700

    New methods for IonCalculator

--- reachables/src/main/java/com/act/lcms/v2/IonCalculator.java
+++ reachables/src/main/java/com/act/lcms/v2/IonCalculator.java
@@ -1,12 +1,19 @@
 package com.act.lcms.v2;
 
 
+import com.act.lcms.MS1;
+
 import java.util.List;
+import java.util.function.Predicate;
 
 public interface IonCalculator {
 
   List<Ion> getIons(Isotope isotope);
 
+  List<Ion> getSelectedIons(Isotope isotope, Predicate<MS1.MetlinIonMass> ionFilter);
+
   List<Ion> getIons(Metabolite metabolite);
+
+  List<Ion> getSelectedIons(Metabolite metabolite, Predicate<MS1.MetlinIonMass> ionFilter);
 
 }
