c8e506224ec101af9b5bedb1397e5a0730a8a130
Gil Goldshlager <gil@20n.com>
2016-10-27 16:49:53 -0400

    Remove mass index

--- reachables/src/main/java/com/act/biointerpretation/networkanalysis/MetaboliteToMz.java
+++ reachables/src/main/java/com/act/biointerpretation/networkanalysis/MetaboliteToMz.java
@@ -4,30 +4,47 @@ package com.act.biointerpretation.networkanalysis;
 import com.act.lcms.MS1;
 import com.act.lcms.v2.Ion;
 import com.act.lcms.v2.IonCalculator;
-import com.act.lcms.v2.IsotopeCalculator;
+import com.act.lcms.v2.Isotope;
 import com.act.lcms.v2.Metabolite;
+import com.jacob.com.NotImplementedException;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static com.act.lcms.MS1.computeIonMz;
 import static com.act.lcms.MS1.ionDeltas;
 
-public class MetaboliteToMz {
+public class MetaboliteToMz implements IonCalculator<Ion> {
 
   List<MS1.MetlinIonMass> ionMasses;
-  IsotopeCalculator isotopeCalculator;
-  IonCalculator ionCalculator;
 
   public MetaboliteToMz(Set<String> ions) {
     ionMasses = Arrays.asList(ionDeltas);
     ionMasses.removeIf(m -> !ions.contains(m.getName()));
   }
 
-  List<Ion> getIons(Metabolite metabolite) {
+  @Override
+  public List<Ion> getIons(Isotope isotope) {
+    throw new NotImplementedException("Not yet implemented!");
+  }
+
+  @Override
+  public List<Ion> getSelectedIons(Isotope isotope, Predicate<MS1.MetlinIonMass> ionFilter) {
+    throw new NotImplementedException("Not yet implemented!");
+  }
+
+  @Override
+  public List<Ion> getIons(Metabolite metabolite) {
+    return getSelectedIons(metabolite, ion -> true);
+  }
+
+  @Override
+  public List<Ion> getSelectedIons(Metabolite metabolite, Predicate<MS1.MetlinIonMass> ionFilter) {
     double mass = metabolite.getMonoIsotopicMass();
-    return ionMasses.stream().map(ion -> new MzIon(computeIonMz(mass, ion), ion)).collect(Collectors.toList());
+    return ionMasses.stream().filter(ionFilter).map(ion -> new MzIon(computeIonMz(mass, ion), ion)).collect(Collectors.toList());
+
   }
 }
