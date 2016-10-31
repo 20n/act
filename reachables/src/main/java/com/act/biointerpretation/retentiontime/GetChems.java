b090266656f0cafd1c1ba886564d220c1e14e0ab
vramakrishnan <vijay@20n.com>
2016-10-31 14:55:33 -0700

    added stuff

--- reachables/src/main/java/com/act/biointerpretation/retentiontime/GetChems.java
+++ reachables/src/main/java/com/act/biointerpretation/retentiontime/GetChems.java
@@ -1,24 +1,25 @@
 package com.act.biointerpretation.retentiontime;
 
+import act.server.DBIterator;
 import act.server.MongoDB;
 import act.shared.Chemical;
+import com.mongodb.DBObject;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.util.List;
 
 public class GetChems {
 
   public static void main(String[] args) throws Exception {
-
     MongoDB mongoDB = new MongoDB("localhost", 27017, "marvin");
+    DBIterator chemIter = mongoDB.getIteratorOverChemicals();
 
-    List<Chemical> chemicalList = mongoDB.getSigmaChemicals();
-
-    try (BufferedWriter predictionWriter = new BufferedWriter(new FileWriter(new File("/mnt/shared-data/Vijay/ret_time_prediction/marvin_chems.txt")))) {
-      for (Chemical chemical : chemicalList) {
-        predictionWriter.write(chemical.getInChI());
+    try (BufferedWriter predictionWriter = new BufferedWriter(new FileWriter(new File("/mnt/shared-data/Vijay/ret_time_prediction/marvin_all_chems.txt")))) {
+      while(chemIter.hasNext()) {
+        DBObject chemObj = chemIter.next();
+        Chemical chem = mongoDB.convertDBObjectToChemical(chemObj);
+        predictionWriter.write(chem.getInChI());
         predictionWriter.write("\n");
       }
     }
