0451bacdec913176e6fee6ca8c06d808f97a8d62
vramakrishnan <vijay@20n.com>
2016-10-31 15:35:10 -0700

    added sutff

--- reachables/src/main/java/com/act/biointerpretation/retentiontime/GetChems.java
+++ reachables/src/main/java/com/act/biointerpretation/retentiontime/GetChems.java
@@ -8,6 +8,9 @@ import com.mongodb.DBObject;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class GetChems {
 
@@ -15,12 +18,24 @@ public class GetChems {
     MongoDB mongoDB = new MongoDB("localhost", 27017, "marvin");
     DBIterator chemIter = mongoDB.getIteratorOverChemicals();
 
+    Set<Integer> indexes = new HashSet<>();
+    Random random = new Random(10);
+
+    for (int i = 0; i < 5000; i++) {
+      indexes.add(random.nextInt(943622));
+    }
+
+    int counter = 0;
+
     try (BufferedWriter predictionWriter = new BufferedWriter(new FileWriter(new File("/mnt/shared-data/Vijay/ret_time_prediction/marvin_all_chems.txt")))) {
       while(chemIter.hasNext()) {
-        DBObject chemObj = chemIter.next();
-        Chemical chem = mongoDB.convertDBObjectToChemical(chemObj);
-        predictionWriter.write(chem.getInChI());
-        predictionWriter.write("\n");
+        if (indexes.contains(counter)) {
+          DBObject chemObj = chemIter.next();
+          Chemical chem = mongoDB.convertDBObjectToChemical(chemObj);
+          predictionWriter.write(chem.getInChI());
+          predictionWriter.write("\n");
+        }
+        counter++;
       }
     }
   }
