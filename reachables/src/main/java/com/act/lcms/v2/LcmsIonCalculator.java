49516b007197353015f09aeccbe55fec587f68e2
thomas-20n <thomas@20n.com>
2016-11-02 10:01:34 -0700

    Adding filtering by ion name and mode

--- reachables/src/main/java/com/act/lcms/v2/LcmsIonCalculator.java
+++ reachables/src/main/java/com/act/lcms/v2/LcmsIonCalculator.java
@@ -5,6 +5,7 @@ import com.act.lcms.MS1;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -28,11 +29,19 @@ public class LcmsIonCalculator implements IonCalculator {
     return getIonsFromMass(isotope.getIsotopicMass(), ionFilter);
   }
 
+  public List<Ion> getSelectedIons(Isotope isotope, Set<String> ionNames, MS1.IonMode mode) {
+    return getIonsFromMass(isotope.getIsotopicMass(), x -> ionNames.contains(x.getName()) && x.getMode() == mode);
+  }
+
   public List<Ion> getIons(Metabolite metabolite) {
     return getIonsFromMass(metabolite.getMonoIsotopicMass(), x -> true);
   }
 
   public List<Ion> getSelectedIons(Metabolite metabolite, Predicate<MS1.MetlinIonMass> ionFilter) {
     return getIonsFromMass(metabolite.getMonoIsotopicMass(), ionFilter);
+  }
+
+  public List<Ion> getSelectedIons(Metabolite metabolite, Set<String> ionNames, MS1.IonMode mode) {
+    return getIonsFromMass(metabolite.getMonoIsotopicMass(), x -> ionNames.contains(x.getName()) && x.getMode() == mode);
   }
 }
