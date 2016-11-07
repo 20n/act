743b2017ec09d627b7c20188ea3d8dab7f2f698b
vramakrishnan <vijay@20n.com>
2016-11-07 11:13:43 -0800

    added stuff

--- reachables/src/main/java/com/act/biointerpretation/retentiontime/GetChems.java
+++ reachables/src/main/java/com/act/biointerpretation/retentiontime/GetChems.java
@@ -1,28 +1,22 @@
 package com.act.biointerpretation.retentiontime;
 
-import act.server.DBIterator;
 import act.server.MongoDB;
 import act.shared.Chemical;
-import chemaxon.formats.MolExporter;
+import chemaxon.calculations.clean.Cleaner;
 import chemaxon.formats.MolImporter;
+import chemaxon.marvin.calculations.LogPMethod;
+import chemaxon.marvin.calculations.MajorMicrospeciesPlugin;
+import chemaxon.marvin.calculations.logPPlugin;
 import chemaxon.struc.Molecule;
-import com.act.analysis.chemicals.molecules.MoleculeImporter;
-import com.act.utils.TSVParser;
 import com.act.utils.TSVWriter;
-import com.mongodb.DBObject;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.Set;
 
 public class GetChems {
 
@@ -34,9 +28,14 @@ public class GetChems {
     List<String> header = new ArrayList<>();
     header.add("Name");
     header.add("Inchi");
+    header.add("LogP");
+    header.add("Mass");
 
     TSVWriter<String, String> writer = new TSVWriter<>(header);
     writer.open(new File("/mnt/shared-data/Vijay/ret_time_prediction/marvin_l2_drugbank_sigma.txt"));
+
+    logPPlugin plugin = new logPPlugin();
+    MajorMicrospeciesPlugin microspeciesPlugin = new MajorMicrospeciesPlugin();
 
     for (Chemical chemical : mongoDB.getDrugbankChemicals()) {
       drugBankChems.put(chemical.getInChI(), chemical);
@@ -54,11 +53,51 @@ public class GetChems {
         Map<String, String> row = new HashMap<>();
         row.put("Name", drugBankChems.get(line).getFirstName());
         row.put("Inchi", line);
+
+        Molecule molecule = MolImporter.importMol(line, "inchi");
+
+        Cleaner.clean(molecule, 3);
+        plugin.standardize(molecule);
+        microspeciesPlugin.setpH(2.7);
+        microspeciesPlugin.setMolecule(molecule);
+        microspeciesPlugin.run();
+
+        Molecule phMol = microspeciesPlugin.getMajorMicrospecies();
+        plugin.setlogPMethod(LogPMethod.CONSENSUS);
+        plugin.setUserTypes("logPTrue,logPMicro,logPNonionic");
+        plugin.setMolecule(phMol);
+        plugin.run();
+
+        Double mass = molecule.getMass();
+        Double logP = plugin.getlogPTrue();
+        row.put("Mass", mass.toString());
+        row.put("LogP", logP.toString());
+
         writer.append(row);
       } else if (sigmaChems.keySet().contains(line)) {
         Map<String, String> row = new HashMap<>();
         row.put("Name", sigmaChems.get(line).getFirstName());
         row.put("Inchi", line);
+
+        Molecule molecule = MolImporter.importMol(line, "inchi");
+
+        Cleaner.clean(molecule, 3);
+        plugin.standardize(molecule);
+        microspeciesPlugin.setpH(2.7);
+        microspeciesPlugin.setMolecule(molecule);
+        microspeciesPlugin.run();
+
+        Molecule phMol = microspeciesPlugin.getMajorMicrospecies();
+        plugin.setlogPMethod(LogPMethod.CONSENSUS);
+        plugin.setUserTypes("logPTrue,logPMicro,logPNonionic");
+        plugin.setMolecule(phMol);
+        plugin.run();
+
+        Double mass = molecule.getMass();
+        Double logP = plugin.getlogPTrue();
+        row.put("Mass", mass.toString());
+        row.put("LogP", logP.toString());
+
         writer.append(row);
       }
     }
