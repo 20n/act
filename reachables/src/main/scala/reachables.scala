c7ec23e7d355e08f0241c0172caa8456c5ba2d56
MichaelLampe20n <Michael@20n.com>
2016-11-15 10:55:20 -0800

    CLI requirements for correctness

--- reachables/src/main/scala/reachables.scala
+++ reachables/src/main/scala/reachables.scala
@@ -6,12 +6,14 @@ import act.server.MongoDB
 import org.apache.commons.cli.{CommandLine, DefaultParser, HelpFormatter, Options, ParseException, Option => CliOption}
 import org.apache.logging.log4j.LogManager
 
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 import scala.io.Source
 
 object reachables {
   private val HELP_FORMATTER: HelpFormatter = new HelpFormatter
   HELP_FORMATTER.setWidth(100)
-  val logger = LogManager.getLogger(getClass.getName)
+  val LOGGER = LogManager.getLogger(getClass.getName)
 
   private val HELP_MESSAGE =
     """Usage: run --prefix=PRE --hasSeq=true|false --regressionSuiteDir=path --extra=[semicolon-sep db.chemical fields]
@@ -28,6 +30,7 @@ object reachables {
   private val OPTION_NATIVES_FILE = "n"
   private val OPTION_COFACTORS_FILE = "c"
   private val OPTION_REGRESSION_DIR = "r"
+  private val OPTION_EXTRA_INFORMATION = "e"
 
   private def getCommandLineOptions: Options = {
     val options = List[CliOption.Builder](
@@ -38,21 +41,31 @@ object reachables {
         desc("Prefix applied to any file generated").
         required(true),
 
+      CliOption.builder(OPTION_NATIVES_FILE).
+        hasArg().
+        longOpt("useNativesFile").
+        desc("Path to file containing native chemicals.").
+        required(true),
+
+      CliOption.builder(OPTION_COFACTORS_FILE).
+        hasArg().
+        longOpt("useCofactorsFile").
+        desc("Path to file containing cofactor chemicals.").
+        required(true),
+
       CliOption.builder(OPTION_HAS_SEQ).
         longOpt("hasSeq").
         desc("Flag to indicate if only reactions with sequences should be used."),
 
-      CliOption.builder(OPTION_NATIVES_FILE).
-        longOpt("useNativesFile").
-        desc("Path to file containing native chemicals."),
-
-      CliOption.builder(OPTION_COFACTORS_FILE).
-        longOpt("useCofactorsFile").
-        desc("Path to file containing cofactor chemicals."),
-
       CliOption.builder(OPTION_REGRESSION_DIR).
+        hasArg.
         longOpt("regressionSuiteDir").
-        desc("Path to the directory that regression suite files should be written to."),
+        desc("Path to the directory that contains regression test files."),
+
+      CliOption.builder(OPTION_EXTRA_INFORMATION).
+        hasArgs.valueSeparator(';').
+        longOpt("extra").
+        desc("Ensure chemicals with certain information are included."),
 
       CliOption.builder("h").argName("help").desc("Prints this help message").longOpt("help")
     )
@@ -69,7 +82,7 @@ object reachables {
     System.exit(1)
   }
 
-  def parseCommandLineOptions(args: Array[String]): CommandLine = {
+  private def parseCommandLineOptions(args: Array[String]): CommandLine = {
     val opts = getCommandLineOptions
 
     // Parse command line options
@@ -79,18 +92,18 @@ object reachables {
       cl = Option(parser.parse(opts, args))
     } catch {
       case e: ParseException =>
-        logger.error(s"Argument parsing failed: ${e.getMessage}\n")
+        LOGGER.error(s"Argument parsing failed: ${e.getMessage}\n")
         exitWithHelp(opts)
     }
 
     if (cl.isEmpty) {
-      logger.error("Detected that command line parser failed to be constructed.")
+      LOGGER.error("Detected that command line parser failed to be constructed.")
       exitWithHelp(opts)
     }
 
     if (cl.get.hasOption("help")) exitWithHelp(opts)
 
-    logger.info("Finished processing command line information")
+    LOGGER.info("Finished processing command line information")
     cl.get
   }
 
@@ -99,64 +112,58 @@ object reachables {
 
     val prefix = params.getOptionValue(OPTION_PREFIX)
 
-    writeReachableTree(prefix,
-      params.hasOption(OPTION_HAS_SEQ),
-      Option(params.getOptionValue(OPTION_NATIVES_FILE)),
+    writeReachableTree(prefix, params.hasOption(OPTION_HAS_SEQ),
+      Option(params.getOptionValues(OPTION_EXTRA_INFORMATION)),
+      Option(params.getOptionValue(OPTION_REGRESSION_DIR)),
+      params.getOptionValue(OPTION_NATIVES_FILE),
       Option(params.getOptionValue(OPTION_COFACTORS_FILE)))
   }
 
-  def writeReachableTree(prefix: String, needSeq: Boolean, nativesFile: Option[String], cofactorsFile: Option[String]) {
-    val rdir = prefix + ".regressions/" // regression output directory
-
+  def writeReachableTree(prefix: String, needSeq: Boolean, extraInformation: Option[Array[String]],
+                        regressionDir: Option[String],
+                        nativesFile: String,
+                        cofactorsFile: Option[String]) {
     // Connect to the DB so that extended attributes for chemicals can be fetched as we serialize.
     val db = new MongoDB("localhost", 27017, "marvin")
 
+    /* --------------- Parse Options ------------------ */
+    val regressionOutputDirectory = s"$prefix.regressions/"
 
-    val universal_natives = if (nativesFile.isDefined) {
-      val data = Source.fromFile(nativesFile.get).getLines
+    val universal_natives = {
+      val data = Source.fromFile(nativesFile).getLines
       val inchis = data.filter { x => x.length > 0 && x.charAt(0) != '#' }
-      collection.mutable.Set(inchis.toSeq: _*)
-    } else {
-      null
+      collection.mutable.Set(inchis.toSeq: _*).toSet.asJava
     }
 
-    val universal_natives = if (cofactorsFile.isDefined) {
+    val universal_cofactors = if (cofactorsFile.isDefined) {
       val data = Source.fromFile(cofactorsFile.get).getLines
       val inchis = data.filter { x => x.length > 0 && x.charAt(0) != '#' }
-      collection.mutable.Set(inchis.toSeq: _*).toSet
+      collection.mutable.Set(inchis.toSeq: _*).toSet.asJava
+    } else {
+      null
+    }
+
+    val regression_suite_files = if (regressionDir.isDefined){
+      val files = new File(regressionDir.get).listFiles
+      val testfiles = files.map(n => n.getAbsolutePath).filter(_.endsWith(".test.txt"))
+      testfiles.toSet
+    } else {
+      Set()
+    }
+
+    val chems_w_extra_fields = if (extraInformation.isDefined){
+      extraInformation.get
     } else {
       null
     }
 
 
-    val regression_suite_files =
-      opts.get("regressionSuiteDir") match {
-        case Some(dir) => {
-          val files = new File(dir).listFiles
-          val testfiles = files
-            .map(n => n.getAbsolutePath)
-            .filter(_.endsWith(".test.txt"))
-          testfiles.toSet
-        }
-        case _ => Set()
-      }
-
-    val chems_w_extra_fields =
-      opts.get("extra") match {
-        case Some(fields) => fields split ";"
-        case None => null
-      }
-
+    /* --------------- Construct Reachable Tree ------------------ */
     // set parameter for whether we want to exclude rxns that dont have seq
     GlobalParams._actTreeOnlyIncludeRxnsWithSequences = needSeq
 
-    // compute the reachables tree!
-    val restrict_to_enzymes_that_have_seqs = false
-    val tree = LoadAct.getReachablesTree(universal_natives,
-      universal_cofactors,
-      restrict_to_enzymes_that_have_seqs,
-      chems_w_extra_fields)
-    println("Done: L2 reachables computed. Num reachables found: " + tree.nodesAndIds.size)
+    val tree = LoadAct.getReachablesTree(universal_natives, universal_cofactors, needSeq, chems_w_extra_fields)
+    LOGGER.info(s"Done: L2 reachables computed. Num reachables found: ${tree.nodesAndIds.size}")
 
     // get inchis for all the reachables
     // Network.nodesAndIds(): Map[Node, Long] i.e., maps nodes -> ids
@@ -164,7 +171,7 @@ object reachables {
     // so we take the Map[Node, Long] and map each of the key_value pairs
     // by looking up their ids (using n_ids._2) in chemId2Inchis
     // then get a nice little set from that Iterable
-    def id2InChIName(id: Long) = id ->(ActData.instance.chemId2Inchis.get(id), ActData.instance.chemId2ReadableName.get(id))
+    def id2InChIName(id: Long) = id -> (ActData.instance.chemId2Inchis.get(id), ActData.instance.chemId2ReadableName.get(id))
     def tab(id_inchi_name: (Long, (String, String))) = id_inchi_name._1 + "\t" + id_inchi_name._2._2 + "\t" + id_inchi_name._2._1
 
     val reachables: Map[Long, (String, String)] = tree.nodesAndIds.map(x => id2InChIName(x._2)).toMap
@@ -172,13 +179,13 @@ object reachables {
     val r_inchis: Set[String] = reachables.values.toSet.map(fst) // reachables.values are (inchi, name)
 
     // create output directory for regression test reports, if not already exists
-    makeRegressionDirectory(rdir)
+    makeRegressionDirectory(regressionOutputDirectory)
     // run regression suites if provided
-    regression_suite_files.foreach(test => runRegression(r_inchis, test, rdir))
+    regression_suite_files.foreach(test => runRegression(r_inchis, test, regressionOutputDirectory))
 
     // serialize ActData, which contains a summary of the relevant act
     // data and the corresponding computed reachables state
-    ActData.instance.serialize(prefix + ".actdata")
+    ActData.instance.serialize(s"$prefix.actdata")
   }
 
   def runRegression(reachable_inchis: Set[String], test_file: String, output_report_dir: String) {
@@ -187,11 +194,10 @@ object reachables {
 
     val hdrs = Set("inchi", "name", "plausibility", "comment", "reference")
     if (testcols.isEmpty || !testcols.head.toSet.equals(hdrs)) {
-      println("Invalid test file: " + test_file)
-      println("\tExpected: " + hdrs.toString)
-      println("\tFound: " + testcols.head.toString)
+      LOGGER.warn("Invalid test file: " + test_file)
+      LOGGER.warn("\tExpected: " + hdrs.toString)
+      LOGGER.warn("\tFound: " + testcols.head.toString)
     } else {
-
       // delete the header from the data set, leaving behind only the test rows
       val hdr = testcols.head
       val rows = testcols.drop(1)
@@ -202,9 +208,9 @@ object reachables {
       val (passed, failed) = rows.partition(testrow => runRegression(add_hdrs(testrow), reachable_inchis))
 
       val report = generateReport(test_file, passed, failed)
-      writeTo(output_report_dir + "/" + new File(test_file).getName, report)
-      println("Regression file: " + test_file)
-      println("Total test: " + rows.length + " (passed, failed): (" + passed.length + ", " + failed.length + ")")
+      writeTo(new File(output_report_dir, new File(test_file).getName).getAbsolutePath, report)
+      LOGGER.info(s"Regression file: $test_file")
+      LOGGER.info(s"Total test: ${rows.length} (passed, failed): (${passed.length}, ${failed.length})")
     }
   }
 
@@ -212,11 +218,11 @@ object reachables {
     val dirl = new File(dir)
     if (dirl exists) {
       if (dirl.isFile) {
-        println(dir + " already exists as a file. Need it as dir for regression output. Abort.")
+        LOGGER.error(s"$dir already exists as a file. Need it as dir for regression output. Abort.")
         System.exit(-1)
       }
     } else {
-      dirl.mkdir()
+      dirl.mkdirs()
     }
   }
 
@@ -228,19 +234,19 @@ object reachables {
     val lines =
     // add summary to head of report file
       List(
-        "** Regression test result for " + f,
-        "\tTOTAL: " + total + " PASSED: " + passed.length,
-        "\tTOTAL: " + total + " FAILED: " + failed.length
+        s"** Regression test result for $f",
+        s"\tTOTAL: $total PASSED: ${passed.length}",
+        s"\tTOTAL: $total FAILED: ${failed.length}"
       ) ++ (
         // add details of cases that succeeded
         if (write_successes)
-          passed.map("\t\tPASSED: " + _).toList
+          passed.map("\t\tPASSED: " + _)
         else
           List()
         ) ++ (
         // add details of cases that failed
         if (write_failures)
-          failed.map("\t\tFAILED: " + _).toList
+          failed.map("\t\tFAILED: " + _)
         else
           List()
         )
@@ -258,11 +264,7 @@ object reachables {
     val should_exist = data.getOrElse("plausibility", "TRUE").toBoolean // plausibility column
 
     val exists = reachable_inchis.contains(inchi)
-
-    if (should_exist)
-      exists // if should-exist then output whether it exists
-    else
-      !exists // if should-not-exist then output negation of it-exists in reachable
+    exists
   }
 
   def writeTo(fname: String, json: String) {
