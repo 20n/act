7963d0205bb26609163e7f23963a2863a247d62e
thomas-20n <thomas@20n.com>
2016-11-16 11:19:28 -0800

    Mapping now works

--- reachables/src/main/java/act/installer/reachablesexplorer/WordCloudGenerator.java
+++ reachables/src/main/java/act/installer/reachablesexplorer/WordCloudGenerator.java
@@ -38,8 +38,8 @@ public class WordCloudGenerator {
     }
   }
 
-  public void generateWordCloud(String inchi, String filename) throws IOException {
-    String cmd = String.format("Rscript %s %s %s %s %s", rScript.getAbsolutePath(), inchi, filename, host, port);
+  public void generateWordCloud(String inchi, File file) throws IOException {
+    String cmd = String.format("Rscript %s %s %s %s %s", rScript.getAbsolutePath(), inchi, file.getAbsolutePath(), host, port);
 
     try {
       Process p = rt.exec(cmd);
@@ -62,7 +62,7 @@ public class WordCloudGenerator {
   public static void main(String[] args) {
     WordCloudGenerator g = new WordCloudGenerator("localhost", "27017");
     try {
-      g.generateWordCloud("InChI=1S/C8H9NO2/c1-6(10)9-7-2-4-8(11)5-3-7/h2-5,11H,1H3,(H,9,10)", "~/test-1");
+      g.generateWordCloud("InChI=1S/C8H9NO2/c1-6(10)9-7-2-4-8(11)5-3-7/h2-5,11H,1H3,(H,9,10)", new File("~/test-1"));
     } catch (IOException e) {System.out.println(String.format("Caught expection %s", e.getMessage()));}
   }
 
