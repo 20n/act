1fc544950c08f471fa71040391be6de62e905697
thomas-20n <thomas@20n.com>
2016-11-21 11:17:38 -0800

    Updated wordclouds

--- reachables/src/main/java/act/installer/reachablesexplorer/RWordCloudGenerator.R
+++ reachables/src/main/java/act/installer/reachablesexplorer/RWordCloudGenerator.R
@@ -1,5 +1,5 @@
 
-library(rmongodb)
+library(mongolite)
 library(wordcloud)
 
 kArgs <- commandArgs(trailingOnly = TRUE)
@@ -8,25 +8,35 @@ input.inchi <- kArgs[1]
 output.file <- kArgs[2]
 host <- kArgs[3]
 port <- kArgs[4]
-mongo <- mongo.create(paste(host, port, sep = ":"))
 
-query <- list(InChI=input.inchi)
 
-result <- mongo.find.one(mongo, "actv01.chemicals", query)
+library(mongolite)
 
-l <- mongo.bson.to.list(result)
+options(mongodb = list(
+  "host" = paste(host, port, sep = ":")
+))
+databaseName <- "validator_profiling_2"
+collectionName <- "chemicals"
 
-usage.terms.urls <- l$xref$BING$metadata$usage_terms
+db <- mongo(collection = collectionName,
+            url = sprintf(
+              "mongodb://%s/%s",
+              options()$mongodb$host,
+              databaseName))
 
-getUsageTerm <- function(x) x$usage_term
-getFreq <- function(x) length(x$urls)
 
-terms <- sapply(usage.terms.urls, getUsageTerm)
-freq <- sapply(usage.terms.urls, getFreq)
+result <- db$find(sprintf('{"InChI": "%s"}', input.inchi))
+
+
+usage.terms.urls <- as.data.frame(result$xref$BING$metadata$usage_terms)
+
+terms <- usage.terms.urls$usage_term
+freq <- sapply(usage.terms.urls$urls, length)
+
 pal2 <- brewer.pal(8,"Dark2")
 
 set.seed(27)
 
 png(output.file)
-wordcloud(terms,freq, scale=c(8,.2),min.freq=1, max.words=30, random.order=FALSE, rot.per=0, colors=pal2)
+wordcloud(terms,freq, scale=c(4,.5),min.freq=1, max.words=30, random.order=FALSE, rot.per=0, colors=pal2)
 dev.off()
