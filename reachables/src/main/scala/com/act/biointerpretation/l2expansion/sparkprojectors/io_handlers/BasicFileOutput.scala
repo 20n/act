e12dfac8a48a1cc990ee9dc17878b1af85e762c8
MichaelLampe20n <Michael@20n.com>
2016-11-21 10:43:08 -0800

    :)

---
+++ reachables/src/main/scala/com/act/biointerpretation/l2expansion/sparkprojectors/io_handlers/BasicFileOutput.scala
@@ -0,0 +1,20 @@
+package com.act.biointerpretation.l2expansion.sparkprojectors.io_handlers
+
+import java.io.File
+
+import org.apache.log4j.LogManager
+
+trait BasicFileOutput extends BasicOutput {
+  private val LOGGER = LogManager.getLogger(getClass)
+
+  final def createOutputDirectory(directory: File): Unit = {
+    if (directory.exists() && !directory.isDirectory) {
+      LOGGER.error(s"Found file when expected directory at ${directory.getAbsolutePath}.  " +
+        "Unable to set create output directories.")
+      System.exit(1)
+    } else {
+      LOGGER.info(s"Creating output directory at ${directory.getAbsolutePath}")
+      directory.mkdirs()
+    }
+  }
+}
