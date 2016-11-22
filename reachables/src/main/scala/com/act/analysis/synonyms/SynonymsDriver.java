07d521bcca823f39f85cdd5d80ad150e639efd69
thomas-20n <thomas@20n.com>
2016-11-22 15:23:02 -0800

    Investigating empty output

--- reachables/src/main/scala/com/act/analysis/synonyms/SynonymsDriver.java
+++ reachables/src/main/scala/com/act/analysis/synonyms/SynonymsDriver.java
@@ -2,6 +2,8 @@ package com.act.analysis.synonyms;
 
 
 import com.act.biointerpretation.l2expansion.L2InchiCorpus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -12,6 +14,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SynonymsDriver {
+
+  private static final Logger LOGGER = LogManager.getFormatterLogger(SynonymsDriver.class);
 
   private static final String LOCATION = "/mnt/shared-data/Thomas/L2inchis";
   private static final String OUTPUT_LOCATION = "/mnt/shared-data/Thomas/synonyms-test/L2inchis-synonyms";
@@ -30,15 +34,23 @@ public class SynonymsDriver {
 
     SynonymsExtractor extractor = new SynonymsExtractor();
     List<String> inchis = SynonymsDriver.getInchis(new File(LOCATION));
+    inchis = inchis.subList(0,10);
     List<Synonyms> synonymses = inchis.stream().map(Synonyms::new).collect(Collectors.toList());
+    LOGGER.info("Constructed list of %d synonymses", synonymses.size());
     synonymses.forEach(SynonymsExtractor::populateChemaxonSynonyms);
+    LOGGER.info("successfully updated chemaxon synonyms");
     synonymses.forEach(extractor::populateBrendaSynonyms);
+    LOGGER.info("successfully updated brenda synonyms");
     synonymses.forEach(extractor::populatePubchemSynonyms);
+    LOGGER.info("successfully updated pubchem synonyms");
 
+    String csvSynonyms;
     try {
-      new BufferedWriter(new FileWriter(OUTPUT_LOCATION)).write(SynonymsTSVWriter.toCSV(synonymses));
+      csvSynonyms = SynonymsTSVWriter.toCSV(synonymses);
+      LOGGER.info(csvSynonyms);
+      new BufferedWriter(new FileWriter(OUTPUT_LOCATION)).write(csvSynonyms);
     } catch (IOException e) {
-
+      e.printStackTrace();
     }
   }
 
