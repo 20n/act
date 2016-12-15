a1c3e63808aa4b8ec21ee8ff1da3782ff7990974
J. Christopher Anderson <chris@20n.com>
2016-12-15 14:13:04 -0800

    ProteinMetadataFactory and ProteinMetadataComparator are complete!

--- reachables/src/main/java/com/act/biointerpretation/metadata/ProteinMetadata.java
+++ reachables/src/main/java/com/act/biointerpretation/metadata/ProteinMetadata.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /**
  * Created by jca20n on 12/7/16.
  */
-public class ProteinMetadata implements Comparator {
+public class ProteinMetadata {
     String peptide;
 
     //These are strict requirements that these be ok
@@ -14,52 +14,30 @@ public class ProteinMetadata implements Comparator {
     Double kcatkm;
     Double specificActivity;
 
-    //Thgse things should be downranked but not dealbreakers
-    Localization localization;
+    //Thgse things should be downranked but not dealbreakers, they are fixable
+    Map<Host, Localization> localization;
     Boolean heteroSubunits;
     Boolean modifications;
 
 
-    public boolean isValid(Host host, Localization localization) {
-        if(kcatkm < 1.0) {   //Not in the top 80% of proteins, and anecdotally the bare minimum activity limit
+    public boolean isValid(Host host) {
+        //Don't bother if it's a really bad enzyme
+        if(kcatkm != null && kcatkm < 1.0) {   //Not in the top 80% of proteins, and anecdotally the bare minimum activity limit
             return false;
         }
-        if(specificActivity < 0.3) {  //Not in the top 80% of proteins
+
+        //Don't bother if it's a really bad enzyme
+        if(specificActivity != null && specificActivity < 0.3) {  //Not in the top 80% of proteins
             return false;
         }
-//        if(cloned == false) {
-//            return false;
-//        }
+
+        //Don't bother if there is a negaative observation about the protein working in the host
+        Integer clonedval = cloned.get(host);
+        if(clonedval != null && clonedval < 0) {
+            return false;
+        }
 
         return true;
     }
 
-    @Override
-    public int compare(Object o1, Object o2) {
-        ProteinMetadata p1 = (ProteinMetadata) o1;
-        ProteinMetadata p2 = (ProteinMetadata) o2;
-
-        //Return 0 if everything is the same
-        if(p1.kcatkm - p2.kcatkm < 0.0001) {
-            if(p1.specificActivity -p2.specificActivity < 0.0001) {
-                if(p1.localization.equals(p2.localization)) {
-                    if(p1.heteroSubunits == p2.heteroSubunits) {
-                        return 0;
-                    }
-                }
-            }
-        }
-
-        if(heteroSubunits == false) {  //If the protein has partners, it is downranked
-            return 0;
-        }
-
-        if(modifications == false) {  //If the protein has partners, it is downranked
-            return 0;
-        }
-
-        //Return pos if p1 > p2
-
-        return 0;
-    }
 }
