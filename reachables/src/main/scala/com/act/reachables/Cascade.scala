8dff0824a9b1f5b76d1430426cfca45c1a71c957
MichaelLampe20n <Michael@20n.com>
2017-01-11 14:28:21 -0800

    attempt 1

--- reachables/src/main/scala/com/act/reachables/Cascade.scala
+++ reachables/src/main/scala/com/act/reachables/Cascade.scala
@@ -398,7 +398,10 @@ object Cascade extends Falls {
       val branchedPaths = PathwayConstructor.getAllPaths(network, target)
       val result = PathwayConstructor.createNetworksFromPath(branchedPaths, network)
       println(branchedPaths)
-      println(result)
+      result.foreach(x => {
+        println
+        println(x.toDOT)
+      })
       Option(result.map(convertNetworkToPath(_, target)))
     } else {
       paths
