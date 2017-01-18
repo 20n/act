7addf52cae18008bf7ee7da2b6e0b48e1a73efa5
vramakrishnan <vijay@20n.com>
2017-01-18 11:54:45 -0800

    addressed comments

--- reachables/src/main/python/Wiki/generate_category_pages.py
+++ reachables/src/main/python/Wiki/generate_category_pages.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 from pymongo import MongoClient
 import json
 import operator
@@ -10,10 +8,10 @@ import os
 
 reachables_db = sys.argv[1]
 base_dir = sys.argv[2]
+db_name = sys.argv[3]
 
-defaultDBName = 'wiki_reachables'
 client = MongoClient('localhost', 27017)
-db = client[defaultDBName]
+db = client[db_name]
 
 # Make the category page directory
 categoryPath = os.path.join(base_dir, "Categories")
@@ -26,23 +24,14 @@ def makePage(page_name, chemicals):
     fileName = page_name
     target = open(os.path.join(categoryPath, fileName), 'w')
 
-    counter = 0
-
     for chemical in chemicals:
         if (chemical["inchikey"]):
             chemName = chemical["inchikey"].encode("utf-8")
             chemLink = "[[{0}]]".format(chemName)
             target.write(chemLink)
             target.write("\n\n")
-            counter += 1
-
-    print(counter)
 
     target.close()
-
-
-### generate all chemicals
-#makePage("All_Chemicals", db[reachables_db].find())
 
 ### generate Drug molecules pages
 makePage("Drug_Molecules", db[reachables_db].find({"xref.DRUGBANK": {"$exists": True}}))
@@ -54,6 +43,9 @@ makePage("Sigma_Molecules", db[reachables_db].find({"xref.SIGMA": {"$exists": Tr
 makePage("Wikipedia_Molecules", db[reachables_db].find({"xref.WIKIPEDIA": {"$exists": True}}))
 
 usageTerms = {"aroma": {}, "flavor": {}, "monomer": {}, "polymer": {}, "analgesic": {}}
+
+### Generate a dictionary of proportion of all usage terms of a given chemical that corresponds to the usage terms of
+### interest, as specified above.
 
 for chemical in db[reachables_db].find({"usage-wordcloud-filename": {"$ne": None}}):
     if (chemical["inchikey"]):
@@ -77,20 +69,17 @@ for chemical in db[reachables_db].find({"usage-wordcloud-filename": {"$ne": None
                     else:
                         usageTerms[usageKey][freq] = [chemLink]
 
+### We generate the usage terms pages that are sorted by the usage frequency
+
 for term in usageTerms:
     print(term)
     fileName = term.capitalize()
     target = open(os.path.join(categoryPath, fileName), 'w')
-
-    counter = 0
 
     sortedKeys = sorted(usageTerms[term].keys(), reverse=True)
     for key in sortedKeys:
         for chemLink in usageTerms[term][key]:
             target.write(chemLink)
             target.write("\n\n")
-            counter += 1
-
-    print(counter)
 
     target.close()
