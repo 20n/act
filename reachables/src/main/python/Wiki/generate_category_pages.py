d6e4c0fe5b4f9e2406c6b39cea9bc4850ef1fc7f
vramakrishnan <vijay@20n.com>
2017-01-18 14:02:12 -0800

    added more comments

--- reachables/src/main/python/Wiki/generate_category_pages.py
+++ reachables/src/main/python/Wiki/generate_category_pages.py
@@ -15,6 +15,7 @@ base_dir = sys.argv[2]
 db_name = sys.argv[3]
 
 client = MongoClient('localhost', 27017)
+
 db = client[db_name]
 
 # Make the category page directory
@@ -23,19 +24,16 @@ categoryPath = os.path.join(base_dir, "Categories")
 if not os.path.exists(categoryPath):
     os.makedirs(categoryPath)
 
-def makePage(page_name, chemicals):
-    print(page_name)
-    fileName = page_name
-    target = open(os.path.join(categoryPath, fileName), 'w')
+def makePage(fileName, chemicals):
+    with open(os.path.join(categoryPath, fileName), 'w') as target:
+        for chemical in chemicals:
+            if (chemical["inchikey"] is not None):
+                chemName = chemical["inchikey"].encode("utf-8")
+                chemLink = "[[{0}]]".format(chemName)
+                target.write(chemLink)
+                target.write("\n\n")
 
-    for chemical in chemicals:
-        if (chemical["inchikey"]):
-            chemName = chemical["inchikey"].encode("utf-8")
-            chemLink = "[[{0}]]".format(chemName)
-            target.write(chemLink)
-            target.write("\n\n")
-
-    target.close()
+    print("Finished printing page name: " + fileName)
 
 ### generate Drug molecules pages
 makePage("Drug_Molecules", db[reachables_db].find({"xref.DRUGBANK": {"$exists": True}}))
@@ -48,42 +46,43 @@ makePage("Wikipedia_Molecules", db[reachables_db].find({"xref.WIKIPEDIA": {"$exi
 
 usageTerms = {"aroma": {}, "flavor": {}, "monomer": {}, "polymer": {}, "analgesic": {}}
 
-### Generate a dictionary of proportion of all usage terms of a given chemical that corresponds to the usage terms of
-### interest, as specified above.
+### For each of the usage pages, we want to rank order the chemicals based on the proportion
+### of links that are related to the usage term. If a chemical has a greater proportion of it's
+### usage links linked to the usage term, it should list higher in the page. In order to do this,
+### we create a dictionary for every usage term that maps the frequency of the usage term appearing
+### to the chemical.
 
+### First we find all the chemicals that have usage terms associated with them.
 for chemical in db[reachables_db].find({"usage-wordcloud-filename": {"$ne": None}}):
     if (chemical["inchikey"]):
         chemName = chemical["inchikey"].encode("utf-8")
         chemLink = "[[{0}]]".format(chemName)
 
-        dictTerms = {}
+        dictOfUsageTerms = {}
         totalCount = 0
 
         for usage_term in chemical["xref"]["BING"]["metadata"]["usage_terms"]:
-            dictTerms[usage_term["usage_term"]] = len(usage_term["urls"])
+            dictOfUsageTerms[usage_term["usage_term"]] = len(usage_term["urls"])
             totalCount += len(usage_term["urls"])
 
-        for key in dictTerms.keys():
+        for key in dictOfUsageTerms.keys():
             for usageKey in usageTerms.keys():
                 if usageKey in key:
-                    freq = (dictTerms[key] * 1.0 / totalCount) * 100.0
-                    freq = math.floor(freq)
+                    freq = (dictOfUsageTerms[key] / totalCount) * 100
                     if freq in usageTerms[usageKey]:
                         usageTerms[usageKey][freq] += [chemLink]
                     else:
                         usageTerms[usageKey][freq] = [chemLink]
 
 ### We generate the usage terms pages that are sorted by the usage frequency
-
 for term in usageTerms:
-    print(term)
     fileName = term.capitalize()
-    target = open(os.path.join(categoryPath, fileName), 'w')
 
-    sortedKeys = sorted(usageTerms[term].keys(), reverse=True)
-    for key in sortedKeys:
-        for chemLink in usageTerms[term][key]:
-            target.write(chemLink)
-            target.write("\n\n")
+    with open(os.path.join(categoryPath, fileName), 'w') as target:
+        sortedKeys = sorted(usageTerms[term].keys(), reverse=True)
+        for key in sortedKeys:
+            for chemLink in usageTerms[term][key]:
+                target.write(chemLink)
+                target.write("\n\n")
 
-    target.close()
+    print("Finished printing page name: " + fileName)
